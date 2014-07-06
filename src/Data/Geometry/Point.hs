6c3b1a4da35d1b6580d7477e1fa1a1a3f99e7591
Frank Staals <frank@Hina.local>
2014-07-06 23:17:56 +0200

    Some more comments + renamed the n :: Nat to d, since we are considering dimensions

--- src/Data/Geometry/Point.hs
+++ src/Data/Geometry/Point.hs
@@ -36,20 +36,24 @@ import Data.Vinyl.Universe.Field((:::), ElField(..))
 
 import GHC.TypeLits
 
+-- | A type expressing a dimension. Note that something like 'D 2' expresses
+-- *only* the second dimention (in whatever space we consider). Say the
+-- two-dimensional plane is expressed by the type `R 2`.
+data D (d :: Nat)
+  -- essentially, we needed a wrapper type to get back from kind nat to kind *
 
-data D (n :: Nat)
 
 -- | Fields are indexed by either a type level natural number (a dimension), or
 -- by a type level string. If it is a natural number, the type of the value
 -- will be determined by the interpretation function. If it is a string, we can
 -- specify the type ourselves.
 data Field :: * -> * where
-  NatField :: KnownNat n    => Field (D n)
+  NatField :: KnownNat d    => Field (D d)
   SymField :: KnownSymbol s => Field (s ::: t)
 
 -- | Singletons for Field
 data SField :: * -> * where
-  SNatField :: KnownNat n    => SField (Field (D n))
+  SNatField :: KnownNat d    => SField (Field (D d))
   SSymField :: KnownSymbol s => SField (Field (s ::: t))
 
 
@@ -59,20 +63,29 @@ data SField :: * -> * where
 data TElField :: * -> (TyFun * *) -> * where
   TElField :: TElField t el
 
-type instance App (TElField r) (Field (D n))     = r
+type instance App (TElField r) (Field (D d))     = r
 type instance App (TElField r) (Field (s ::: t)) = t
 
 
 -- | Shorthand for naming dimention fields
-type DField (n :: Nat)  = Field (D n)
+type DField (d :: Nat)  = Field (D d)
 
 -- | And a fancy name for the symbol fields
 type (s :: Symbol) :~>: (t :: *) = Field (s ::: t)
 
 
 -- | Similar shorthands for the corresponding singletons
-type SDField (n :: Nat)             = SField (DField n)
+type SDField (d :: Nat)             = SField (DField d)
 type SSField (s :: Symbol) (t :: *) = SField (s :~>: t)
+
+
+
+
+
+
+data R (d :: Nat)
+
+
 
 
 x :: SDField 1
