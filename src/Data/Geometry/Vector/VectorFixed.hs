6447fb34f87f95277e910eccfd162bfa8005252a
Frank Staals <frank@Hina.local>
2014-10-24 21:21:04 +0200

    Fixing Vector

--- src/Data/Geometry/Vector/VectorFixed.hs
+++ src/Data/Geometry/Vector/VectorFixed.hs
@@ -1,8 +1,10 @@
+{-# LANGUAGE StandaloneDeriving #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE FlexibleInstances  #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving  #-}
 {-# LANGUAGE MultiParamTypeClasses  #-}
 {-# LANGUAGE UndecidableInstances #-}
-module Data.Geometry.Vector where
+module Data.Geometry.Vector.VectorFixed where
 
 import           Control.Applicative
 
@@ -25,77 +27,57 @@ import qualified Linear.V3 as L3
 --------------------------------------------------------------------------------
 
 newtype Vector (d :: Nat)  (r :: *) = Vector { _unV :: Vec (ToPeano d) r }
-                                     deriving (Show,Eq,Ord)
 
 ----------------------------------------
-
--- All these instances are basically the standard thing deriving would produce
-
-
 type Arity  (n :: Nat)  = V.Arity (ToPeano n)
--- type Arity1 (n :: Nat1) = V.Arity (Nat1ToPeano n)
+
+deriving instance (Show r, Arity d) => Show (Vector d r)
+deriving instance (Eq r, Arity d)   => Eq (Vector d r)
+deriving instance (Ord r, Arity d)  => Ord (Vector d r)
+
+deriving instance Arity d  => Functor (Vector d)
+deriving instance Arity d  => Foldable (Vector d)
+
+deriving instance Arity d  => Applicative (Vector d)
+
+instance Arity d => Traversable (Vector d) where
+  traverse f (Vector v) = Vector <$> traverse f v
 
 
--- instance (Arity d, Eq a) => Eq (Vector d a) where
---   (Vector u) == (Vector v) = u == v
+instance Arity d => Additive (Vector d) where
+  zero = pure 0
+  (Vector u) ^+^ (Vector v) = Vector $ V.zipWith (+) u v
 
--- instance (Arity d, Ord a) => Ord (Vector d a) where
---   (Vector u) <= (Vector v) = u <= v
+instance Arity d => Affine (Vector d) where
+  type Diff (Vector d) = Vector d
 
--- instance (Arity d, Show a) => Show (Vector d a) where
---   show (Vector v) = "Vector " ++ show v
-
--- instance Arity d => Functor (Vector d) where
---   fmap f (Vector v) = Vector $ fmap f v
-
--- instance Arity d => Applicative (Vector d) where
---   pure                        = Vector . pure
---   (Vector fv)  <*> (Vector v) = Vector $ fv <*> v
-
--- instance Arity d => Foldable (Vector d) where
---   foldMap f (Vector v) = foldMap f v
-
--- instance Arity d => Traversable (Vector d) where
---   traverse f (Vector v) = Vector <$> traverse f v
+  u .-. v = u ^-^ v
+  p .+^ v = p ^+^ v
 
 
+instance Arity d => Metric (Vector d)
 
+type instance V.Dim (Vector d) = ToPeano d
 
-
--- instance Arity d => Additive (Vector d) where
---   zero = pure 0
---   (Vector u) ^+^ (Vector v) = Vector $ V.zipWith (+) u v
-
--- instance Arity d => Affine (Vector d) where
---   type Diff (Vector d) = Vector d
-
---   u .-. v = u ^-^ v
---   p .+^ v = p ^+^ v
-
-
--- instance Arity d => Metric (Vector d) where
---   (Vector u) `dot` (Vector v) = V.sum $ V.zipWith (*) u v
-
-
--- type instance V.Dim (Vector d) = ToPeano d
-
--- instance Arity d => V.Vector (Vector d) r where
---   construct    = Vector <$> V.construct
---   inspect    v = V.inspect (unV v)
---   basicIndex v = V.basicIndex (unV v)
+instance Arity d => V.Vector (Vector d) r where
+  construct    = Vector <$> V.construct
+  inspect    v = V.inspect (_unV v)
+  basicIndex v = V.basicIndex (_unV v)
 
 -- ----------------------------------------
 
--- -- | Get the head and tail of a vector
--- destruct            :: (Arity dp, ToPeano d ~ S (ToPeano dp))
---                     => Vector d r -> (r, Vector dp r)
--- destruct (Vector v) = (V.head v, Vector $ V.tail v)
+-- | Get the head and tail of a vector
+destruct            :: (Arity dp, ToPeano d ~ S (ToPeano dp))
+                    => Vector d r -> (r, Vector dp r)
+destruct (Vector v) = (V.head v, Vector $ V.tail v)
 
 
--- cross       :: Num r => Vector 3 r -> Vector 3 r -> Vector 3 r
--- u `cross` v = fromV3 $ (toV3 u) `L3.cross` (toV3 v)
---   where
---     toV3 vv              = let [a,b,c] = V.toList vv in L3.V3 a b c
---     fromV3 (L3.V3 a b c) = Vector $ V.mk3 a b c
+cross       :: Num r => Vector 3 r -> Vector 3 r -> Vector 3 r
+u `cross` v = fromV3 $ (toV3 u) `L3.cross` (toV3 v)
+
+
+toV3 vv              = let [a,b,c] = V.toList vv in L3.V3 a b c
+
+fromV3 (L3.V3 a b c) = Vector $ V.mk3 a b c
 
 -- --------------------------------------------------------------------------------
