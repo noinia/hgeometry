61d76f10e44227b0f95f2bb372918c1969e00902
Frank Staals <frank@Hina.local>
2014-10-25 14:45:54 +0200

    Properites of Polylines

--- src/Data/Geometry/PolyLine.hs
+++ src/Data/Geometry/PolyLine.hs
@@ -1,19 +1,27 @@
+{-# LANGUAGE DeriveFunctor  #-}
 module Data.Geometry.PolyLine where
 
-
+import Data.Geometry.Vector(Arity)
 import Data.Geometry.Point
 import Data.Ext
 
 import qualified Data.Sequence as S
 
-newtype PolyLine d r pe = PolyLine { unPolyLine :: S.Seq (Point d r :+ pe) }
-                          -- deriving (Show,Eq,Ord)
+newtype PolyLine d pe r = PolyLine { unPolyLine :: S.Seq (Point d r :+ pe) }
 
+deriving instance (Show r, Show pe, Arity d) => Show (PolyLine d pe r)
+deriving instance (Eq r, Eq pe, Arity d)     => Eq (PolyLine d pe r)
+deriving instance (Ord r, Ord pe, Arity d)   => Ord (PolyLine d pe r)
+deriving instance Arity d                    => Functor (PolyLine d pe)
 
-data LineSegment d r pe = LineSegment { _start :: Point d r :+ pe
+data LineSegment d pe r = LineSegment { _start :: Point d r :+ pe
                                       , _end   :: Point d r :+ pe
                                       }
-                          -- deriving (Show,Eq,Ord)
+
+deriving instance (Show r, Show pe, Arity d) => Show (LineSegment d pe r)
+deriving instance (Eq r, Eq pe, Arity d)     => Eq (LineSegment d pe r)
+deriving instance (Ord r, Ord pe, Arity d)   => Ord (LineSegment d pe r)
+deriving instance Arity d                    => Functor (LineSegment d pe)
 
 
 
