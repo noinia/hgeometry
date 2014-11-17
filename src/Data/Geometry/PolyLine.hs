f21bc0e021acbe1038c2fdd324487d7cf792aece
Frank Staals <frank@Hina.local>
2014-11-17 22:43:18 +0100

    Trivial layout change in polyline

--- src/Data/Geometry/PolyLine.hs
+++ src/Data/Geometry/PolyLine.hs
@@ -20,9 +20,9 @@ import qualified Data.Sequence as S
 newtype PolyLine d pe r = PolyLine { _unPolyLine :: S.Seq (Point d r :+ pe) }
 makeLenses ''PolyLine
 
-deriving instance (Show r, Show pe, Arity d) => Show (PolyLine d pe r)
-deriving instance (Eq r, Eq pe, Arity d)     => Eq (PolyLine d pe r)
-deriving instance (Ord r, Ord pe, Arity d)   => Ord (PolyLine d pe r)
+deriving instance (Show r, Show pe, Arity d) => Show    (PolyLine d pe r)
+deriving instance (Eq r, Eq pe, Arity d)     => Eq      (PolyLine d pe r)
+deriving instance (Ord r, Ord pe, Arity d)   => Ord     (PolyLine d pe r)
 deriving instance Arity d                    => Functor (PolyLine d pe)
 type instance Dimension (PolyLine d pe r) = d
 type instance NumType   (PolyLine d pe r) = r
