2d3be6d093d17c5669462d60fbff99b047862391
Frank Staals <frank@Hina.local>
2015-03-26 20:00:24 +0100

    Balls, Circles, and Discs

---
+++ src/Data/Geometry/Ball.hs
@@ -0,0 +1,69 @@
+{-# LANGUAGE TemplateHaskell  #-}
+{-# LANGUAGE DeriveFunctor  #-}
+{-# LANGUAGE UndecidableInstances #-}
+module Data.Geometry.Ball where
+
+import Control.Lens
+
+import Data.Geometry.Line
+import Data.Geometry.Point
+import Data.Geometry.Properties
+import Data.Geometry.Vector
+import GHC.TypeLits
+import Linear.Affine(qdA, (.-.), (.+^))
+import Linear.Vector((^/))
+
+--------------------------------------------------------------------------------
+
+data Ball d r = Ball { _center        :: Point d r
+                     , _squaredRadius :: r
+                     }
+makeLenses ''Ball
+
+deriving instance (Show r, Arity d) => Show (Ball d r)
+deriving instance (Eq r, Arity d)   => Eq (Ball d r)
+deriving instance Arity d           => Functor (Ball d)
+
+type instance NumType   (Ball d r) = r
+type instance Dimension (Ball d r) = d
+
+
+
+
+data PointBallQueryResult = Inside | On | Outside deriving (Show,Read,Eq)
+
+inBall                 :: (Arity d, Ord r, Num r)
+                       => Point d r -> Ball d r -> PointBallQueryResult
+p `inBall` (Ball c sr) = case qdA p c `compare` sr of
+                           LT -> Inside
+                           EQ -> On
+                           GT -> Outside
+
+p `insideBall` b = p `inBall` b == Inside
+
+
+
+
+
+--------------------------------------------------------------------------------
+
+type Circle = Ball 2
+
+-- | Given three points, get the circle through the three points. If the three
+-- input points are colinear we return Nothing
+--
+-- >>> circle (point2 0 10) (point2 10 0) (point2 (-10) 0)
+
+circle       :: (Eq r, Fractional r)
+             => Point 2 r -> Point 2 r -> Point 2 r -> Maybe (Circle r)
+circle p q r = case f p `intersect` f q of
+                 LineLineIntersection c -> Just $ Ball c (qdA c p)
+                 _                      -> Nothing -- The two lines f p and f q are
+                                                   -- parallel, that means the three
+                                                   -- input points where colinear.
+  where
+    -- Given a point p', get the line perpendicular, and through the midpoint
+    -- of the line segment p'r
+    f p' = let v        = r .-. p'
+               midPoint = p' .+^ (v ^/ 2)
+           in perpendicularTo (Line midPoint v)
