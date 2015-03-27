585cfd5a967155180b4cd538c6be18d648fa22c4
Frank Staals <frank@Hina.local>
2015-03-27 11:04:28 +0100

    producing s.t. that we can paste into ipe

--- scripts/minDisk.hs
+++ scripts/minDisk.hs
@@ -1,12 +1,13 @@
 module MinDisk where
 
-import qualified Data.Foldable as F
 import           Control.Lens
+import           Data.Ext
+import qualified Data.Foldable as F
 import           Data.Geometry.Algorithms.SmallestEnclosingBall
 import           Data.Geometry.Ball
-import           Data.Geometry.Point
 import           Data.Geometry.Line
-import           Data.Ext
+import           Data.Geometry.Point
+import qualified Data.ByteString as B
 
 import           Data.Geometry.Ipe.Reader
 import           Data.Geometry.Ipe.Writer
@@ -30,7 +31,7 @@ main = do
 main' fp = do
   pls    <- polylinesFromIpeFile fp
   gen <- getStdGen
-  mapM_ (print . ipeWrite . minDisk' gen) pls
+  mapM_ (B.putStr . fromJust . toIpeXML . minDisk' gen) pls
 
 
 minDisk' :: RandomGen g => g -> PolyLine 2 () Double -> DiskResult () Double
