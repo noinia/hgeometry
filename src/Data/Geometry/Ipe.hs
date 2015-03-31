eb4ed8e9aba293e6f52436db323e6ba03ca25da4
Frank Staals <frank@Hina.local>
2015-03-31 20:04:57 +0200

    Fixed some import issue

--- src/Data/Geometry/Ipe.hs
+++ src/Data/Geometry/Ipe.hs
@@ -1,10 +1,8 @@
-module Data.Geometry.Ipe( module IT
-                        , module C, module A
-                        , module W, module R
+module Data.Geometry.Ipe( module I
                         ) where
 
-import qualified Data.Geometry.Ipe.Types as IT
-import qualified Data.Geometry.Ipe.Writer as W
-import qualified Data.Geometry.Ipe.Reader as R
-import qualified Data.Geometry.Ipe.Relations as C
-import qualified Data.Geometry.Ipe.Attributes as A
+import qualified Data.Geometry.Ipe.Types as I
+import qualified Data.Geometry.Ipe.Writer as I
+import qualified Data.Geometry.Ipe.Reader as I
+import qualified Data.Geometry.Ipe.Relations as I
+import qualified Data.Geometry.Ipe.Attributes as I
