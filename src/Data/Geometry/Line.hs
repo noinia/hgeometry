84e1ebb0c573c2a8d9278fb1d920af16de0a5bae
Frank Staals <frank@Shanna.FStaals.net>
2015-04-14 11:40:33 +0200

    Added HalfLines

--- src/Data/Geometry/Line.hs
+++ src/Data/Geometry/Line.hs
@@ -4,20 +4,20 @@
 {-# LANGUAGE TemplateHaskell  #-}
 module Data.Geometry.Line where
 
-
 import           Control.Applicative
 import           Control.Lens
-import qualified Data.Foldable as F
-import qualified Data.List as L
-import           Data.Semigroup
-import           Data.Ord(comparing)
 import           Data.Ext
+import qualified Data.Foldable as F
 import           Data.Geometry.Box
 import           Data.Geometry.Interval
 import           Data.Geometry.Point
 import           Data.Geometry.Properties
 import           Data.Geometry.Transformation
 import           Data.Geometry.Vector
+import qualified Data.List as L
+import           Data.Maybe(maybe)
+import           Data.Ord(comparing)
+import           Data.Semigroup
 import qualified Data.Seq2 as S2
 import           Linear.Affine(Affine(..),distanceA)
 import           Linear.Vector((*^))
@@ -124,6 +124,130 @@ instance (Eq r, Fractional r) => Eq (Intersection (Line 2 r) (Line 2 r)) where
   _                        == _                        = False
 
 
+
+--------------------------------------------------------------------------------
+-- * Supporting Lines
+
+class HasSupportingLine t where
+  supportingLine :: t -> Line (Dimension t) (NumType t)
+
+instance HasSupportingLine (Line d r) where
+  supportingLine = id
+
+--------------------------------------------------------------------------------
+-- * d-dimensional Half-Lines
+
+data HalfLine d r = HalfLine { _startPoint        :: Point  d r
+                             , _halfLineDirection :: Vector d r
+                             }
+makeLenses ''HalfLine
+
+
+
+deriving instance (Show r, Arity d) => Show    (HalfLine d r)
+deriving instance (Eq r, Arity d)   => Eq      (HalfLine d r)
+deriving instance Arity d           => Functor (HalfLine d)
+
+type instance Dimension (HalfLine d r) = d
+type instance NumType   (HalfLine d r) = r
+
+instance HasStart (HalfLine d r) where
+  type StartCore  (HalfLine d r) = Point d r
+  type StartExtra (HalfLine d r) = ()
+
+  start = lens ((:+ ()) . _startPoint) (\(HalfLine _ v) p -> HalfLine (p^.core) v)
+
+instance HasSupportingLine (HalfLine d r) where
+  supportingLine ~(HalfLine p v) = Line p v
+
+-- | Half-Lines are transformable
+instance (Num r, AlwaysTruePFT d) => IsTransformable (HalfLine d r) where
+  transformBy t = toHalfLine . transformPointFunctor t . toLineSegment'
+    where
+      toLineSegment' :: (Num r, Arity d) => HalfLine d r -> LineSegment d () r
+      toLineSegment' (HalfLine p v) = LineSegment (p :+ ()) ((p .+^ v) :+ ())
+
+instance (Ord r, Fractional r) => (HalfLine 2 r) `IsIntersectableWith` (Line 2 r) where
+  data Intersection (HalfLine 2 r) (Line 2 r) = NoHalfLineLineIntersection
+                                              | HalfLineLineIntersection !(Point 2 r)
+                                              | HalfLineLineOverlap      !(HalfLine 2 r)
+                                              deriving (Show,Eq)
+
+  nonEmptyIntersection NoHalfLineLineIntersection = False
+  nonEmptyIntersection _                          = True
+
+  hl `intersect` l = case supportingLine hl `intersect` l of
+    SameLine _             -> HalfLineLineOverlap hl
+    LineLineIntersection p -> if p `onHalfLine` hl then HalfLineLineIntersection p
+                                                   else NoHalfLineLineIntersection
+    ParallelLines          -> NoHalfLineLineIntersection
+
+
+instance (Ord r, Fractional r) => (HalfLine 2 r) `IsIntersectableWith` (HalfLine 2 r) where
+  data Intersection (HalfLine 2 r) (HalfLine 2 r) = NoHalfLineHalfLineIntersection
+                                                  | HLHLIntersectInPoint    !(Point 2 r)
+                                                  | HLHLIntersectInSegment  !(LineSegment 2 () r)
+                                                  | HLHLIntersectInHalfLine !(HalfLine 2 r)
+                                                  deriving (Show,Eq)
+
+  nonEmptyIntersection NoHalfLineHalfLineIntersection = False
+  nonEmptyIntersection _                              = True
+
+  hl' `intersect` hl = case supportingLine hl' `intersect` supportingLine hl of
+    ParallelLines          -> NoHalfLineHalfLineIntersection
+    LineLineIntersection p -> if p `onHalfLine` hl' && p `onHalfLine` hl then HLHLIntersectInPoint p
+                                                                         else NoHalfLineHalfLineIntersection
+    SameLine _             -> let p   = _startPoint hl'
+                                  q   = _startPoint hl
+                                  seg = LineSegment (p :+ ()) (q :+ ())
+                              in case (p `onHalfLine` hl, q `onHalfLine` hl') of
+                                   (False,False) -> NoHalfLineHalfLineIntersection
+                                   (False,True)  -> HLHLIntersectInHalfLine hl
+                                   (True, False) -> HLHLIntersectInHalfLine hl'
+                                   (True, True)  -> if hl == hl' then HLHLIntersectInHalfLine hl
+                                                                 else HLHLIntersectInSegment seg
+
+
+
+instance (Ord r, Fractional r) => (LineSegment 2 p r) `IsIntersectableWith` (HalfLine 2 r) where
+  data Intersection (LineSegment 2 p r) (HalfLine 2 r) = NoSegmentHalfLineIntersection
+                                                       | SegmentHalfLineIntersection !(Point 2 r)
+                                                       | SegmentOnHalfLine           !(LineSegment 2 () r)
+
+  nonEmptyIntersection NoSegmentHalfLineIntersection = False
+  nonEmptyIntersection _                             = True
+
+  s `intersect` hl = case supportingLine s `intersect` supportingLine hl of
+    ParallelLines          -> NoSegmentHalfLineIntersection
+    LineLineIntersection p -> if p `onSegment` s && p `onHalfLine` hl then SegmentHalfLineIntersection p
+                                                                      else NoSegmentHalfLineIntersection
+    SameLine _             -> let p = s  ^.start.core
+                                  q = s  ^.end.core
+                                  r = hl ^.start.core
+                                  seg a b = LineSegment (a :+ ()) (b :+ ())
+                              in case (p `onHalfLine` hl, q `onHalfLine` hl) of
+                                   (False, False)   -> NoSegmentHalfLineIntersection
+                                   (False, True)    -> SegmentOnHalfLine $ seg r q
+                                   (True,  False)   -> SegmentOnHalfLine $ seg p r
+                                   (True,  True)    -> SegmentOnHalfLine $ seg p q
+
+
+
+
+-- | Test if a point lies on a half-line
+onHalfLine :: (Ord r, Fractional r, Arity d) => Point d r -> HalfLine d r -> Bool
+p `onHalfLine` (HalfLine q v) = maybe False (>= 0) $ scalarMultiple (p .-. q) v
+
+
+
+
+
+-- | Transform a LineSegment into a half-line, by forgetting the second endpoint.
+toHalfLine                     :: (Num r, Arity d) => LineSegment d p r -> HalfLine d r
+toHalfLine (LineSegment p' q') = let p = p' ^.core
+                                     q = q' ^.core
+                                 in HalfLine p (q .-. p)
+
 --------------------------------------------------------------------------------
 -- * d-dimensional LineSegments
 
@@ -142,6 +266,9 @@ instance HasEnd (LineSegment d p r) where
   type EndExtra (LineSegment d p r) = p
   end = lens (_end . _unLineSeg) (\(LineSegment s _) t -> LineSegment s t)
 
+instance (Num r, Arity d) => HasSupportingLine (LineSegment d p r) where
+  supportingLine (LineSegment (p :+ _) (q :+ _)) = lineThrough p q
+
 deriving instance (Show r, Show p, Arity d) => Show (LineSegment d p r)
 deriving instance (Eq r, Eq p, Arity d)     => Eq (LineSegment d p r)
 deriving instance (Ord r, Ord p, Arity d)   => Ord (LineSegment d p r)
@@ -159,15 +286,17 @@ instance (Num r, AlwaysTruePFT d) => IsTransformable (LineSegment d p r) where
   transformBy = transformPointFunctor
 
 
+
+
+
+
+
+
 -- ** Converting between Lines and LineSegments
 
 toLineSegment            :: (Monoid p, Num r, Arity d) => Line d r -> LineSegment d p r
 toLineSegment (Line p v) = LineSegment (p       :+ mempty)
                                        (p .+^ v :+ mempty)
-
-toLine                                 :: (Num r, Arity d) => LineSegment d p r -> Line d r
-toLine (LineSegment (p :+ _) (q :+ _)) = lineThrough p q
-
 
 -- *** Intersecting LineSegments
 
@@ -189,8 +318,8 @@ instance (Ord r, Fractional r) =>
       LineLineIntersection r | onBothSegments r -> LineSegLineSegIntersection r
       _                                         -> NoIntersection
     where
-      la = toLine a
-      lb = toLine b
+      la = supportingLine a
+      lb = supportingLine b
       onBothSegments r = onSegment r a && onSegment r b
 
 instance (Ord r, Fractional r) =>
@@ -204,7 +333,7 @@ instance (Ord r, Fractional r) =>
   nonEmptyIntersection NoLineLineSegmentIntersection = False
   nonEmptyIntersection _                             = True
 
-  s `intersect` l = case (toLine s) `intersect` l of
+  s `intersect` l = case (supportingLine s) `intersect` l of
     SameLine _                               -> LineContainsSegment s
     LineLineIntersection p | p `onSegment` s -> LineLineSegmentIntersection p
     _                                        -> NoLineLineSegmentIntersection
@@ -243,7 +372,7 @@ onSegment       :: (Ord r, Fractional r, Arity d)
                 => Point d r -> LineSegment d p r -> Bool
 p `onSegment` l = let s         = l^.start.core
                       t         = l^.end.core
-                      inRange x = (fromIntegral 0) <= x && x <= (fromIntegral 1)
+                      inRange x = 0 <= x && x <= 1
                   in maybe False inRange $ scalarMultiple (p .-. s) (t .-. s)
 
 
@@ -286,7 +415,7 @@ overlap l m = mim >>= \im -> case il `intersect` im of
 
 -- | Lines are transformable, via line segments
 instance (Num r, AlwaysTruePFT d) => IsTransformable (Line d r) where
-  transformBy t = toLine . transformPointFunctor t . toLineSegment'
+  transformBy t = supportingLine . transformPointFunctor t . toLineSegment'
     where
       toLineSegment' :: (Num r, Arity d) => Line d r -> LineSegment d () r
       toLineSegment' = toLineSegment
