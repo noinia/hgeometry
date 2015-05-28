0466d81a4600e8f462a86045cf6b757b5502fd7d
Frank Staals <frank@Shanna.FStaals.net>
2015-05-28 11:48:20 +0200

    deleting junk

--- src/Data/TypeLevel/Common.hs
+++
@@ -1,8 +0,0 @@
-module Data.TypeLevel.Common where
-
-import Data.Vinyl.TypeLevel
-
-type family (n :: Nat) <= (m :: Nat) :: Bool where
-  Z     <= m     = True
-  (S n) <= Z     = False
-  (S n) <= (S m) = n <= m
