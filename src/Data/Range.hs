4d7f2a19638db3facbc83838e3a2b209c3bd798a
Frank Staals <frank@Hina.local>
2015-06-14 00:18:30 +0200

    Fixed a bug, added doctests

--- src/Data/Range.hs
+++ src/Data/Range.hs
@@ -80,6 +80,8 @@ pattern ClosedRange l u = Range (Closed l) (Closed u)
 -- | A range from l to u, ignoring/forgetting the type of the enpoints
 pattern Range' l u <- (_lower &&& _upper -> (l,u))
 
+
+
 prettyShow             :: Show a => Range a -> String
 prettyShow (Range l u) = concat [ lowerB, show (l^.unEndPoint), ", "
                                 , show (u^.unEndPoint), upperB
@@ -121,12 +123,28 @@ instance Ord a => (Range a) `IsIntersectableWith` (Range a) where
 
   nonEmptyIntersection = defaultNonEmptyIntersection
 
+  -- >>> (ClosedRange 1 10) `intersect` (OpenRange 5 10)
+  -- (Col Range {_lower = Open 5, _upper = Closed 10})
+  -- >>> (OpenRange 1 10) `intersect` (OpenRange 5 10)
+  -- (Col Range {_lower = Open 5, _upper = Open 10})
+  -- >>> (ClosedRange (1::Int) 10) `intersect` (OpenRange 50 (60 :: Int))
+  -- (Col NoIntersection)
+  -- >>> (OpenRange 1 10) `intersect` (OpenRange 10 12)
+  -- (Col NoIntersection)
+  -- >>> (OpenRange 1 10) `intersect` (ClosedRange 10 12)
+  -- (Col Range {_lower = Closed 10, _upper = Open 10})
+  --
   -- The intersection is empty, if after clipping, the order of the end points is inverted
   -- or if the endpoints are the same, but both are open.
   r@(Range l u) `intersect` s = let i@(Range l' u') = clipLower' l . clipUpper' u $ s
                                 in if isValid i then coRec i else coRec NoIntersection
 
 -- | Get the width of the interval
+--
+-- >>> width $ ClosedRange 1 10
+-- 9
+-- >>> width $ OpenRange 5 10
+-- 5
 width   :: Num r => Range r -> r
 width i = i^.upper.unEndPoint - i^.lower.unEndPoint
 
@@ -135,9 +153,13 @@ width i = i^.upper.unEndPoint - i^.lower.unEndPoint
 --------------------------------------------------------------------------------
 -- * Helper functions
 
+-- | Clip the interval from below. I.e. intersect with the interval {l,infty),
+-- where { is either open, (, orr closed, [.
 clipLower     :: Ord a => EndPoint a -> Range a -> Maybe (Range a)
 clipLower l r = let r' = clipLower' l r in if isValid r' then Just r' else Nothing
 
+-- | Clip the interval from above. I.e. intersect with (-\infty, u}, where } is
+-- either open, ), or closed, ],
 clipUpper     :: Ord a => EndPoint a -> Range a -> Maybe (Range a)
 clipUpper u r = let r' = clipUpper' u r in if isValid r' then Just r' else Nothing
 
@@ -154,12 +176,12 @@ isValid r@(Range l u) = case (_unEndPoint l) `compare` (_unEndPoint u) of
 -- operation is unsafe, as it may produce an invalid range (where l > u)
 clipLower'                  :: Ord a => EndPoint a -> Range a -> Range a
 clipLower' l' r@(Range l u) = case l' `cmpLower` l of
-                                LT -> Range l' u
+                                GT -> Range l' u
                                 _  -> r
 -- operation is unsafe, as it may produce an invalid range (where l > u)
 clipUpper'                  :: Ord a => EndPoint a -> Range a -> Range a
 clipUpper' u' r@(Range l u) = case u' `cmpUpper` u of
-                                GT -> Range l u'
+                                LT -> Range l u'
                                 _  -> r
 
 -- | Compare end points, Closed < Open
@@ -184,3 +206,26 @@ cmpUpper a b = case (_unEndPoint a) `compare` (_unEndPoint b) of
                          (Closed _, Closed _) -> EQ
                          (Open _,  _)         -> LT  -- otherwise, choose the Closed one
                          (Closed _,_)         -> GT  -- is the *largest*
+
+
+
+
+--------------------------------------------------------------------------------
+
+-- | Shift a range x units to the left
+--
+-- >>> prettyShow $ shiftLeft 10 (ClosedRange 10 20)
+-- [0,10]
+-- >>> prettyShow $ shiftLeft 10 (OpenRange 15 25)
+-- (5,15)
+shiftLeft   :: Num r => r -> Range r -> Range r
+shiftLeft x = shiftRight (-x)
+
+-- | Shifts the range to the right
+--
+-- >>> prettyShow $ shiftRight 10 (ClosedRange 10 20)
+-- [20,30]
+-- >>> prettyShow $ shiftRight 10 (OpenRange 15 25)
+-- (25,35)
+shiftRight   :: Num r => r -> Range r -> Range r
+shiftRight x = fmap (+x)
