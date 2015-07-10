0c9ed91ce267ababceeeb99e90fd17107d4cb5a9
Frank Staals <frank@Hina.local>
2015-07-10 15:18:49 +0200

    More IpeOut's

--- src/Data/Geometry/Ipe/IpeOut.hs
+++ src/Data/Geometry/Ipe/IpeOut.hs
@@ -2,16 +2,23 @@
 module Data.Geometry.Ipe.IpeOut where
 
 import           Control.Applicative
+import           Control.Lens
+import           Data.Bifunctor
 import           Data.Ext
 import qualified Data.Foldable as F
 import           Data.Geometry.Ball
 import           Data.Geometry.Ipe.Attributes
 import           Data.Geometry.Ipe.Types
-import           Data.Geometry.PolyLine
+import           Data.Geometry.LineSegment
 import           Data.Geometry.Point
+import           Data.Geometry.PolyLine
+import           Data.Geometry.Transformation
 import qualified Data.List.NonEmpty as NE
 import           Data.Proxy
+import qualified Data.Seq2 as S2
+import           Data.Text(Text)
 import qualified Data.Traversable as Tr
+import           Data.Vinyl
 
 --------------------------------------------------------------------------------
 
@@ -54,20 +61,33 @@ diskMark = mark "mark/disk(sx)"
 
 --------------------------------------------------------------------------------
 
--- lineSegment :: ToIpe (LineSegment 2 p r) (PathSegment r)
--- lineSegment = PolyLineSegment ()
+lineSegment :: IpeOut (LineSegment 2 p r) (Path r)
+lineSegment = fromPathSegment lineSegment'
+
+lineSegment' :: IpeOut (LineSegment 2 p r) (PathSegment r)
+lineSegment' = IpeOut $ PolyLineSegment . fromLineSegment . first (const ())
 
 
+polyLine :: IpeOut (PolyLine 2 p r) (Path r)
+polyLine = fromPathSegment polyLine'
 
---path
+polyLine' :: IpeOut (PolyLine 2 a r) (PathSegment r)
+polyLine' = IpeOut $ PolyLineSegment . first (const ())
 
 
+circle :: Floating r => IpeOut (Circle p r) (Path r)
+circle = fromPathSegment circle'
 
-circle                   :: Floating r => IpeOut (Circle p r) (PathSegment r)
-circle = ToIpe circle'
+circle' :: Floating r => IpeOut (Circle p r) (PathSegment r)
+circle' = IpeOut circle''
   where
-    circle' (Ball (c :+ _) r) = EllipseSegment m
+    circle'' (Circle (c :+ _) r) = EllipseSegment m
       where
         m = translation (toVec c) |.| uniformScaling (sqrt r) ^. transformationMatrix
         -- m is the matrix s.t. if we apply m to the unit circle centered at the origin, we
         -- get the input circle.
+
+
+-- | Helper to construct a IpeOut g Path, for when we already have an IpeOut g PathSegment
+fromPathSegment    :: IpeOut g (PathSegment r) -> IpeOut g (Path r)
+fromPathSegment io = IpeOut $ Path . S2.l1Singleton . asIpe io
