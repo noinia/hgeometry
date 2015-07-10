4b0a246017178715dd064a7eee96ce054a8a233b
Frank Staals <frank@Hina.local>
2015-07-10 16:47:27 +0200

    More work on IpeOut stuff + updated the minDisk demo (and moved it)

--- src/Data/Geometry/Ipe/IpeOut.hs
+++ src/Data/Geometry/Ipe/IpeOut.hs
@@ -2,11 +2,12 @@
 module Data.Geometry.Ipe.IpeOut where
 
 import           Control.Applicative
-import           Control.Lens
+import           Control.Lens hiding (only)
 import           Data.Bifunctor
 import           Data.Ext
 import qualified Data.Foldable as F
-import           Data.Geometry.Ball
+import           Data.Geometry.Ball hiding (disk)
+import           Data.Geometry.Boundary
 import           Data.Geometry.Ipe.Attributes
 import           Data.Geometry.Ipe.Types
 import           Data.Geometry.LineSegment
@@ -26,6 +27,9 @@ newtype IpeOut g i = IpeOut { asIpe :: g -> i }
 
 
 
+
+
+
 -- | Given a Prism to convert a geometry object into an ipe geometry object,
 -- the geometry object, and a record with its attributes, construct its corresponding
 -- ipe Object.
@@ -37,6 +41,16 @@ asIpeObject         :: ( Rec (f r) ats ~ IpeObjectAttrElF r (it ats)
                     -> g -> Rec (f r) ats -> IpeObject r (it ats)
 asIpeObject f g ats = asObject (asIpe f g) ats
 
+
+asIpeObject' :: ( Rec (f r) ats ~ IpeObjectAttrElF r (it ats)
+                , RevIpeObjectValueElF (DefaultIpeOut g) ~ it
+                , (DefaultIpeOut g) ~ IpeObjectValueElF r (it ats)
+                , HasDefaultIpeOut g
+                )
+             => g -> Rec (f r) ats -> IpeObject r (it ats)
+asIpeObject' = asIpeObject defaultIpeOut
+
+
 -- | Given one of the ipe values, (i.e. a Path, IpeSymbol, etc.) and a Rec of
 -- the appropriate corresponding type, construct an ipe Object from the two.
 asObject     :: ( Rec (f r) ats ~ IpeObjectAttrElF r (it ats)
@@ -46,6 +60,25 @@ asObject     :: ( Rec (f r) ats ~ IpeObjectAttrElF r (it ats)
              -> IpeObjectAttrElF r (it ats)
              -> IpeObject r (it ats)
 asObject x r = IpeObject $ x :+ r
+
+
+
+
+
+class HasDefaultIpeOut g where
+  type DefaultIpeOut g
+
+  defaultIpeOut :: IpeOut g (DefaultIpeOut g)
+
+  -- defaultAttributes :: Rec (f r) ats
+
+instance HasDefaultIpeOut (LineSegment 2 p r) where
+  type DefaultIpeOut (LineSegment 2 p r) = Path r
+  defaultIpeOut = lineSegment
+
+instance Floating r => HasDefaultIpeOut (Disk p r) where
+  type DefaultIpeOut (Disk p r) = Path r
+  defaultIpeOut = disk
 
 
 --------------------------------------------------------------------------------
@@ -75,6 +108,9 @@ polyLine' :: IpeOut (PolyLine 2 a r) (PathSegment r)
 polyLine' = IpeOut $ PolyLineSegment . first (const ())
 
 
+disk :: Floating r => IpeOut (Disk p r) (Path r)
+disk = IpeOut $ asIpe circle . Boundary
+
 circle :: Floating r => IpeOut (Circle p r) (Path r)
 circle = fromPathSegment circle'
 
@@ -91,3 +127,22 @@ circle' = IpeOut circle''
 -- | Helper to construct a IpeOut g Path, for when we already have an IpeOut g PathSegment
 fromPathSegment    :: IpeOut g (PathSegment r) -> IpeOut g (Path r)
 fromPathSegment io = IpeOut $ Path . S2.l1Singleton . asIpe io
+
+
+
+
+ls = (ClosedLineSegment (only origin) (only (point2 1 1)))
+
+
+--test :: IpeObject Integer ('IpePath '[])
+test = asIpeObject lineSegment ls emptyPathAttributes
+
+
+-- test' :: IpeObject Integer ('IpePath '[])
+test' = asIpeObject' ls emptyPathAttributes
+
+
+
+
+emptyPathAttributes :: Rec (PathAttribute r) '[]
+emptyPathAttributes = RNil
