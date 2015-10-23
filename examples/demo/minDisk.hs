c20ac9459c5c66b2432489216f1fb17d6b4a82c6
Frank Staals <frank@Hina.local>
2015-10-23 21:45:42 +0200

    Updated the mindisk example

--- examples/demo/minDisk.hs
+++ examples/demo/minDisk.hs
@@ -1,9 +1,13 @@
+{-# LANGUAGE ScopedTypeVariables #-}
 module MinDisk where
 
 import           Algorithms.Geometry.SmallestEnclosingBall.RandomizedIncrementalConstruction
-import           Control.Lens
+import           Algorithms.Geometry.SmallestEnclosingBall.Types
+
+import           Control.Lens hiding (only)
 import           Data.Ext
 import qualified Data.Foldable as F
+import qualified Data.Traversable as Tr
 import           Data.Geometry
 import           Data.Geometry.Ball
 import           Data.Geometry.Line
@@ -12,10 +16,8 @@ import           Data.Monoid
 
 --import           Data.Geometry.Ipe
 
+import           Data.Geometry.Ipe
 import           Data.Geometry.Ipe.Types
-import           Data.Geometry.Ipe.IpeOut
-import           Data.Geometry.Ipe.Reader
-import           Data.Geometry.Ipe.Writer
 import           Data.Maybe
 import           Data.Seq2
 import           Data.Vinyl
@@ -27,8 +29,6 @@ diskResult :: Floating r => IpeOut (DiskResult p r) (IpeObject r)
 diskResult = IpeOut f
   where
     f (DiskResult d pts) = asIpeGroup (asIpeObject d mempty : (F.toList . fmap g $ pts))
-                                      mempty
-
     g p = asIpeObject (p^.core) mempty
 
 main = do
@@ -37,14 +37,23 @@ main = do
 
 
 main' fp = do
-  pls    <- polylinesFromIpeFile fp
-  let ipeP = undefined -- read a single page
-      pts  = ipeP^..content._IpeUse.core.symbolPoint
-      polies = ipeP^..content._Path.core.asPolyLine
-  gen <- getStdGen
-  print pls
-  mapM_ (print . (^.enclosingDisk) . minDisk' gen) pls
-  mapM_ (printAsIpeSelection . asIpe diskResult . minDisk' gen) pls
+    ep <- readSinglePageFile fp
+    gen <- getStdGen
+    case ep of
+      Left err                       -> print err
+      Right (ipeP :: IpePage Double) ->
+        case map only $ ipeP^..content.Tr.traverse._IpeUse.core.symbolPoint of
+          (a:b:rest) -> do
+                          let res = smallestEnclosingDisk gen a b rest
+                          printAsIpeSelection . asIpe diskResult $ res
+          _          -> putStrLn "Not enough points!"
+
+
+      -- polies = ipeP^..content.Tr.traverse._IpePath.core._asPolyLine
+
+      -- print pls
+      -- mapM_ (print . (^.enclosingDisk) . minDisk' gen) polies
+      -- mapM_ (printAsIpeSelection . asIpe diskResult . minDisk' gen) polies
 
 
 minDisk' :: RandomGen g => g -> PolyLine 2 () Double -> DiskResult () Double
