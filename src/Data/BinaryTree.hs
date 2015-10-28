35605f01996327779338c7acf7560783acb5a789
Frank Staals <frank@Hina.local>
2015-10-28 09:46:02 +0100

    Generalized the binary tree thing; added a measured instance

--- src/Data/BinaryTree.hs
+++ src/Data/BinaryTree.hs
@@ -1,4 +1,5 @@
 {-# Language DeriveFunctor #-}
+{-# Language FunctionalDependencies #-}
 module Data.BinaryTree where
 
 import Control.Applicative
@@ -7,27 +8,53 @@ import Data.List.NonEmpty(NonEmpty)
 import Data.Semigroup
 import Data.Traversable
 
-data BinLeafTree a = Leaf a
-                   | Node (BinLeafTree a) (BinLeafTree a)
-                   deriving (Show,Read,Eq,Ord,Functor)
+data BinLeafTree v a = Leaf a
+                     | Node (BinLeafTree v a) v (BinLeafTree v a)
+                     deriving (Show,Read,Eq,Ord,Functor)
 
-instance Foldable BinLeafTree where
-  foldMap f (Leaf a) = f a
-  foldMap f (Node l r) = foldMap f l `mappend` foldMap f r
 
-instance Traversable BinLeafTree where
-  traverse f (Leaf a)   = Leaf <$> f a
-  traverse f (Node l r) = Node <$> traverse f l <*> traverse f r
+class Semigroup v => Measured v a | a -> v where
+  measure :: a -> v
 
-instance Semigroup (BinLeafTree a) where
-  l <> r = Node l r
+-- | smart constructor
+node     :: Measured v a => BinLeafTree v a -> BinLeafTree v a -> BinLeafTree v a
+node l r = Node l (measure l <> measure r) r
 
-asBalancedBinLeafTree    :: NonEmpty a -> BinLeafTree a
+
+instance Measured v a => Measured v (BinLeafTree v a) where
+  measure (Leaf x)     = measure x
+  measure (Node _ v _) = v
+
+
+instance Foldable (BinLeafTree v) where
+  foldMap f (Leaf a)     = f a
+  foldMap f (Node l _ r) = foldMap f l `mappend` foldMap f r
+
+instance Traversable (BinLeafTree v) where
+  traverse f (Leaf a)     = Leaf <$> f a
+  traverse f (Node l v r) = Node <$> traverse f l <*> pure v <*> traverse f r
+
+instance Measured v a => Semigroup (BinLeafTree v a) where
+  l <> r = node l r
+
+
+asBalancedBinLeafTree    :: NonEmpty a -> BinLeafTree Size (Elem a)
 asBalancedBinLeafTree ys = asBLT (length ys') ys'
   where
     ys' = toList ys
 
-    asBLT _ [x] = Leaf x
+    asBLT _ [x] = Leaf (Elem x)
     asBLT n xs  = let h       = n `div` 2
                       (ls,rs) = splitAt h xs
-                  in Node (asBLT h ls) (asBLT (n-h) rs)
+                  in node (asBLT h ls) (asBLT (n-h) rs)
+
+newtype Size = Size Int deriving (Show,Read,Eq,Num,Integral,Enum,Real,Ord)
+
+instance Semigroup Size where
+  x <> y = x + y
+
+newtype Elem a = Elem { _unElem :: a }
+               deriving (Show,Read,Eq,Ord,Functor,Foldable,Traversable)
+
+instance Measured Size (Elem a) where
+  measure _ = 1
