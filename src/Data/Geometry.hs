2284f8b0209a362895454e57c50306d6774e4279
Frank Staals <frank@Hina.local>
2016-05-25 20:43:06 +0200

    No longer exporting Box by default

--- src/Data/Geometry.hs
+++ src/Data/Geometry.hs
@@ -6,22 +6,18 @@ module Data.Geometry( module Data.Geometry.Properties
                     , module Data.Geometry.LineSegment
                     , module Data.Geometry.PolyLine
                     , module Data.Geometry.Polygon
-                    , module Data.Geometry.Box
                     , module Linear.Affine
                     , module Linear.Vector
                     ) where
 
 
-import Data.Geometry.Properties
-import Data.Geometry.Transformation
-
-import Data.Geometry.Point
-import Data.Geometry.Vector
 import Data.Geometry.Line
 import Data.Geometry.LineSegment
+import Data.Geometry.Point
 import Data.Geometry.PolyLine hiding (fromPoints)
 import Data.Geometry.Polygon hiding (fromPoints)
-import Data.Geometry.Box
-
+import Data.Geometry.Properties
+import Data.Geometry.Transformation
+import Data.Geometry.Vector
 import Linear.Affine hiding (Point, origin)
 import Linear.Vector
