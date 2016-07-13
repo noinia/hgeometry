174513d0c92f0053c18b372d4a96f4018983aa6a
Frank Staals <frank@Hina.local>
2016-07-13 20:45:42 +0200

    more expected pairwise distance stuff

--- examples/Demo/ExpectedPairwiseDistance.hs
+++ examples/Demo/ExpectedPairwiseDistance.hs
@@ -1,33 +1,80 @@
+{-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 module Demo.ExpectedPairwiseDistance where
 
 import           Algorithms.Geometry.WellSeparatedPairDecomposition.Types
 import           Algorithms.Geometry.WellSeparatedPairDecomposition.WSPD
 import           Control.Lens
+import Data.Proxy
+import GHC.TypeLits(natVal,KnownNat)
 import           Data.BinaryTree
+import qualified Data.ByteString       as B
+import qualified Data.ByteString.Char8 as C
+import           Data.Char (isSpace)
 import           Data.Ext
 import           Data.Geometry
 import qualified Data.List.NonEmpty as NonEmpty
+import           Data.Maybe (mapMaybe)
 import           Data.Semigroup
 
+import Debug.Trace
 
+--------------------------------------------------------------------------------
 
--- | Evaluates the formula: $\sum_{p,q \in pts} \|pq\|*Prb[sample has size k and contains p and q]$ which solves to $\frac{{n-2 \choose k-2}}{{n \choose k}} \sum_{p,q} \|pq\|$
+-- | Evaluates the formula: $\sum_{p,q \in pts} \|pq\|*Prb[sample has size k
+-- and contains p and q]$ which solves to $\frac{{n-2 \choose k-2}}{{n \choose
+-- k}} \sum_{p,q} \|pq\|$
+--
+-- running time: $O(n^2)$, where $n$ is the number of points
 expectedPairwiseDistance       :: (Floating r, Arity d) => Int -> [Point d r :+ p] -> r
 expectedPairwiseDistance k pts = makeExpected k pts pairwiseDist
 
+-- | A $(1+\varepsilon)$-approximation of expectedPairwiseDistance
+--
+-- running time: $O((1/eps)^d * n\log n)$, where $n$ is the number of points
+approxExpectedPairwiseDistance          :: (Floating r, Ord r
+                                           , AlwaysTrueWSPD d, Index' 0 d
+                                         , Show r, Show p)
+                                         => r -> Int -> [Point d r :+ p] -> r
+approxExpectedPairwiseDistance eps k pts =
+  makeExpected k pts (approxPairwiseDistance eps)
 
--- approxExpectedPairwiseDistance ::
+--------------------------------------------------------------------------------
+-- * Computing Distances
+
+-- | Sum of the pairwise distances
+pairwiseDist     :: (Floating r, Arity d) => [Point d r :+ p] -> r
+pairwiseDist pts = sum [ euclideanDist (p^.core) (q^.core) | p <- pts, q <- pts]
 
 
+-- | $(1+\eps)$-approximation of the sum of the pairwise distances.
+--
+-- running time: $O((1/eps)^d * n\log n)$, where $n$ is the number of points
+approxPairwiseDistance         :: (Floating r, Ord r, AlwaysTrueWSPD d, Index' 0 d
+                                  , Show r, Show p)
+                               => r -> [Point d r :+ p] -> r
+approxPairwiseDistance _   []  = 0
+approxPairwiseDistance eps pts =
+    sum [ (size as)*(size bs)*euclideanDist (repr as) (repr bs) | (as,bs) <- pairs ]
+  where
+    t     = withSizes . fairSplitTree . NonEmpty.fromList $ pts
+    pairs = wellSeparatedPairs (4 / eps) t
+      -- TODO: Check the 4!!!
+
+    size (access -> (Sized (Size i) _))  = fromIntegral i
+    repr (access -> (Sized _ (First p))) = p^.core
+
+
+--------------------------------------------------------------------------------
+-- * Helper stuff
+
+-- | Helper to turn the result of 'f k' into 'the expected 'f k', assuming that
+-- we select a set of k points.
 makeExpected         :: (Fractional r, Foldable t) => Int -> t a -> (t a -> r) -> r
 makeExpected k pts f = prb * f pts
   where
     n   = length pts
     prb = ((n - 2) `choose` (k - 2)) / (n `choose` k)
-
-
-
-
 
 
 choose       :: (Integral a, Num b) => a -> a -> b
@@ -40,27 +87,45 @@ n `choose` k = fromIntegral $ fac n' `div` (fac (n'-k') * fac k')
 
     fac z = product [1..z]
 
+-- newtype WSPDMeasured a = WSPDMeasured a
 
--- approxPairwiseDistance         :: (Floating r, Arity d) => r -> [Point d r :+ p] -> r
--- approxPairwiseDistance _   []  = 0
--- approxPairwiseDistance eps pts =
---     sum [ euclideanDist (p^.core) (q^.core) | p <- pts, q <- pts]
---   where
---     n   = length pts
+-- instance Measured (Sized (First a)) (WSPDMeasured a) where
+--   measure (WSPDMeasured p) = Sized 1 (First p)
 
-newtype F a = F a
-instance Semigroup (F a) where
-  l <> _ = l
+-- instance Measured v (WSPDMeasured (Point d r :+ p))
+--            => Measured v (SplitTree d p r v) where
+--   measure (Leaf p)      = measure $ WSPDMeasured p
+--   measure (Node _ nd _) = nd^.nodeData
 
-
-withSizes :: SplitTree d p r a -> SplitTree d p r (Sized (F (Point d r :+ p)))
+-- | Annotate the split tree with sizes
+withSizes :: SplitTree d p r a -> SplitTree d p r (Sized (First (Point d r :+ p)))
 withSizes = foldUp f Leaf
   where
-    f l (NodeData j b _) r = let nd = (measure l)^.nodeData <> (measure r)^.nodeData
+    f l (NodeData j b _) r = let nd = (access l) <> (access r)
                              in Node l (NodeData j b nd) r
 
+-- | Get the measurement for a given splittree
+access               :: BinLeafTree (NodeData d r (Sized (First a))) a -> Sized (First a)
+access (Leaf x)      = Sized 1 (First x)
+access (Node _ nd _) = nd^.nodeData
 
 
--- | Sum of the pairwise distances
-pairwiseDist     :: (Floating r, Arity d) => [Point d r :+ p] -> r
-pairwiseDist pts = sum [ euclideanDist (p^.core) (q^.core) | p <- pts, q <- pts]
+--
+-- | CVS file, in which every line consists of a name, followed by exactly d coordinates
+parseInput :: forall d r. (Arity d, KnownNat d, Read r)
+           => B.ByteString -> [Point d r :+ B.ByteString]
+parseInput = mapMaybe toPoint . drop 1 . C.lines
+  where
+    trim      = fst . C.spanEnd isSpace . C.dropWhile isSpace
+    fromList' = fromList . take (fromInteger . natVal $ (Proxy :: Proxy d))
+
+    toPoint bs = let (n:rs) = map trim . C.split ',' $ bs
+                     p      = fmap Point . fromList' . map (read . C.unpack) $ rs
+                 in (:+ n) <$> p
+
+
+readInput :: (Arity d, KnownNat d, Read r) => FilePath -> IO [Point d r :+ C.ByteString]
+readInput = fmap parseInput . B.readFile
+
+test :: FilePath -> IO [Point 3 Double :+ C.ByteString]
+test = readInput
