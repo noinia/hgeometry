2ddcf591be2832390422972fc741300ae744b69b
Frank Staals <frank@Hina.local>
2017-12-16 22:15:55 +0100

    - Fixed the LowerEnvelopeSpec testcase by checking testing using approximate   equality

--- test/Algorithms/Geometry/LowerEnvelope/LowerEnvSpec.hs
+++ test/Algorithms/Geometry/LowerEnvelope/LowerEnvSpec.hs
@@ -1,21 +1,25 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 module Algorithms.Geometry.LowerEnvelope.LowerEnvSpec where
 
 import qualified Algorithms.Geometry.LowerEnvelope.DualCH as DualCH
 import           Control.Lens
+import           Data.Eq.Approximate
 import           Data.Ext
 import           Data.Geometry
 import           Data.Geometry.Ipe
 import           Data.Geometry.Line
-import qualified Data.List.NonEmpty as NonEmpty
 import           Data.List.NonEmpty (NonEmpty(..))
+import qualified Data.List.NonEmpty as NonEmpty
 import           Data.Maybe (mapMaybe)
 import           Data.Proxy
 import           Data.Ratio
 import           Data.Semigroup
 import           Data.Vinyl.CoRec
+import           GHC.TypeLits
 import           Test.Hspec
 import           Util
+
 -- import Algorithms.Geometry.LowerEnvelope.Types
 
 spec :: Spec
@@ -28,11 +32,13 @@ testCases fp = (runIO $ readInput fp) >>= \case
     Right tcs -> mapM_ toSpec tcs
 
 
+type Approx r = AbsolutelyApproximateValue (Proxy 3) r
+
 
 
 data TestCase r = TestCase { _lines    :: NonEmpty (Line 2 r :+ ())
                            , _color    :: Maybe (IpeColor r)
-                           , _solution :: [Point 2 r]
+                           , _solution :: [Point 2 (Approx r)]
                            }
                   deriving (Show,Eq)
 
@@ -51,7 +57,7 @@ readInput fp = fmap f <$> readSinglePageFile fp
         segs = page^..content.traverse._withAttrs _IpePath _asLineSegment
         pts  = page^..content.traverse._IpeUse
 
-        solutionOf c = [ p^.core.symbolPoint
+        solutionOf c = [ AbsolutelyApproximateValue <$> p^.core.symbolPoint
                        | p <- pts, lookup' p == c
                        ]
 
@@ -60,7 +66,39 @@ readInput fp = fmap f <$> readSinglePageFile fp
 lookup' (_ :+ ats) = lookupAttr (Proxy :: Proxy Stroke) ats
 
 
-
 toSpec                    :: (Fractional r, Ord r, Show r) => TestCase r -> Spec
 toSpec (TestCase ls c sol) = it ("testing the " <> show c <> " set") $
-  (map (^.core) . DualCH.vertices . DualCH.lowerEnvelope $ ls) `shouldBe` sol
+  (map (approx . (^.core))
+   . DualCH.vertices . DualCH.lowerEnvelope $ ls) `shouldBe` sol
+
+
+approx = fmap AbsolutelyApproximateValue
+
+-- shouldApprox       :: forall f r. ( Functor f, Eq (f (Approx r))
+--                                   , Show (f (Approx r))
+--                                   )
+--                    => f r -> f r -> Expectation
+-- a `shouldApprox` b = a' `shouldBe` b'
+--   where
+--     a' :: f (Approx r)
+--     a' = AbsolutelyApproximateValue <$> a
+--     b' :: f (Approx r)
+--     b' = AbsolutelyApproximateValue <$> b
+
+
+
+instance KnownNat n => AbsoluteTolerance (Proxy n) where
+  absoluteToleranceOf = toleranceFromKnownNat . getAbsoluteTolerance
+
+toleranceFromKnownNat   :: (Fractional r, KnownNat n) => proxy n -> r
+toleranceFromKnownNat p = recip . fromInteger $ (10 :: Integer) ^ (natVal p)
+
+instance KnownNat n => RelativeTolerance (Proxy n) where
+  relativeToleranceOf = toleranceFromKnownNat . getRelativeTolerance
+
+instance KnownNat n => ZeroTolerance (Proxy n) where
+  zeroToleranceOf = toleranceFromKnownNat . getZeroTolerance
+
+--     where
+--       f   :: Fractional r => AbsolutelyApproximateValue (Proxy n) r -> r
+--       f _ = recip $ (fromInteger 10) ^^ (natVal (Proxy :: Proxy n))
