b27459e2c00f25782332fa26762fff1dc2e77a29
Frank Staals <frank@Hina.local>
2018-07-07 12:25:49 +0200

    Making sure VectorFamily is as fast as VectorFamilyPeano

--- src/Data/Geometry/Vector/VectorFamily.hs
+++ src/Data/Geometry/Vector/VectorFamily.hs
@@ -36,18 +36,13 @@ type instance V.Dim   (Vector d)   = d
 type instance Index   (Vector d r) = Int
 type instance IxValue (Vector d r) = r
 
-type Arity d = ( ImplicitArity (Peano d)
-               , V.Arity d
-               , Fam.FromPeano (Peano d) ~ d
-               )
+type Arity d = ImplicitArity (Peano d)
 
-instance (Eq r,  Arity d) => Eq  (Vector d r) where
-  (MKVector u) == (MKVector v) = u == v
-  {-# INLINE (==) #-}
+type Arity2 d = (Arity d, Fam.FromPeano (Peano d) ~ d)
 
-instance (Ord r, Arity d) => Ord (Vector d r) where
-  (MKVector u) `compare` (MKVector v) = u `compare` v
-  {-# INLINE compare #-}
+
+deriving instance (Eq r,  Arity d) => Eq  (Vector d r)
+deriving instance (Ord r, Arity d) => Ord (Vector d r)
 
 deriving instance Arity d => Functor     (Vector d)
 deriving instance Arity d => Foldable    (Vector d)
@@ -57,10 +52,10 @@ deriving instance Arity d => Additive (Vector d)
 deriving instance Arity d => Metric (Vector d)
 deriving instance Arity d => Affine (Vector d)
 
-instance Arity d => Ixed (Vector d r) where
+instance Arity2 d => Ixed (Vector d r) where
   ix = element'
 
-instance Arity d => V.Vector (Vector d) r where
+instance Arity2 d => V.Vector (Vector d) r where
   construct  = MKVector <$> V.construct
   inspect    = V.inspect . _unV
   basicIndex = V.basicIndex . _unV
@@ -91,13 +86,13 @@ pattern Vector4 x y z w = (Vector (L4.V4 x y z w))
 
 --------------------------------------------------------------------------------
 
-vectorFromList :: Arity d => [r] -> Maybe (Vector d r)
+vectorFromList :: Arity2 d => [r] -> Maybe (Vector d r)
 vectorFromList = V.fromListM
 
-vectorFromListUnsafe :: Arity d => [r] -> Vector d r
+vectorFromListUnsafe :: Arity2 d => [r] -> Vector d r
 vectorFromListUnsafe = V.fromList
 
-destruct              :: (Arity d, Arity (d + 1))
+destruct              :: (Arity2 d, Arity2 (d + 1))
                       => Vector (d + 1) r -> (r, Vector d r)
 destruct (MKVector v) = MKVector <$> Fam.destruct v
 
@@ -105,14 +100,14 @@ destruct (MKVector v) = MKVector <$> Fam.destruct v
 -- * Indexing vectors
 
 -- | Lens into the i th element
-element   :: forall proxy i d r. (Arity d, Arity i, (i + 1) <= d)
+element   :: forall proxy i d r. (Arity2 d, Arity2 i, (i + 1) <= d)
           => proxy i -> Lens' (Vector d r) r
 element _ = singular . element' . fromInteger $ natVal (C :: C i)
 
 
 -- | Similar to 'element' above. Except that we don't have a static guarantee
 -- that the index is in bounds. Hence, we can only return a Traversal
-element' :: forall d r. Arity d => Int -> Traversal' (Vector d r) r
+element' :: forall d r. Arity2 d => Int -> Traversal' (Vector d r) r
 element' = V.element
 
 
@@ -120,22 +115,22 @@ element' = V.element
 -- * Snoccing and consindg
 
 -- | Add an element at the back of the vector
-snoc :: (Arity (d + 1), Arity d
+snoc :: (Arity2 (d + 1), Arity2 d
         ) => Vector d r -> r -> Vector (d + 1) r
 snoc = flip V.snoc
 
 -- | Get a vector of the first d - 1 elements.
-init :: (Arity d, Arity (d + 1)) => Vector (d + 1) r -> Vector d r
+init :: (Arity2 d, Arity2 (d + 1)) => Vector (d + 1) r -> Vector d r
 init = V.reverse . V.tail . V.reverse
 
-last :: forall d r. (Arity d, Arity (d + 1)) => Vector (d + 1) r -> r
+last :: forall d r. (Arity2 d, Arity2 (d + 1)) => Vector (d + 1) r -> r
 last = view $ element (C :: C d)
 
 -- | Get a prefix of i elements of a vector
-prefix :: forall i d r. (Arity d, Arity i, i <= d)
+prefix :: forall i d r. (Arity d, Arity2 i, i <= d)
        => Vector d r -> Vector i r
 prefix = let i = fromInteger . natVal $ (C :: C i)
-         in vectorFromListUnsafe . take i . V.toList
+         in vectorFromListUnsafe . take i . F.toList
 
 --------------------------------------------------------------------------------
 -- * Specific on 3-dimensional vectors
