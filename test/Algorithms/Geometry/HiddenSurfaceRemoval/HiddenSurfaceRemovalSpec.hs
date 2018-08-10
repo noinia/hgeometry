565c6a3a099b85626d72b34735c729f476a3fe48
Frank Staals <frank@Hina.local>
2018-08-10 13:13:41 +0200

    Missing spec files (although they don't really do much yet)

---
+++ test/Algorithms/Geometry/HiddenSurfaceRemoval/HiddenSurfaceRemovalSpec.hs
@@ -0,0 +1,19 @@
+module Algorithms.Geometry.HiddenSurfaceRemoval.HiddenSurfaceRemovalSpec where
+
+import Algorithms.Geometry.HiddenSurfaceRemoval.HiddenSurfaceRemoval
+
+
+import           Data.Geometry.Point
+import           Data.Geometry.Triangle
+import           Data.Geometry.Arrangement
+import           Control.Lens
+import           Data.Ext
+import           Test.Hspec
+
+
+spec :: Spec
+spec = pure ()
+  -- do
+  --   describe "HiddenSurfaceremoval tests" $ do
+  --     it "triangle order" $ do
+  --       1 `shouldBe` 1
