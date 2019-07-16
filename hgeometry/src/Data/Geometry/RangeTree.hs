6d49a5a58e9873135650a28708600c236720be55
Frank Staals <frank@fstaals.net>
2019-07-16 13:30:01 +0200

    working on the multidimensional rangetree

--- hgeometry/src/Data/Geometry/RangeTree.hs
+++ hgeometry/src/Data/Geometry/RangeTree.hs
@@ -1,10 +1,14 @@
+{-# LANGUAGE UndecidableInstances #-}
 module Data.Geometry.RangeTree where
 
 import           Control.Lens
 import           Data.BinaryTree
 import           Data.Ext
+import qualified Data.Foldable as F
 import           Data.Geometry.Point
 import           Data.Geometry.Properties
+import qualified Data.Geometry.RangeTree.Generic as GRT
+import           Data.Geometry.Vector
 import           Data.List.NonEmpty (NonEmpty(..))
 import qualified Data.List.NonEmpty as NonEmpty
 import           Data.Range
@@ -12,141 +16,143 @@ import           Data.Semigroup
 import           Data.Semigroup.Foldable
 import qualified Data.Set as Set
 import           Data.Util
-
---------------------------------------------------------------------------------
-
-data NodeData v r = NodeData { _minVal     :: !(Min r)
-                             , _maxVal     :: !(Max r)
-                             , _assoc      :: !v
-                             } deriving (Show,Eq,Functor)
-
-instance (Semigroup v, Ord r) => Semigroup (NodeData v r) where
-  NodeData mi ma v <> NodeData mi' ma' v' = NodeData (mi <> mi') (ma <> ma') (v <> v')
-
-
--- | A generic range tree.
-newtype RangeTree v q r =
-  RangeTree { _unRangeTree :: BinLeafTree (NodeData v r) (NodeData (v,q) r) }
-    deriving (Show,Eq)
-
+import           GHC.TypeLits
 
 
 --------------------------------------------------------------------------------
 
--- | Creates a range tree
-createTree   :: ( Ord r
-                , Measured v p
-                , Semigroup p
-                )
-             => NonEmpty (r :+ p)
-             -> RangeTree v p r
-createTree = createTree'
-           . fmap (\pts -> let x =  pts^.to NonEmpty.head.core
-                           in x :+ (sconcat . fmap (^.extra) $ pts))
-           . NonEmpty.groupAllWith1 (^.core) -- sort and group on r value
+type RangeTree1D d v p r =  GRT.RangeTree (v (Point d r :+ p)) (Leaf1 d v p r) r
 
--- | pre: input is sorted and grouped by x-coord
-createTree'     :: (Ord r, Measured v p)
-                => NonEmpty (r :+ p)
-                -> RangeTree v p r
-createTree' pts = RangeTree t
+newtype GLeaf1 c e = Leaf1 [c :+ e]
+  deriving (Show,Eq,Ord,Semigroup,Monoid)
+
+
+type Leaf1 d v p r = GLeaf1 (Point d r) p
+
+--------------------------------------------------------------------------------
+
+class MeasuredRT v where
+  measureRT :: [a] -> v a
+
+--------------------------------------------------------------------------------
+
+instance (MeasuredRT v, Semigroup (v (Point d r :+ p))
+         ) => Measured (v (Point d r :+ p)) (Leaf1 d v p r) where
+  measure (Leaf1 pts) = measureRT pts
+
+
+create1DTree :: (Ord r, Measured (v (Point d r :+ p)) (Leaf1 d v p r))
+             => NonEmpty (r :+ (Point d r :+ p)) -> RangeTree1D d v p r
+create1DTree = GRT.createTree . fmap (&extra %~ Leaf1 . (:[]))
+
+--------------------------------------------------------------------------------
+
+newtype Assoc2 d v p r = Assoc { getAssoc :: Maybe (RangeTree1D d v p r) }
+
+
+
+deriving instance (Show (v (Point d r :+ p)), Show r, Show p, Arity d) => Show (Assoc2 d v p r)
+
+
+-- | Creates an associated DS from a pre-sorted list of points
+createAssoc' :: (Ord r, MeasuredRT v, Semigroup (v (Point d r :+ p)))
+            => [r :+ Leaf1 d v p r] -> Assoc2 d v p r
+createAssoc' = Assoc . fmap GRT.createTree' . NonEmpty.nonEmpty
+
+createAssoc :: (Ord r, MeasuredRT v, Semigroup (v (Point d r :+ p)))
+            => [r :+ (Point d r :+ p)] -> Assoc2 d v p r
+createAssoc = Assoc . fmap create1DTree . NonEmpty.nonEmpty
+
+
+instance (Ord r, Arity d, MeasuredRT v, Semigroup (v (Point d r :+ p)))
+         => Monoid (Assoc2 d v p r) where
+  mempty = Assoc Nothing
+
+instance ( Ord r, Arity d, MeasuredRT v, Semigroup (v (Point d r :+ p))
+         ) => Semigroup (Assoc2 d v p r) where
+  (Assoc l) <> (Assoc r) = createAssoc' $ merge (toList l) (toList r)
+    where
+      toList = maybe [] (F.toList . GRT.toAscList)
+
+merge :: (Ord r, Semigroup ps) => [r :+ ps] -> [r :+ ps] -> [r :+ ps]
+merge = go
   where
-    t = view _1
-      . foldUp (\(SP l dl) _ (SP r dr) -> let d = dl <> dr in SP (Node l d r) d
-               )
-               (\(Elem (x :+ ld)) -> let v = measure ld
-                                     in SP (Leaf $ NodeData (Min x) (Max x) (v,ld))
-                                           (NodeData (Min x) (Max x) v)
-               )
-      . asBalancedBinLeafTree $ pts
-
---------------------------------------------------------------------------------
--- * Querying x
-
--- | Range search
---
--- running time: \(O(\log n)\)
-search    :: (Ord r, Monoid v) => Range r -> RangeTree v p r -> v
-search qr = mconcat . search' qr
-
--- | Range search, report the (associated data structures of the)
--- \(O(\log n)\) nodes that form the disjoint union of the range we
--- are querying with.
---
--- running time: \(O(\log n)\)
-search'    :: Ord r
-           => Range r -> RangeTree v p r -> [v]
-search' qr = search'' qr . _unRangeTree
-
--- | The actual search
-search''    :: Ord r
-            => Range r
-            -> BinLeafTree (NodeData v r) (NodeData (v,q) r)
-            -> [v]
-search'' qr t = case t of
-    Leaf (NodeData _ _ (v,_)) | qr `covers` rangeOf t -> [v]
-                              | otherwise             -> []
-    Node l (NodeData _ _ v) r | qr `covers` rangeOf t -> [v]
-                              | otherwise             -> msearch l <> msearch r
-  where
-    msearch t' | qr `intersects` rangeOf t' = search'' qr t'
-               | otherwise                  = []
+    go []         bs = bs
+    go as         [] = as
+    go as@(a:as') bs@(b:bs') = case (a^.core) `compare` (b^.core) of
+       LT -> a : go as' bs
+       GT -> b : go as  bs'
+       EQ -> (a^.core :+ (a^.extra) <> (b^.extra)) : go as' bs'
 
 
--- | Helper function to get the range of  a binary leaf tree
-rangeOf              :: BinLeafTree (NodeData v r) (NodeData v' r) -> Range r
-rangeOf (Leaf d)     = rangeOf' d
-rangeOf (Node _ d _) = rangeOf' d
-
--- | Get the range of a node
-rangeOf'                                :: NodeData v r -> Range r
-rangeOf' (NodeData (Min mi) (Max ma) _) = ClosedRange mi ma
 
 
---------------------------------------------------------------------------------
--- * Updates
 
--- support inserting and deleting points, assuming that the x-coord already exists.
+
+type RangeTree2D d v p r = GRT.RangeTree (Assoc2 d v p r) (Leaf2 d v p r) r
+
+newtype GLeaf2 c e = Leaf2 (GLeaf1 c e) deriving (Show,Eq,Ord,Semigroup,Monoid)
+
+type Leaf2 d v p r = GLeaf2 (Point d r) p
+
+instance ( MeasuredRT v, Semigroup (v (Point d r :+ p)), 2 <= d, Arity d, Ord r
+         ) => Measured (Assoc2 d v p r) (Leaf2 d v p r) where
+  measure (Leaf2 (Leaf1 pts)) = createAssoc . map (\p -> p^.core.yCoord :+ p) $ pts
+
+
+create2DTree :: (Ord r, MeasuredRT v, Semigroup (v (Point d r :+ p)), Arity d, 2 <= d, 1 <= d)
+             => NonEmpty (Point d r :+ p) -> RangeTree2D d v p r
+create2DTree = GRT.createTree . fmap (\p -> (p^.core.xCoord) :+ Leaf2 (Leaf1 [p]))
+
+search   :: (Ord r, Monoid (v (Point d r :+ p)))
+         => Vector 2 (Range r) -> RangeTree2D d v p r -> v (Point d r :+ p)
+search r = mconcat . search' r
+
+search'                 :: Ord r
+                        => Vector 2 (Range r) -> RangeTree2D d v p r -> [v (Point d r :+ p)]
+search' (Vector2 xr yr) = concatMap (queryAssoc yr) . GRT.search' xr
+
+queryAssoc   :: Ord r => Range r -> Assoc2 d v p r -> [v (Point d r :+ p)]
+queryAssoc r = maybe [] (GRT.search' r) . getAssoc
+
+
+-- newtype RangeTree (d :: Nat) v p r = RangeTree (GRT.RangeTree v p r)
+
+
+
+-- newtype Assoc v = Assoc v
+
+
+
+-- newtype Leaf = LeafD () r
+
+-- newtype AssocD v = AssocD (RangeTree (d - 1) v p r)
 
 
 --------------------------------------------------------------------------------
+instance MeasuredRT GRT.Report where
+  measureRT = GRT.Report
+--------------------------------------------------------------------------------
 
-newtype Report p = Report { reportList :: [p] }
-  deriving (Show,Eq,Ord,Functor,Foldable,Semigroup,Monoid)
+newtype Count a = Count Int
 
-instance Measured (Report p) (Report p) where
-  measure = id
+instance MeasuredRT Count where
+  measureRT = Count . length
 
-createReportingTree :: Ord r => NonEmpty (r :+ [p]) -> RangeTree (Report p) (Report p) r
-createReportingTree = createTree . fmap (&extra %~ Report)
+--------------------------------------------------------------------------------
 
-report    :: (Ord r) => Range r -> RangeTree (Report p) q r -> [p]
-report qr = reportList . search qr
+data And l r a = And (l a) (r a) deriving (Show,Eq,Ord)
 
+type (:*:) l r = And l r
 
-----------------------------------------
+instance (MeasuredRT l, MeasuredRT r) => MeasuredRT (l :*: r) where
+  measureRT xs = And (measureRT xs) (measureRT xs)
 
-newtype Count = Count { getCount :: Int }
-  deriving (Show,Eq,Ord)
-
-instance Semigroup Count where
-  (Count a) <> (Count b) = Count $ a + b
-
-instance Monoid Count where
-  mempty = Count 0
-
-newtype CountOf p = CountOf [p]
-  deriving (Show,Eq,Ord,Functor,Foldable,Semigroup,Monoid)
+instance (Semigroup (l a), Semigroup (r a)) => Semigroup ((l :*: r) a) where
+  (And l r) <> (And l' r') = And (l <> l') (r <> r')
 
 
-instance Measured Count (CountOf p) where
-  measure (CountOf xs) = Count $ length xs
+-- newtype All (ls :: [* -> *]) a = All (Map ls a)
 
-
-createCountingTree :: Ord r => NonEmpty (r :+ [p]) -> RangeTree Count (CountOf p) r
-createCountingTree = createTree . fmap (&extra %~ CountOf)
-
--- | Perform a counting query
---
-count    :: Ord r => Range r -> RangeTree Count p r -> Int
-count qr = getCount . search qr
+-- type family Map (ls :: [* -> *]) (a :: *) where
+--   Map '[]
