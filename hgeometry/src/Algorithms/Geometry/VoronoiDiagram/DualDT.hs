651963d3ef49743f9ca0de1f737fa40082263fd6
Frank Staals <frank@fstaals.net>
2019-10-21 20:54:57 +0200

    some more work on computing a Voronoi diagram

--- hgeometry/src/Algorithms/Geometry/VoronoiDiagram/DualDT.hs
+++ hgeometry/src/Algorithms/Geometry/VoronoiDiagram/DualDT.hs
@@ -2,24 +2,39 @@
 module Algorithms.Geometry.VoronoiDiagram.DualDT where
 
 
-import Control.Lens
-import           Algorithms.Geometry.DelaunayTriangulation.DivideAndConqueror
+import           Algorithms.Geometry.DelaunayTriangulation.DivideAndConquer
 import           Algorithms.Geometry.DelaunayTriangulation.Types
+import           Control.Lens
 import           Data.Ext
-import           Data.Geometry.Point
 import           Data.Geometry.Ball
+import           Data.Geometry.Box
+import           Data.Geometry.Point
+import           Data.Geometry.Vector
+import           Data.Geometry.HalfLine
+import           Data.Geometry.PlanarSubdivision
+import           Data.Geometry.Point
 import qualified Data.List.NonEmpty as NonEmpty
-import           Data.PlanarGraph
-import           Data.PlaneGraph
+import           Data.PlanarGraph(FaceId)
+import           Data.PlaneGraph(PlaneGraph)
+import qualified Data.PlaneGraph as PG
 import           Data.Proxy
 import qualified Data.Vector as V
 
 --------------------------------------------------------------------------------
 
+type UnboundedVoronoiEdges s e r = V.Vector (VertexId' s, HalfLine 2 r :+ e)
 
 
+data VertexType v = BoundingBoxVertex | VoronoiVertex v deriving (Show,Eq,Ord)
 
-type VoronoiDiagram s r p = PlaneGraph s Primal_ () () (Point 2 r :+ p) r
+
+data VoronoiDiagram s v e f r =
+  VoronoiDiagram { _boundedDiagram         :: PlanarSubdivision s (VertexType v) e f r
+                 , _boundedArea            :: Rectangle () r
+                 , _unboundedIntersections :: UnboundedVoronoiEdges s e r
+                 } deriving (Show,Eq)
+
+
 
 
 
@@ -28,27 +43,63 @@ type VoronoiDiagram s r p = PlaneGraph s Primal_ () () (Point 2 r :+ p) r
 
 voronoiDiagram        :: (Ord r, Fractional r)
                       => proxy s -> NonEmpty.NonEmpty (Point 2 r :+ p)
-                      -> PlanarGraph s Dual_ (Maybe (Point 2 r :+ ()))
-                                             ()
-                                             (Point 2 r :+ p)
-                         --VoronoiDiagram s r p
-voronoiDiagram px pts = undefined
+                      -> VoronoiDiagram s () () p r
+voronoiDiagram px pts = VoronoiDiagram diag bBox unb'
   where
+    oid = PG.outerFaceId dt
+
     -- dt' :: PlaneGraph s Primal_ p () () r
     dt' = toPlaneGraph px $ delaunayTriangulation pts
-    dt  = dt'&faceData .~ (fmap (toVDVertex dt') . faces' $ dt')
-    vd  = dual dt
+    dt  = dt'&PG.faceData .~ (fmap (toVDVertex dt') . PG.faces' $ dt')
+
+    bBox = grow 1 . boundingBoxList' . V.mapMaybe snd . PG.faces $ dt
+
+    diag = undefined
+
+    unb = unBoundedEdge dt <$> PG.boundary oid dt
+         -- this gives the unboundededges from their actual voronoi vertices
+    unb' = undefined
+
+
+-- | Computes the unbounded edge corresponding to this dart of the
+-- convex hull.
+--
+-- running time: \(O(1)\)
+unBoundedEdge      :: Fractional r
+                   => PlaneGraph s v e (Maybe (Point 2 r)) r -- ^ the delaunaytriangulation
+                   -> Dart s
+                   -> (FaceId' s, HalfLine 2 r :+ ())
+unBoundedEdge dt d = let (p,q)  = over both (^.location) $ dt^.PG.endPointsOf d
+                         fi     = PG.leftFace d dt
+                         Just v = dt^.dataOf fi
+                           -- the face to the left of this dart should be a triangle
+                           -- and thus have a
+                     in (fi, ext $ unboundedEdgeFrom v p q)
+
+
+-- | Given an edge of the convex hull (specifiekd by two adjacent
+-- vertices) and the vertex in the Voronoi diagram that is incident to
+-- these two sites, computes the halfine representing their unbouded
+-- edge of the VD.
+unboundedEdgeFrom       :: Fractional r
+                        => Point 2 r -- ^ The starting point of the unbounded edge
+                        -> Point 2 r -- ^ vertex of the CH
+                        -> Point 2 r -- ^ adjacent vertex of the CH
+                        -> HalfLine 2 r
+unboundedEdgeFrom v p q = HalfLine v (midPoint p q .-. v)
+  where
+    midPoint a b = a .+^ ((b^.vector) ^/ 2)
 
 
 
 
 
 toVDVertex        :: (Fractional r, Eq r)
-                  => PlaneGraph s w v e f r -> FaceId s w
-                  -> Maybe (Point 2 r :+ ())
+                  => PlaneGraph s v e f r -> FaceId' s
+                  -> Maybe (Point 2 r)
 toVDVertex dt fi
   | V.length bvs /= 3 = Nothing
-  | otherwise         = disk a b c ^?_Just.center
+  | otherwise         = disk a b c ^?_Just.center.core
     where
-      bvs     = boundaryVertices fi dt
-      [a,b,c] = map (\v -> dt^.dataOf v.core) . V.toList $ bvs
+      bvs     = PG.boundaryVertices fi dt
+      [a,b,c] = map (\v -> dt^.PG.locationOf v) . V.toList $ bvs
