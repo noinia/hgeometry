66e3ffaf15799cef87a94bf03375e82d97cb2bde
Frank Staals <frank@fstaals.net>
2020-01-12 12:43:55 +0100

    fiddling with triangulateworld

--- hgeometry-examples/src/Demo/TriangulateWorld.hs
+++ hgeometry-examples/src/Demo/TriangulateWorld.hs
@@ -1,20 +1,21 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 module Demo.TriangulateWorld where
 
-import Algorithms.Geometry.LineSegmentIntersection (hasSelfIntersections)
-import Algorithms.Geometry.PolygonTriangulation.Triangulate (triangulate)
-import Algorithms.Geometry.PolygonTriangulation.MakeMonotone (makeMonotone)
-import Data.Maybe(mapMaybe)
-import Control.Lens
-import Data.Data
-import Data.Ext
-import Data.Geometry.Ipe
-import Data.Geometry.Polygon
-import Data.Geometry.PlanarSubdivision
-import Data.Semigroup
-import Options.Applicative
+import           Algorithms.Geometry.LineSegmentIntersection (hasSelfIntersections)
+import           Algorithms.Geometry.LineSegmentIntersection.BentleyOttmann (interiorIntersections)
+import           Algorithms.Geometry.PolygonTriangulation.MakeMonotone (makeMonotone)
+import           Algorithms.Geometry.PolygonTriangulation.Triangulate (triangulate)
+import           Control.Lens
+import           Data.Data
+import           Data.Ext
 import qualified Data.Foldable as F
-
+import           Data.Geometry.Ipe
+import           Data.Geometry.PlanarSubdivision
+import           Data.Geometry.Polygon
+import qualified Data.Map as Map
+import           Data.Maybe (mapMaybe)
+import           Data.Semigroup
+import           Options.Applicative
 
 --------------------------------------------------------------------------------
 
@@ -47,10 +48,12 @@ mainWith (Options inFile outFile) = do
       Right (page :: IpePage Rational) -> runPage page
   where
     runPage page = do
-      let polies  = page^..content.to flattenGroups.traverse._withAttrs _IpePath _asMultiPolygon
+      let polies  = page^..content.to flattenGroups.traverse._withAttrs _IpePath _asSimplePolygon
           polies' = filter (not . hasSelfIntersections . (^.core)) polies
+          intersections' = concatMap (Map.keys . interiorIntersections
+                                      . listEdges . (^.core)) polies
           subdivs = map (\(pg :+ _) -> triangulate (Identity PX) pg) polies'
-          yMonotones = tail . mapMaybe (^?_2.core._Left)
+          yMonotones = mapMaybe (^?_2.core._Left)
                      . concatMap (F.toList.rawFacePolygons) $ subdivs
           ofs = map (\s -> rawFaceBoundary (outerFaceId s) s) subdivs
           segs    = map (^._2.core) . concatMap (F.toList . edgeSegments) $ subdivs
@@ -58,6 +61,12 @@ mainWith (Options inFile outFile) = do
                             , [ iO' s  | s  <- segs ]
                             , [ iO' pg | pg <- yMonotones ]
                             ]
+      putStrLn $ "#polygons found: " <> show (length polies)
+
+      putStrLn $ "first <=100 self-intersections: "
+      mapM_ print $ take 100 intersections'
+      putStrLn $ "number of non-self intersecting polygons: " <> show (length polies')
+
       mapM_ print . map (\pg -> pg^.core.to polygonVertices.to length) $ polies'
       writeIpeFile outFile . singlePageFromContent $ out
 
