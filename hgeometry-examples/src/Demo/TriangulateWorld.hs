0aea5794b7a52b2ceec2febcd1ff005fd5130e33
Frank Staals <frank@fstaals.net>
2020-01-13 21:04:59 +0100

    some cleaning up

--- hgeometry-examples/src/Demo/TriangulateWorld.hs
+++ hgeometry-examples/src/Demo/TriangulateWorld.hs
@@ -53,13 +53,13 @@ mainWith (Options inFile outFile) = do
           intersections' = concatMap (Map.keys . interiorIntersections
                                       . listEdges . (^.core)) polies
           subdivs = map (\(pg :+ _) -> triangulate (Identity PX) pg) polies'
-          yMonotones = mapMaybe (^?_2.core._Left)
+          triangles' = mapMaybe (^?_2.core._Left)
                      . concatMap (F.toList.rawFacePolygons) $ subdivs
           ofs = map (\s -> rawFaceBoundary (outerFaceId s) s) subdivs
           segs    = map (^._2.core) . concatMap (F.toList . edgeSegments) $ subdivs
           out     = mconcat [ [ iO' pg | pg <- polies ]
                             , [ iO' s  | s  <- segs ]
-                            , [ iO' pg | pg <- yMonotones ]
+                            , [ iO' pg | pg <- triangles' ]
                             ]
       putStrLn $ "#polygons found: " <> show (length polies)
 
@@ -68,42 +68,7 @@ mainWith (Options inFile outFile) = do
       putStrLn $ "number of non-self intersecting polygons: " <> show (length polies')
 
       mapM_ print . map (\pg -> pg^.core.to polygonVertices.to length) $ polies'
-      writeIpeFile outFile . singlePageFromContent $ out
 
-
--- mainWith                          :: Options -> IO ()
--- mainWith (Options inFile outFile) = do
---     ePage <- readSinglePageFile inFile
---     case ePage of
---       Left err                         -> print err
---       Right (page :: IpePage Rational) -> runPage page
---   where
---     runPage page = do
---       let orig = page^.content
---           all' = page^.content.to flattenGroups
---       writeIpeFile outFile . singlePageFromContent $ orig <> all'
-
-
--- type ValT = EitherT IO
-
--- flattenGroups :: [IpeObject r] -> [IpeObject r]
--- flattenGroups = concatMap flattenGroups'
-
--- flattenGroups'                              :: IpeObject r -> [IpeObject r]
--- flattenGroups' (IpeGroup (Group gs :+ ats)) =
---       map (applyAts ats) . concatMap flattenGroups' $ gs
---     where
---       applyAts ats = id
--- flattenGroups' o                            = [o]
-
--- runExcept'   :: (Show e) => ExceptT e IO () -> IO ()
--- runExcept' m = runExceptT m >>= \case
---                 Left e   -> print e
---                 Right () -> pure ()
-
--- mainWith                          :: Options -> IO ()
--- mainWith (Options inFile outFile) = runExcept' $ do
---     (page :: IpePage Rational) <- readSinglePageFile inFile
---     let polies = page^..content.traverse._withAttrs _IpePath _asSimplePolygon
---     let out = undefinedL
---     lift $  writeIpeFile outFile . singlePageFromContent $ out
+      putStrLn "# triangles: "
+      print (length $ triangles')
+      -- writeIpeFile outFile . singlePageFromContent $ out
