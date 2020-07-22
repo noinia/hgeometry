83e8623e6d3df10b37a8a47d3224c730911fa51e
Frank Staals <frank@fstaals.net>
2020-07-22 09:54:49 +0200

    partial work on sketchy rendering

--- hgeometry-ipe/src/Data/Geometry/Ipe/Sketchy.hs
+++ hgeometry-ipe/src/Data/Geometry/Ipe/Sketchy.hs
@@ -1,25 +1,56 @@
+{-# LANGUAGE TemplateHaskell #-}
+--------------------------------------------------------------------------------
+-- |
+-- Module      :  Data.Geometry.Ipe.Sketchy
+-- Copyright   :  (C) Frank Staals
+-- License     :  see the LICENSE file
+-- Maintainer  :  Frank Staals
+--
+-- Sketchy style rendering of primitives based on
+--
+-- <https://openaccess.city.ac.uk/id/eprint/1274/1/wood_sketchy_2012.pdf Sketchy Rendering for Information Visualization>
+-- by Jo Wood,Member, Petra Isenberg, Tobias Isenberg,,Jason Dykes,
+-- Nadia Boukhelifa and Aidan Slingsby,
+--
+--
+--------------------------------------------------------------------------------
 module Data.Geometry.Ipe.Sketchy where
 
-import Control.Monad.Random.Class
-import Data.Ext
-import Data.Geometry.CatmulRomSpline
-import Data.Geometry.LineSegment
-import Data.Geometry.Point
-import Data.Geometry.Vector
-import System.Random (Random(..))
+import           Control.Lens
+import           Control.Monad.Random.Class
+import           Data.Ext
+import           Data.Geometry.CatmulRomSpline (Spline)
+import qualified Data.Geometry.CatmulRomSpline as Spline
+import           Data.Geometry.LineSegment
+import           Data.Geometry.Point
+import           Data.Geometry.Polygon
+import           Data.Geometry.Vector
+import           Data.List.NonEmpty (NonEmpty(..))
+import qualified Data.List.NonEmpty as NonEmpty
+import           Data.Util
+import           System.Random (Random(..))
 
-import Control.Monad.Random
+import           Control.Monad.Random
 
 --------------------------------------------------------------------------------
+-- * Rendering Line Segments
 
-sketch                                      :: (MonadRandom m, Fractional r, Random r, Ord r)
-                                            => r -> LineSegment 2 p r -> m (Spline 2 r)
-sketch r s@(LineSegment' (p :+ _) (q :+ _)) =
-    fromPoints <$> perturb r p <*> a <*> b  <*> perturb r q
+-- | Renders a line segment using a Sketchy style.
+--
+-- as in the paper, we render a segment using two strokes.
+sketch     :: (MonadRandom m, Floating r, Random r, Ord r)
+           => r -> LineSegment 2 p r -> m (Two (Spline 2 r))
+sketch r s = Two <$> sketchSegment r s <*> sketchSegment r s
+
+-- | Renders a line segment in a 'sketchy' style using a Catmul-rom spline.
+sketchSegment                                      :: (MonadRandom m, Floating r, Random r, Ord r)
+                                                   => r -> LineSegment 2 p r -> m (Spline 2 r)
+sketchSegment r s@(LineSegment' (p :+ _) (q :+ _)) =
+    Spline.fromPoints <$> perturb r p <*> a <*> b  <*> perturb r q
   where
-    v@(Vector2 vx vy) = q .-. p
-    u = Vector2 (-vy) vx  -- perpendicular vector to v
-    l = quadrance v
+    v = q .-. p
+    u = signorm . perp $ v  -- perpendicular vector to v
+
 
     -- offset for the midpoint perpendicular to the segment by (1/200)^th of the length
     da = let d = 1 / 200 in (*^ u) <$> getRandomR (-d,d)
@@ -28,13 +59,49 @@ sketch r s@(LineSegment' (p :+ _) (q :+ _)) =
     -- direction along the segment.
     db = let d = 1 / 10
          in (\du dv -> du *^ u ^+^ dv *^ v) <$> getRandomR (-r,r) <*> getRandomR (-d,d)
-    -- TODO: Normalize the du w.r. u; i.e. if u is a unit vector then this is fine, but
-    -- now the scaling w.r.t u is way too big.
 
     a = (interpolate (1/2) s .+^) <$> da
     b = (interpolate (3/4) s .+^) <$> db
 
+--------------------------------------------------------------------------------
+-- * Rendering Arcs
 
+
+-- sketchArc :: r ->
+
+--------------------------------------------------------------------------------
+-- * Hachures
+
+data HachureSettings r = HachureSettings { _direction :: Vector 2 r
+                                         -- ^ Direction of the hachures
+                                         , _distance  :: r
+                                         -- ^ distance between the hachures
+                                         }
+                       deriving (Show,Eq)
+makeLenses ''HachureSettings
+
+-- | Compute Hachures for a polygon
+hachure          :: (MonadRandom m, Floating r, Random r, Ord r)
+                 => r -> HachureSettings r -> Polygon t p r -> m (NonEmpty (Spline 2 r))
+hachure r hs pg = undefined
+  -- run a sweep, three types of events: insert segment, delete segment, create hachure
+
+
+
+
+data Event r = Insert (LineSegment 2 () r)
+             | Delete (LineSegment 2 () r)
+             | Hachure
+             deriving (Show,Eq)
+
+--------------------------------------------------------------------------------
+-- * Helper Functions
+
+-- | Perpendicular vector
+perp                  :: Num r => Vector 2 r -> Vector 2 r
+perp (Vector2 vx vy) = Vector2 (-vy) vx
+
+-- | Perturbs a point
 perturb     :: (MonadRandom m, Random r, Num r, Ord r, Arity d) => r -> Point d r -> m (Point d r)
 perturb r p = (p .+^) <$> getPertubation r
 
