c5acbbbe83bbdc77817e0f93b3a1863adecda9ee
mloffler <m.loffler@uu.nl>
2020-10-30 13:47:24 +0100

    added some basic 2D transformations

--- hgeometry/src/Data/Geometry/Transformation.hs
+++ hgeometry/src/Data/Geometry/Transformation.hs
@@ -142,3 +142,33 @@ skewX        :: Num r => r -> Transformation 2 r
 skewX lambda = Transformation . Matrix $ Vector3 (Vector3 1 lambda 0)
                                                  (Vector3 0 1      0)
                                                  (Vector3 0 0      1)
+
+-- | Create an identity transformation.
+identity :: Transformation 2 Float
+identity = Transformation . Matrix $ Vector3 (Vector3 1 0 0)
+                                             (Vector3 0 1 0)
+                                             (Vector3 0 0 1)
+
+-- | Create a matrix that corresponds to a rotation by 'a' radians counter-clockwise 
+--   around the origin.
+rotation :: Float -> Transformation 2 Float
+rotation a = Transformation . Matrix $ Vector3 (Vector3 (cos a) (- sin a) 0)
+                                               (Vector3 (sin a) (  cos a) 0)
+                                               (Vector3 0       0         1)
+
+-- | Create a matrix that corresponds to a reflection in a line through the origin
+--   which makes an angle of 'a' radians with the positive 'x'-asis, in counter-clockwise
+--   orientation.
+reflection :: Float -> Transformation 2 Float
+reflection a = rotation a |.| reflectionV |.| rotation (-a)
+
+reflectionV :: Transformation 2 Float
+reflectionV = Transformation . Matrix $ Vector3 (Vector3 1   0  0)
+                                                (Vector3 0 (-1) 0)
+                                                (Vector3 0   0  1)
+
+reflectionH :: Transformation 2 Float
+reflectionH = Transformation . Matrix $ Vector3 (Vector3 (-1) 0  0)
+                                                (Vector3   0  1  0)
+                                                (Vector3   0  0  1)
+
