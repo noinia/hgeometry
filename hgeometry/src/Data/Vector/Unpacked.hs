e24ac4167945d5e19a94cda1ed8ed18df40b1d7b
David Himmelstrup <lemmih@gmail.com>
2021-01-03 09:53:44 +0800

    Switch to abstract representation which is easier to optimize.

--- hgeometry/src/Data/Vector/Unpacked.hs
+++ hgeometry/src/Data/Vector/Unpacked.hs
@@ -1,53 +1,69 @@
+{-# LANGUAGE BangPatterns      #-}
 {-# LANGUAGE DefaultSignatures #-}
 {-# LANGUAGE DeriveAnyClass    #-}
 {-# LANGUAGE InstanceSigs      #-}
 {-# LANGUAGE TypeFamilies      #-}
 module Data.Vector.Unpacked where
 
-import Control.Monad.Primitive (PrimMonad, PrimState)
-import Data.Coerce             (coerce)
--- import           Data.Ratio
+import           Control.DeepSeq
+import           Control.Monad.Identity
+import           Control.Monad.Primitive (PrimMonad, PrimState)
 import           Data.Tree
-import qualified Data.Vector         as V
-import qualified Data.Vector.Generic as G
-import qualified Data.Vector.Unboxed as VU
+import qualified Data.Vector             as V
+import qualified Data.Vector.Generic     as G
+import qualified Data.Vector.Unboxed     as VU
 import           GHC.Real
 
-import           Data.Vector.Unpacked.Mutable (NoPacking, Unpack)
+import           Data.Vector.Unpacked.Mutable (Left, R, Rep (..), Right, Unpack)
 import qualified Data.Vector.Unpacked.Mutable as Mutable
 
-newtype Vector a = Vector (Unpacked (Mutable.S a))
+import Data.Geometry.Point
+-- import GHC.DataSize
+import Linear.V2
+
+newtype Vector a = Vector { unVector :: Unpacked a (R a) }
 
 instance (Unpack a, Show a) => Show (Vector a) where
   show = show . G.toList
 
 type instance G.Mutable Vector = Mutable.MVector
-type instance G.Mutable Unpacked = Mutable.Unpacked
+-- type instance G.Mutable Unpacked = Mutable.Unpacked
 
-data Unpacked a where
-  Rational :: !(V.Vector Integer) -> !(V.Vector Integer) -> Unpacked Rational
-  Tuple    :: !(Unpacked a) -> !(Unpacked b) -> Unpacked (a,b)
-  Unboxed  :: VU.Unbox a => !(VU.Vector a) -> Unpacked a
-  Boxed    :: !(V.Vector a) -> Unpacked (NoPacking a)
+data Unpacked a (r::Rep) where
+  Rational :: !(V.Vector Integer) -> !(V.Vector Integer) -> Unpacked Rational RatRep
+  Tuple    :: !(Unpacked (Left (a,b)) (R (Left (a,b)))) ->
+              !(Unpacked (Right (a,b)) (R (Right (a,b)))) ->
+              Unpacked (a,b) (TRep (R a) (R b))
+  Unboxed  :: VU.Unbox a => !(VU.Vector a) -> Unpacked a URep
+  Boxed    :: !(V.Vector a) -> Unpacked a BRep
 
 -- instance VU.Unbox (MVector s a)
 instance (Unpack a) => G.Vector Vector a where
   basicUnsafeFreeze :: PrimMonad m => Mutable.MVector (PrimState m) a -> m (Vector a)
-  basicUnsafeFreeze = fmap coerce . uUnsafeFreeze . coerce
+  basicUnsafeFreeze = fmap Vector . uUnsafeFreeze . Mutable.unVector
   basicUnsafeThaw :: PrimMonad m => Vector a -> m (G.Mutable Vector (PrimState m) a)
-  basicUnsafeThaw = fmap coerce . uUnsafeThaw . coerce
+  basicUnsafeThaw = fmap Mutable.Vector . uUnsafeThaw . unVector
   basicLength :: Vector a -> Int
-  basicLength = uLength . coerce
+  basicLength = uLength . unVector
   basicUnsafeSlice :: Int -> Int -> Vector a -> Vector a
-  basicUnsafeSlice i l = coerce . uUnsafeSlice i l . coerce
+  basicUnsafeSlice i l = Vector . uUnsafeSlice i l . unVector
 
+  {-# SPECIALIZE basicUnsafeIndexM :: Monad m => Vector Double -> Int -> m Double #-}
   basicUnsafeIndexM :: Monad m => Vector a -> Int -> m a
-  basicUnsafeIndexM v i = coerce <$> uUnsafeIndexM (coerce v) i
-  basicUnsafeCopy :: PrimMonad m => G.Mutable Vector (PrimState m) a -> Vector a -> m ()
-  basicUnsafeCopy mut immut = uUnsafeCopy (coerce mut) (coerce immut)
+  basicUnsafeIndexM v i = uUnsafeIndexM (unVector v) i
+  basicUnsafeCopy :: PrimMonad m => Mutable.MVector (PrimState m) a -> Vector a -> m ()
+  basicUnsafeCopy mut immut = uUnsafeCopy (Mutable.unVector mut) (unVector immut)
   -- elemseq :: Vector a -> a -> b -> b
 
-uUnsafeSlice :: Int -> Int -> Unpacked a -> Unpacked a
+-- {-# RULES "basicUnsafeIndexM_Double" G.basicUnsafeIndexM = basicUnsafeIndexM_Double #-}
+-- basicUnsafeIndexM_Double :: Monad m => Vector Double -> Int -> m Double
+-- basicUnsafeIndexM_Double v i = coerce <$> uUnsafeIndexM (coerce v :: Unpacked (Packing Double)) i
+
+-- {-# RULES "basicUnsafeIndexM_Tuple" forall u i. G.basicUnsafeIndexM (Vector u) i = basicUnsafeIndexM_Tuple u i #-}
+-- basicUnsafeIndexM_Tuple :: (Unpack a, Unpack b, Monad m) => Unpacked (a,b) (R a) -> Int -> m (a,b)
+-- basicUnsafeIndexM_Tuple v i = coerce <$> uUnsafeIndexM v i
+
+uUnsafeSlice :: Int -> Int -> Unpacked a (R a) -> Unpacked a (R a)
 uUnsafeSlice i j (Rational a b) =
   Rational (G.basicUnsafeSlice i j a) (G.basicUnsafeSlice i j b)
 uUnsafeSlice i j (Tuple a b) =
@@ -57,7 +73,7 @@ uUnsafeSlice i j (Unboxed u) =
 uUnsafeSlice i j (Boxed u) =
   Boxed (G.basicUnsafeSlice i j u)
 
-uUnsafeCopy :: PrimMonad m => Mutable.Unpacked (PrimState m) a -> Unpacked a -> m ()
+uUnsafeCopy :: PrimMonad m => Mutable.Unpacked (PrimState m) a (R a) -> Unpacked a (R a) -> m ()
 uUnsafeCopy (Mutable.Rational a1 b1) (Rational a2 b2) = do
   G.basicUnsafeCopy a1 a2
   G.basicUnsafeCopy b1 b2
@@ -68,16 +84,93 @@ uUnsafeCopy (Mutable.Unboxed u1) (Unboxed u2) = do
   G.basicUnsafeCopy u1 u2
 uUnsafeCopy (Mutable.Boxed u1) (Boxed u2) = do
   G.basicUnsafeCopy u1 u2
-uUnsafeCopy _ _ = error "Impossible"
 
-uUnsafeIndexM :: Monad m => Unpacked a -> Int -> m a
-uUnsafeIndexM arr i = case arr of
-  Rational a b -> (:%) <$> G.basicUnsafeIndexM a i <*> G.basicUnsafeIndexM b i
-  Tuple a b    -> (,) <$> uUnsafeIndexM a i <*> uUnsafeIndexM b i
+data Container a where
+  SimpleContainer :: (Unpack a) => !(Vector a) -> Container a
+
+cIndex :: Container (Double, ()) -> Int -> (Double, ())
+cIndex (SimpleContainer arr) i = G.unsafeIndex arr i
+
+tIndex :: Container (Double, Int) -> Int -> Double
+tIndex (SimpleContainer arr) i = fst (G.unsafeIndex arr i)
+
+bIndex :: Unpacked Bool BRep -> Int -> Bool
+bIndex arr i = runIdentity $ uUnsafeIndexM arr i
+
+uIndex :: Vector Double -> Int -> Double
+uIndex arr i = runIdentity $ uUnsafeIndexM (unVector arr) i
+
+cFromList :: Unpack a => [a] -> Container a
+cFromList lst = SimpleContainer (G.fromList lst)
+
+
+-- memoryDiff :: (NFData (v a), G.Vector v a) => v a -> v a -> IO Word
+-- memoryDiff a b = do
+--   a' <- recursiveSize $!! a
+--   b' <- recursiveSize $!! b
+--   return (b'-a')
+
+listPoint2 :: [Point 2 Rational]
+listPoint2 = [Point2 11 2, Point2 3 4, Point2 5 6, Point2 7 8, Point2 9 10]
+
+listV2 :: [V2 Rational]
+listV2 = -- [V2 11 2, V2 3 4, V2 5 6, V2 7 8, V2 9 10]
+  [V2 1 1, V2 1 1, V2 1 1, V2 1 1, V2 1 1]
+
+listV2' :: [V2 Int]
+listV2' = [V2 1 2, V2 3 4, V2 5 6, V2 7 8, V2 9 10]
+
+listT2 :: [(Rational, Rational)]
+listT2 = [(,) 1 2, (,) 2 3, (,) 4 5, (,) 6 7]
+
+{-# RULES "uUnsafeIndexM_Tuple" uUnsafeIndexM = (\arr ->
+  case arr of
+    Tuple a b -> \i -> do
+      a' <- uUnsafeIndexM a i
+      b' <- uUnsafeIndexM b i
+      return (a', b')) :: Monad m => Unpacked (a, b) (TRep (R a) (R b)) -> Int -> m (a,b) #-}
+
+{-# RULES
+"uUnsafeIndexM_Double" uUnsafeIndexM = uUnsafeIndexM_Double
+"uUnsafeIndexM_()"     uUnsafeIndexM = uUnsafeIndexM_Unit
+#-}
+
+{-# INLINE uUnsafeIndexM_Double #-}
+uUnsafeIndexM_Double :: (Monad m) => Unpacked Double URep -> Int -> m Double
+-- uUnsafeIndexM_Double arr i = uUnsafeIndexM_URep arr i -- Don't use this code. It prevents optimizations.
+uUnsafeIndexM_Double arr i = case arr of
   Unboxed u    -> G.basicUnsafeIndexM u i
-  Boxed u      -> coerce <$> G.basicUnsafeIndexM u i
 
-uUnsafeFreeze :: PrimMonad m => Mutable.Unpacked (PrimState m) a -> m (Unpacked a)
+uUnsafeIndexM_Unit :: (Monad m) => Unpacked () URep -> Int -> m ()
+uUnsafeIndexM_Unit arr i = case arr of
+  Unboxed u    -> G.basicUnsafeIndexM u i
+
+{-# INLINE uUnsafeIndexM_URep #-}
+uUnsafeIndexM_URep :: (R a ~ URep, Monad m) => Unpacked a (R a) -> Int -> m a
+uUnsafeIndexM_URep !arr !i = case arr of
+  Unboxed u    -> G.basicUnsafeIndexM u i
+
+{-# NOINLINE uUnsafeIndexM #-}
+uUnsafeIndexM :: Monad m => Unpacked a (R a) -> Int -> m a
+uUnsafeIndexM !arr !i = case arr of
+  Rational a b -> (:%) <$> G.basicUnsafeIndexM a i <*> G.basicUnsafeIndexM b i
+  Tuple a b    -> (,) <$> uUnsafeIndexM_1 a i <*> uUnsafeIndexM_1 b i
+  Unboxed u    -> G.basicUnsafeIndexM u i
+  Boxed u      -> G.basicUnsafeIndexM u i
+
+uUnsafeIndexM_1 :: Monad m => Unpacked a (R a) -> Int -> m a
+uUnsafeIndexM_1 !arr !i = case arr of
+  Rational a b -> (:%) <$> G.basicUnsafeIndexM a i <*> G.basicUnsafeIndexM b i
+  Tuple a b    -> (,) <$> uUnsafeIndexM_1 a i <*> uUnsafeIndexM_1 b i
+  Unboxed u    -> G.basicUnsafeIndexM u i
+  Boxed u      -> G.basicUnsafeIndexM u i
+
+
+-- uUnsafeIndexM_Double :: Monad m => Unpacked (S Double) -> Int -> m (S Double)
+-- uUnsafeIndexM_Double !arr !i = case arr of
+--   Unboxed u    -> G.basicUnsafeIndexM u i
+
+uUnsafeFreeze :: PrimMonad m => Mutable.Unpacked (PrimState m) a (R a) -> m (Unpacked a (R a))
 uUnsafeFreeze mut = case mut of
   Mutable.Rational a b -> do
     a' <- G.basicUnsafeFreeze a
@@ -87,7 +180,7 @@ uUnsafeFreeze mut = case mut of
   Mutable.Unboxed u -> Unboxed <$> G.basicUnsafeFreeze u
   Mutable.Boxed u -> Boxed <$> G.basicUnsafeFreeze u
 
-uUnsafeThaw :: PrimMonad m => Unpacked a -> m (Mutable.Unpacked (PrimState m) a)
+uUnsafeThaw :: PrimMonad m => Unpacked a (R a) -> m (Mutable.Unpacked (PrimState m) a (R a))
 uUnsafeThaw arr = case arr of
   Rational a b -> do
     a' <- G.basicUnsafeThaw a
@@ -97,17 +190,17 @@ uUnsafeThaw arr = case arr of
   Unboxed u -> Mutable.Unboxed <$> G.basicUnsafeThaw u
   Boxed u -> Mutable.Boxed <$> G.basicUnsafeThaw u
 
-rep :: Vector a -> Tree String
-rep (Vector xs) = urep xs
+rep :: Vector a -> IO ()
+rep (Vector xs) = putStrLn $ drawTree $ urep xs
 
-uLength :: Unpacked a -> Int
+uLength :: Unpacked a (R a) -> Int
 uLength (Rational a _) = V.length a
 uLength (Unboxed u)    = VU.length u
 uLength (Boxed v)      = V.length v
 uLength (Tuple a _)    = uLength a
 
-urep :: Unpacked a -> Tree String
-urep Rational{}  = Node "Rational" [Node "UNBOXED" [], Node "UNBOXED" []]
+urep :: Unpacked a (R a) -> Tree String
+urep Rational{}  = Node "Rational" [Node "BOXED" [], Node "BOXED" []]
 urep Unboxed{}   = Node "UNBOXED" []
 urep Boxed{}     = Node "BOXED" []
 urep (Tuple a b) = Node "Tuple" [urep a, urep b]
--- hgeometry/src/Data/Vector/Unpacked/Mutable.hs
+++ hgeometry/src/Data/Vector/Unpacked/Mutable.hs
@@ -1,69 +1,69 @@
 {-# LANGUAGE DefaultSignatures #-}
 {-# LANGUAGE DeriveAnyClass    #-}
 {-# LANGUAGE InstanceSigs      #-}
+{-# LANGUAGE KindSignatures    #-}
 {-# LANGUAGE TypeFamilies      #-}
 module Data.Vector.Unpacked.Mutable where
 
-import           Control.Monad.Primitive     (PrimMonad, PrimState)
-import           Data.Coerce                 (Coercible, coerce)
-import           Data.Proxy                  (Proxy (..))
+import Control.Monad.Primitive (PrimMonad, PrimState)
+import Data.Coerce             (coerce)
+import Data.Proxy              (Proxy (..))
 -- import           Data.Ratio
 import           Data.Tree
 import qualified Data.Vector.Generic.Mutable as GM
 import qualified Data.Vector.Mutable         as V
 import qualified Data.Vector.Unboxed.Mutable as VU
 import           GHC.Real
-import           Linear.V2
 
-newtype NoPacking a = NoPacking {unNoPacking :: a} deriving (Show)
+newtype MVector s a = Vector { unVector :: Unpacked s a (R a) }
 
-newtype MVector s a = Vector (Unpacked s (S a))
-
-data Unpacked s a where
-  Rational :: !(V.MVector s Integer) -> !(V.MVector s Integer) -> Unpacked s Rational
-  Tuple    :: !(Unpacked s a) -> !(Unpacked s b) -> Unpacked s (a,b)
-  Unboxed  :: VU.Unbox a => !(VU.MVector s a) -> Unpacked s a
-  Boxed    :: !(V.MVector s a) -> Unpacked s (NoPacking a)
+data Unpacked s a (r::Rep) where
+  Rational :: !(V.MVector s Integer) -> !(V.MVector s Integer) -> Unpacked s Rational RatRep
+  Tuple    :: !(Unpacked s (Left (a,b)) (R (Left (a,b)))) ->
+              !(Unpacked s (Right (a,b)) (R (Right (a,b)))) ->
+              Unpacked s (a,b) (TRep (R a) (R b))
+  Unboxed  :: VU.Unbox a => !(VU.MVector s a) -> Unpacked s a URep
+  Boxed    :: !(V.MVector s a) -> Unpacked s a BRep
 
 instance Unpack a => GM.MVector MVector a where
-  basicLength = uLength . coerce
-  basicUnsafeSlice i n = coerce . uUnsafeSlice i n . coerce
-  basicOverlaps a b = uOverlaps (coerce a) (coerce b)
-  
+  basicLength = uLength . unVector
+  basicUnsafeSlice i n = Vector . uUnsafeSlice i n . unVector
+  basicOverlaps a b = uOverlaps (unVector a) (unVector b)
+
   basicUnsafeNew :: (PrimMonad m) => Int -> m (MVector (PrimState m) a)
   basicUnsafeNew n = Vector <$> uUnsafeNew (Proxy @ a) n
 
   basicInitialize :: PrimMonad m => MVector (PrimState m) a -> m ()
-  basicInitialize v = uInitialize (coerce v)
+  basicInitialize v = uInitialize (unVector v)
 
   basicUnsafeReplicate :: PrimMonad m => Int -> a -> m (MVector (PrimState m) a)
   basicUnsafeReplicate i v = Vector <$> uUnsafeReplicate i v
 
   basicUnsafeRead  :: PrimMonad m => MVector (PrimState m) a -> Int -> m a
-  basicUnsafeRead v i = coerce <$> uUnsafeRead (coerce v) i
+  basicUnsafeRead v i = uUnsafeRead (unVector v) i
 
   basicUnsafeWrite :: PrimMonad m => MVector (PrimState m) a -> Int -> a -> m ()
-  basicUnsafeWrite v i a = uUnsafeWrite (coerce v) i (coerce a)
+  basicUnsafeWrite v i a = uUnsafeWrite (unVector v) i a
 
   basicClear       :: PrimMonad m => MVector (PrimState m) a -> m ()
-  basicClear v = uClear (coerce v)
+  basicClear v = uClear (unVector v)
 
   basicSet         :: PrimMonad m => MVector (PrimState m) a -> a -> m ()
-  basicSet v a = uSet (coerce v) (coerce a)
+  basicSet v a = uSet (unVector v) a
 
   basicUnsafeCopy  :: PrimMonad m => MVector (PrimState m) a   -- ^ target
                                   -> MVector (PrimState m) a   -- ^ source
                                   -> m ()
-  basicUnsafeCopy a b = uUnsafeCopy (coerce a) (coerce b)
+  basicUnsafeCopy a b = uUnsafeCopy (unVector a) (unVector b)
 
   basicUnsafeMove  :: PrimMonad m => MVector (PrimState m) a   -- ^ target
                                   -> MVector (PrimState m) a   -- ^ source
                                   -> m ()
-  basicUnsafeMove a b = uUnsafeMove (coerce a) (coerce b)
+  basicUnsafeMove a b = uUnsafeMove (unVector a) (unVector b)
 
 
-uUnsafeMove :: PrimMonad m => Unpacked (PrimState m) a   -- ^ target
-                           -> Unpacked (PrimState m) a   -- ^ source
+uUnsafeMove :: PrimMonad m => Unpacked (PrimState m) a (R a)   -- ^ target
+                           -> Unpacked (PrimState m) a (R a)   -- ^ source
                            -> m ()
 uUnsafeMove (Rational a1 b1) (Rational a2 b2) = do
   GM.basicUnsafeMove a1 a2
@@ -73,10 +73,9 @@ uUnsafeMove (Tuple a1 b1) (Tuple a2 b2)       = do
   uUnsafeMove b1 b2
 uUnsafeMove (Unboxed u1) (Unboxed u2)         = GM.basicUnsafeMove u1 u2
 uUnsafeMove (Boxed u1) (Boxed u2)             = GM.basicUnsafeMove u1 u2
-uUnsafeMove _ _ = error ""
 
-uUnsafeCopy :: PrimMonad m => Unpacked (PrimState m) a   -- ^ target
-                           -> Unpacked (PrimState m) a   -- ^ source
+uUnsafeCopy :: PrimMonad m => Unpacked (PrimState m) a (R a)   -- ^ target
+                           -> Unpacked (PrimState m) a (R a)   -- ^ source
                            -> m ()
 uUnsafeCopy (Rational a1 b1) (Rational a2 b2) = do
   GM.basicUnsafeCopy a1 a2
@@ -86,37 +85,36 @@ uUnsafeCopy (Tuple a1 b1) (Tuple a2 b2)       = do
   uUnsafeCopy b1 b2
 uUnsafeCopy (Unboxed u1) (Unboxed u2)         = GM.basicUnsafeCopy u1 u2
 uUnsafeCopy (Boxed u1) (Boxed u2)             = GM.basicUnsafeCopy u1 u2
-uUnsafeCopy _ _ = error ""
 
-uSet :: PrimMonad m => Unpacked (PrimState m) a -> a -> m ()
+uSet :: PrimMonad m => Unpacked (PrimState m) a (R a) -> a -> m ()
 uSet (Rational l r) (a :% b) = GM.basicSet l a >> GM.basicSet r b
-uSet (Unboxed u) v = GM.basicSet u v
-uSet (Boxed u) v = GM.basicSet u (coerce v)
-uSet (Tuple l r) (a,b) = uSet l a >> uSet r b
+uSet (Unboxed u) v           = GM.basicSet u (coerce v)
+uSet (Boxed u) v             = GM.basicSet u (coerce v)
+uSet (Tuple l r) (a,b)       = uSet l a >> uSet r b
 
-uClear :: PrimMonad m => Unpacked (PrimState m) a -> m ()
+uClear :: PrimMonad m => Unpacked (PrimState m) a (R a) -> m ()
 uClear (Rational a b) = GM.basicClear a >> GM.basicClear b
-uClear (Unboxed u) = GM.basicClear u
-uClear (Boxed v) = GM.basicClear v
-uClear (Tuple a b) = uClear a >> uClear b
+uClear (Unboxed u)    = GM.basicClear u
+uClear (Boxed v)      = GM.basicClear v
+uClear (Tuple a b)    = uClear a >> uClear b
 
-uUnsafeWrite :: PrimMonad m => Unpacked (PrimState m) a -> Int -> a -> m ()
+uUnsafeWrite :: PrimMonad m => Unpacked (PrimState m) a (R a) -> Int -> a -> m ()
 uUnsafeWrite (Rational l r) i (a :% b) = do
   GM.basicUnsafeWrite l i a
   GM.basicUnsafeWrite r i b
-uUnsafeWrite (Unboxed u) i a = GM.basicUnsafeWrite u i a
+uUnsafeWrite (Unboxed u) i a = GM.basicUnsafeWrite u i (coerce a)
 uUnsafeWrite (Boxed v) i a   = GM.basicUnsafeWrite v i (coerce a)
 uUnsafeWrite (Tuple l r) i (a,b) = do
   uUnsafeWrite l i a
   uUnsafeWrite r i b
 
-uUnsafeRead :: PrimMonad m => Unpacked (PrimState m) a -> Int -> m a
+uUnsafeRead :: PrimMonad m => Unpacked (PrimState m) a (R a) -> Int -> m a
 uUnsafeRead (Rational a b) i = (:%) <$> GM.basicUnsafeRead a i <*> GM.basicUnsafeRead b i
 uUnsafeRead (Unboxed u) i    = GM.basicUnsafeRead u i
-uUnsafeRead (Boxed v) i      = NoPacking <$> GM.basicUnsafeRead v i
+uUnsafeRead (Boxed v) i      = GM.basicUnsafeRead v i
 uUnsafeRead (Tuple a b) i    = (,) <$> uUnsafeRead a i <*> uUnsafeRead b i
 
-uInitialize :: PrimMonad m => Unpacked (PrimState m) a -> m ()
+uInitialize :: PrimMonad m => Unpacked (PrimState m) a (R a) -> m ()
 uInitialize (Rational a b) = do
       GM.basicInitialize a
       GM.basicInitialize b
@@ -124,7 +122,7 @@ uInitialize (Unboxed u) = GM.basicInitialize u
 uInitialize (Boxed v) = GM.basicInitialize v
 uInitialize (Tuple a b) = uInitialize a >> uInitialize b
 
-uUnsafeSlice :: Int -> Int -> Unpacked s a -> Unpacked s a
+uUnsafeSlice :: Int -> Int -> Unpacked s a (R a) -> Unpacked s a (R a)
 uUnsafeSlice i n = \case
   Rational a b -> Rational
     (GM.basicUnsafeSlice i n a)
@@ -133,36 +131,37 @@ uUnsafeSlice i n = \case
   Boxed v -> Boxed $ GM.basicUnsafeSlice i n v
   Tuple a b -> Tuple (uUnsafeSlice i n a) (uUnsafeSlice i n b)
 
-uOverlaps :: Unpacked s a -> Unpacked s a -> Bool
+uOverlaps :: Unpacked s a (R a) -> Unpacked s a (R a) -> Bool
 uOverlaps (Rational a1 b1) (Rational a2 b2) = GM.basicOverlaps a1 a2 || GM.basicOverlaps b1 b2
 uOverlaps (Tuple a1 b1) (Tuple a2 b2)       = uOverlaps a1 a2 || uOverlaps b1 b2
 uOverlaps (Unboxed u1) (Unboxed u2)         = GM.basicOverlaps u1 u2
 uOverlaps (Boxed u1) (Boxed u2)             = GM.basicOverlaps u1 u2
-uOverlaps _ _                               = error "cannot happen"
 
 rep :: MVector s a -> Tree String
 rep (Vector xs) = urep xs
 
-uLength :: Unpacked s a -> Int
+uLength :: Unpacked s a (R a) -> Int
 uLength (Rational a _) = V.length a
 uLength (Unboxed u)    = VU.length u
 uLength (Boxed v)      = V.length v
 uLength (Tuple a _)    = uLength a
 
-urep :: Unpacked s a -> Tree String
-urep Rational{}     = Node "Rational" [Node "UNBOXED" [], Node "UNBOXED" []]
-urep Unboxed{}      = Node "UNBOXED" []
-urep Boxed{}        = Node "BOXED" []
-urep (Tuple a b)    = Node "Tuple" [urep a, urep b]
+urep :: Unpacked s a (R a) -> Tree String
+urep Rational{}  = Node "Rational" [Node "UNBOXED" [], Node "UNBOXED" []]
+urep Unboxed{}   = Node "UNBOXED" []
+urep Boxed{}     = Node "BOXED" []
+urep (Tuple a b) = Node "Tuple" [urep a, urep b]
 
-class (Coercible (S a) a) => Unpack a where
-  uUnsafeNew :: PrimMonad m => Proxy a -> Int -> m (Unpacked (PrimState m) (S a))
-  uUnsafeReplicate :: PrimMonad m => Int -> a -> m (Unpacked (PrimState m) (S a))
+class Unpack a where
+  uUnsafeNew :: PrimMonad m => Proxy a -> Int -> m (Unpacked (PrimState m) a (R a))
+  uUnsafeReplicate :: PrimMonad m => Int -> a -> m (Unpacked (PrimState m) a (R a))
 
-  default uUnsafeNew :: (VU.Unbox (S a), S a ~ a, PrimMonad m) => Proxy a -> Int -> m (Unpacked (PrimState m) (S a))
+  default uUnsafeNew :: (VU.Unbox a, R a ~ URep, PrimMonad m) =>
+    Proxy a -> Int -> m (Unpacked (PrimState m) a (R a))
   uUnsafeNew _ n = Unboxed <$> GM.basicUnsafeNew n
 
-  default uUnsafeReplicate :: (VU.Unbox a, S a ~ a, PrimMonad m) => Int -> a -> m (Unpacked (PrimState m) (S a))
+  default uUnsafeReplicate :: (VU.Unbox a, R a ~ URep, PrimMonad m) =>
+    Int -> a -> m (Unpacked (PrimState m) a (R a))
   uUnsafeReplicate n a = Unboxed <$> GM.basicUnsafeReplicate n a
 
 
@@ -179,23 +178,126 @@ instance Unpack Rational where
 
 
 instance (Unpack a, Unpack b) => Unpack (a,b) where
-  uUnsafeNew :: PrimMonad m => Proxy (a, b) -> Int -> m (Unpacked (PrimState m) (S (a, b)))
+  uUnsafeNew :: PrimMonad m => Proxy (a, b) -> Int -> m (Unpacked (PrimState m) (a,b) (TRep (R a) (R b)))
   uUnsafeNew _ n = Tuple <$> uUnsafeNew (Proxy @ a) n <*> uUnsafeNew (Proxy @ b) n
 
   uUnsafeReplicate n (a,b) = Tuple <$> uUnsafeReplicate n a <*> uUnsafeReplicate n b
 
 -- Boxed instance.
-instance {-# OVERLAPS #-} (S a ~ NoPacking a) => Unpack a where
+instance {-# OVERLAPS #-} (R a ~ BRep) => Unpack a where
   uUnsafeNew _ n = Boxed <$> GM.basicUnsafeNew n
   uUnsafeReplicate n a = Boxed <$> GM.basicUnsafeReplicate n a
 
-type family S a where
-  S Rational = Rational
-  S (V2 a) = V2 (S a)
-  S () = ()
-  S Double = Double
-  S (a,b) = (S a, S b)
-  S a = NoPacking a
+data Rep = TRep Rep Rep | BRep | URep | RatRep
+
+-- newtype CVector s a = CVector (Col s a (R a))
+
+-- test_uIndex :: PrimMonad m => CVector (PrimState m) Double -> m Double
+-- test_uIndex (CVector col) = case col of
+--   ColU u -> GM.read u 0
+--   -- ColB v -> GM.read v 0
+
+-- test_uIndex_two :: PrimMonad m => CVector (PrimState m) Double -> m Double
+-- test_uIndex_two vec = cRead vec 0
+
+-- test_uIndex_three :: CVector (PrimState IO) (Double, (Double, Double)) -> IO (Double, (Double, Double))
+-- test_uIndex_three vec = cRead vec 0
+
+-- {-# INLINE cRead #-}
+-- cRead :: PrimMonad m => CVector (PrimState m) a -> Int -> m a
+-- cRead (CVector col) i = colRead col i
+
+-- {-# RULES "colReadDouble" colRead = colReadDouble #-}
+-- {-# INLINE colReadDouble #-}
+-- colReadDouble :: PrimMonad m => Col (PrimState m) Double URep -> Int -> m Double
+-- colReadDouble col i = case col of
+--   ColU u -> GM.read u i
+
+-- {-# RULES "colReadTuple" colRead = (\col ->
+--   case col of
+--     ColT a b -> \i -> do
+--       a' <- colRead a i
+--       b' <- colRead b i
+--       return (a', b')) :: PrimMonad m => Col (PrimState m) (a, b) (TRep (R a) (R b)) -> Int -> m (a,b) #-}
+-- {- INLINE colReadTuple -}
+-- colReadTuple :: PrimMonad m => Col (PrimState m) (a, b) (TRep (R a) (R b)) -> Int -> m (a,b)
+-- colReadTuple col = case col of
+--   ColT a b -> \i -> do
+--     a' <- colRead a i
+--     b' <- colRead b i
+--     return (a', b')
+
+-- {-# RULES "colRead_1" colRead_1 = colRead #-}
+
+-- {-# NOINLINE colRead #-}
+-- {- SPECIALIZE colRead :: Col (PrimState IO) Double URep -> Int -> IO Double -}
+-- colRead :: PrimMonad m => Col (PrimState m) a (R a) -> Int -> m a
+-- colRead col i = case col of
+--   ColT a b -> do
+--     a' <- colRead_1 a i
+--     b' <- colRead_1 b i
+--     return (a', b')
+--   ColU u -> GM.read u i
+--   ColB u -> GM.read u i
+
+-- {- NOINLINE colRead_1 -}
+-- colRead_1 :: PrimMonad m => Col (PrimState m) a (R a) -> Int -> m a
+-- colRead_1 col i = case col of
+--   ColT a b -> do
+--     a' <- colRead_1 a i
+--     b' <- colRead_1 b i
+--     return (a', b')
+--   ColU u -> GM.read u i
+--   ColB u -> GM.read u i
+
+-- data Col s a (r::Rep) where
+--   -- ColT    :: !(Col s (Left (a,b)) (R a)) -> !(Col s (Right (a,b)) (R b))  -> Col s (a,b) (TRep (R a) (R b))
+--   ColT    :: !(Col s (Left (a,b)) (R (Left (a,b)))) -> !(Col s (Right (a,b)) (R (Right (a,b)))) -> Col s (a,b) (TRep (R a) (R b))
+--   ColU    :: VU.Unbox a => !(VU.MVector s a) -> Col s a URep
+--   ColB    :: !(V.MVector s a)                -> Col s a BRep
+
+-- class UnpackCol a where
+--   uUnsafeNewCol :: PrimMonad m => Proxy a -> Int -> m (Col (PrimState m) a (R a))
+--   uUnsafeReplicateCol :: PrimMonad m => Int -> a -> m (Col (PrimState m) a (R a))
+
+--   default uUnsafeNewCol :: (VU.Unbox a, R a ~ URep, PrimMonad m)
+--     => Proxy a -> Int -> m (Col (PrimState m) a (R a))
+--   uUnsafeNewCol _ n = ColU <$> GM.basicUnsafeNew n
+
+--   default uUnsafeReplicateCol :: (VU.Unbox a, R a ~ URep, PrimMonad m) => Int -> a -> m (Col (PrimState m) a (R a))
+--   uUnsafeReplicateCol n a = ColU <$> GM.basicUnsafeReplicate n a
+
+-- instance UnpackCol () where
+-- instance UnpackCol Double where
+-- instance (UnpackCol a, UnpackCol b) => UnpackCol (a,b) where
+--   uUnsafeNewCol :: PrimMonad m => Proxy (a, b) -> Int -> m (Col (PrimState m) (a,b) (TRep (R a) (R b)))
+--   uUnsafeNewCol _ n = ColT <$> uUnsafeNewCol (Proxy @ a) n <*> uUnsafeNewCol (Proxy @ b) n
+
+--   uUnsafeReplicateCol n (a,b) = ColT <$> uUnsafeReplicateCol n a <*> uUnsafeReplicateCol n b
+-- instance {-# OVERLAPS #-} (R a ~ BRep) => UnpackCol a where
+--   uUnsafeNewCol = undefined
+--   uUnsafeReplicateCol = undefined
+
+type family Left a where
+  Left (a,b) = a
+
+type family Right a where
+  Right (a,b) = b
+
+type family R a :: Rep where
+  R Rational = RatRep
+  R ()       = URep
+  R (a,b)    = TRep (R a) (R b)
+  R Double   = URep
+  R a        = BRep
+
+-- type family S a where
+--   S Rational = Rational
+--   S (V2 a) = V2 (S a)
+--   S () = Packing ()
+--   S Double = Packing Double
+--   S (a,b) = (S a, S b)
+--   S a = NoPacking a
 
 -- Element cost of boxed [Point 2 Double :+ ()]:   11 words
 -- Element cost of unboxed [Point 2 Double :+ ()]: 2 words
