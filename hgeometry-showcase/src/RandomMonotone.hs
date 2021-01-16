af9fe6133aaad0000f0cb2eb63b92c762d227682
David Himmelstrup <lemmih@gmail.com>
2021-01-16 11:17:09 +0800

    Update monotone animation.

--- hgeometry-showcase/src/RandomMonotone.hs
+++ hgeometry-showcase/src/RandomMonotone.hs
@@ -1,11 +1,12 @@
 {-# LANGUAGE DataKinds #-}
-module RandomMonotone where
+module RandomMonotone (randomMonotoneShowcase ) where
 
 import           Algorithms.Geometry.MonotonePolygon.MonotonePolygon
 
 import           Control.Lens
 import           Control.Monad.Random
 import           Data.Ext
+import           Data.Ratio
 import qualified Data.Foldable                                       as F
 import           Data.Geometry.Line                                  (Line (..))
 import           Data.Geometry.LineSegment
@@ -25,36 +26,57 @@ import           Graphics.SvgTree                                    (Cap (..),
                                                                       LineJoin (..),
                                                                       strokeLineCap)
 import           Reanimate
+import           Reanimate.Animation
 
 import           Common
 import           Data.RealNumber.Rational
 
 type R = Rational -- RealNumber 5
 
-direction = Vector2 1 0
-p = pAtCenter $
-    scaleUniformlyBy (screenHeight*0.9) $
-    evalRand (randomMonotone 10 direction) (mkStdGen seed)
+{- Animation overview:
+* Start with N random points.
+* Draw polygon outline.
+* Scan through polygon, highlighting intersections.
+* Fade out everything except points.
+* Interpolate to new random set of points.
+-}
 
 randomMonotoneShowcase :: Animation
 randomMonotoneShowcase = scene $ do
-  let pts = map _core $ toPoints p'
-      minP = P.minimumBy (cmpExtreme direction) p
-      maxP = P.maximumBy (cmpExtreme direction) p
-      Just p' = P.findRotateTo (==minP) p
-  adjustZ (+2) $ newSpriteSVG_ $ mkGroup $ map ppPoint [_core minP, _core maxP]
-  wait 1
-  adjustZ succ $ newSpriteSVG_ $ mkGroup $ map ppPoint pts
-  wait 1
-  path <- newPath
-  forM_ pts $ \pt -> pushPath path pt
-  pushPath path (Prelude.head pts)
-  forM_ pts $ \pt -> do
-    newSpriteSVG $ withStrokeColorPixel black $ mkGroup
-      [ ppLine pt iPt
-      | iPt <- intersectionsAt p' direction pt
-      ]
-  wait 1
+    forM_ pairs $ \((dir, pl), next) -> do
+      play $ randomMonotoneSingle pl dir
+      play $ animate (\t ->
+        let morph = lerpPolygon t pl next
+        in ppVertices (map _core $ toPoints morph))
+        & signalA (curveS 2)
+  where
+    pairs = zip monotonePolygons (drop 1 (map snd (monotonePolygons++[Prelude.head monotonePolygons])))
+
+randomMonotoneSingle :: SimplePolygon () R -> Vector 2 R -> Animation
+randomMonotoneSingle p' dir = scene $ do
+    adjustZ succ $ newSpriteSVG_ $ ppVertices pts
+    path <- newPath
+    forM_ pts $ pushPath path
+    pushPath path (Prelude.head pts)
+    wait 1
+    adjustZ succ $ play $
+      applyE (overEnding 0.1 fadeOutE) $
+      applyE (overBeginning 0.1 fadeInE) $
+      pauseAround 0.1 0.1 $
+      signalA (curveS 2) $
+      setDuration 5 $
+      animate $ \t ->
+      let atP = lerpPoint t (maxP^.core) (minP^.core) in
+      withStrokeColorPixel red $ mkGroup
+      [ mkGroup
+        [ ppLine atP iPt
+        , ppMarker iPt ]
+      | iPt <- intersectionsAt p' dir atP ]
+  where
+    pts = map _core $ toPoints p
+    minP = P.minimumBy (cmpExtreme dir) p'
+    maxP = P.maximumBy (cmpExtreme dir) p'
+    Just p = P.findRotateTo (==minP) p'
 
 intersectionsAt :: SimplePolygon () R -> Vector 2 R -> Point 2 R -> [Point 2 R]
 intersectionsAt p direction pt = catMaybes
@@ -88,8 +110,11 @@ pushPath var pt = do
   oldPath <- readVar var
   if null oldPath
     then writeVar var [pt]
-    else tweenVar var 1 $ \_ t ->
-            oldPath ++ [lerpPoint (curveS 2 t) pt (Prelude.last oldPath)]
+    else do
+      let prev = Prelude.last oldPath
+          dist = sqrt $ realToFrac $ squaredEuclideanDist pt prev
+      tweenVar var (dist*0.2) $ \_ t ->
+        oldPath ++ [lerpPoint t pt prev]
 
 showPoints :: [Point 2 Rational] -> SVG
 showPoints pts = mkGroup
@@ -103,10 +128,36 @@ seed = hash "random monotone"
 nPoints :: Int
 nPoints = 10
 
+nPolygons :: Int
+nPolygons = 5
+
+monotonePolygons :: [(Vector 2 R, SimplePolygon () R)]
+monotonePolygons = flip evalRand (mkStdGen seed) $
+  replicateM nPolygons $ do
+    dir <- generateRandomVector2
+    p <- randomMonotone nPoints dir
+    return (dir, pAtCenter $ scaleUniformlyBy (screenHeight*0.9) p)
+
+
+generateRandomVector2 :: RandomGen g => Rand g (Vector 2 Rational)
+generateRandomVector2 = do
+    a <- liftRand $ randomR(-granularity, granularity)
+    b <- liftRand $ randomR(-granularity, granularity)
+    pure $ Vector2 (a % granularity) (b % granularity)
+
+ppVertices :: Real r => [Point 2 r] -> SVG
+ppVertices = mkGroup . map ppPoint
+
 ppPoint :: Real r => Point 2 r -> SVG
 ppPoint (Point2 x y) =
   withFillColorPixel green $
   withStrokeColorPixel black $
   translate (realToFrac x) (realToFrac y) $
   mkCircle nodeRadius
+
+ppMarker :: Real r => Point 2 r -> SVG
+ppMarker (Point2 x y) =
+  withFillColorPixel red $
+  translate (realToFrac x) (realToFrac y) $
+  mkCircle (nodeRadius*0.25)
 
