4c08c03899c1d469970a41ad2fa567db54958907
1ndy <hjdemeyer64@gmail.com>
2021-01-20 13:14:51 -0500

    Created VisibilityPolygon.hs

---
+++ hgeometry/src/Algorithms/Geometry/RaycastingVisibilityPolygon/VisibilityPolygon.hs
@@ -0,0 +1,7 @@
+module Algorithms.Geometry.VisibilityPolygon.Naive where
+
+import Data.Geometry.Polygon.Core
+
+visibility :: SimplePolygon p r -> Point 2 r -> SimplePolygon () r
+visibility polygon point = 
+    error "Not yet implemented"
