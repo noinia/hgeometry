93a62c4b8639cba1b9d6803905ae1460e80d2471
David Himmelstrup <lemmih@gmail.com>
2021-01-21 11:27:13 +0800

    Algorithm description and scaffold for helper functions.

--- hgeometry/src/Algorithms/Geometry/RaycastingVisibilityPolygon/VisibilityPolygon.hs
+++ hgeometry/src/Algorithms/Geometry/RaycastingVisibilityPolygon/VisibilityPolygon.hs
@@ -1,7 +1,84 @@
 module Algorithms.Geometry.VisibilityPolygon.Naive where
 
 import Data.Geometry.Polygon.Core
+import Data.Geometry.Point
+import Data.Geometry.HalfLine
+import Data.Geometry.LineSegment
+import Data.Vector.Circular
+
+
+{- Some interesting cases to consider:
+
+Case 1: Ray from 'x' hits a corner and is blocked.
+
+     /|\
+    / | \
+   /  |  \
+  /   |   \
+ \----x----/
+
+Case 2: Ray from 'x' hits a vertex on the right side and isn't blocked:
+/---------\
+|       | /
+|       |/
+|       |\
+|       | \
+|       |  |
+\-------x--/
+
+Case 3: Ray from 'x' hits a vertex on the left side and isn't blocked:
+/---------\
+ \ |      |
+  \|      |
+  /|      |
+ / |      |
+|  |      |
+\--x------/
+
+
+Case 4: Ray from 'x' to a far corner is blocked by edges.
+   /------\
+  /|      |
+ /----\   |
+   |  |   |
+/-----/   |
+|  |      |
+\--x------/
+
+
+
+The brute-force visibility algorithm works as follows:
+1. X is a given point of origin inside of P.
+2. Sort the vertices of P ccw arount X.
+3. Shoot a ray from X to each vertex in P and collect any intersections.
+4. For each ray, sort the intersections by distance to X. Then:
+  If the ray hits the vertex without any other intersections /and/
+  the ray is blocked by the vertex (see case 1 drawing), then
+  the vertex is in the visibility polygon.
+  If the ray grazes a point on the right side (see case 2) then
+  both the grazed vertex AND the final intersection point are
+  in the visibility polygon.
+  Case 3 is similar to case 2 with the only difference that the
+  grazing vertex and the final intersection point should be reversed
+  in the visibility polygon. Ie the final intersection point should
+  appear before the grazing vertex.
+  For case 4, nothing needs to be done and no points along the ray
+  should appear in the visibility polygon.
+
+-}
+
 
 visibility :: SimplePolygon p r -> Point 2 r -> SimplePolygon () r
 visibility polygon point = 
     error "Not yet implemented"
+
+
+-------------------------------------------------------------------------------
+-- Helper functions
+
+data Event p r
+  = SegmentIntersection (Point 2 r) (LineSegment 2 p r)
+  | VertexIntersection (Point 2 r) (LineSegment 2 p r) (LineSegment 2 p r)
+
+shootRay :: HalfLine 2 r -> CircularVector (LineSegment 2 p r) -> [Event p r]
+shootRay = undefined
