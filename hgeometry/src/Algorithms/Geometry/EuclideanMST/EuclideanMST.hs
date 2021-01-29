cb74896afcedc9b6be48736ea53f84ff57c3ec6f
David Himmelstrup <lemmih@gmail.com>
2021-01-29 15:58:05 +0800

    Rename WellSeparatedPairDecomposition to WSPD.

--- hgeometry/src/Algorithms/Geometry/EuclideanMST/EuclideanMST.hs
+++ hgeometry/src/Algorithms/Geometry/EuclideanMST/EuclideanMST.hs
@@ -11,7 +11,7 @@
 --------------------------------------------------------------------------------
 module Algorithms.Geometry.EuclideanMST.EuclideanMST
   {-# DEPRECATED "This module will be deleted after 2021-06-01. \
-                 \Use Algorithms.Geometry.EuclideanMST instead" #-}
+                 \Use Algorithms.Geometry.EuclideanMST instead." #-}
   ( module Algorithms.Geometry.EuclideanMST ) where
 
 import Algorithms.Geometry.EuclideanMST
