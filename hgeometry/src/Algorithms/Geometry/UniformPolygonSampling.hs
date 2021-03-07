bc66d61911d11d2d1ca417c03e0deab21162c9fd
OwenGraves <ograves.owen@gmail.com>
2021-03-07 15:22:50 -0600

    added toTriangles, could probably be cleaner

--- hgeometry/src/Algorithms/Geometry/UniformPolygonSampling.hs
+++ hgeometry/src/Algorithms/Geometry/UniformPolygonSampling.hs
@@ -1,13 +1,18 @@
 module Algorithms.Geometry.UniformPolygonSampling where
 
-import Control.Monad.Random
-import Data.Geometry.Point
-import Data.Geometry.Polygon.Core
-import Data.Geometry.Triangle
-import Control.Lens
-import Data.Ext
-import Linear.Affine hiding (Point)
-import Linear.Vector
+import           Algorithms.Geometry.PolygonTriangulation.Triangulate
+import           Control.Lens
+import           Control.Monad.Random
+import           Data.Ext
+import           Data.Geometry.Point
+import           Data.Geometry.Polygon.Core
+import           Data.Geometry.Triangle
+import qualified Data.List.NonEmpty                                   as NonEmpty
+import           Data.PlaneGraph
+import           Data.Proxy
+import qualified Data.Vector                                          as V
+import           Linear.Affine                                        hiding (Point)
+import           Linear.Vector
 
 -- | O(n log n)
 samplePolygon :: (RandomGen g, Random r, Fractional r) => Polygon t p r -> Rand g (Point 2 r)
@@ -18,12 +23,19 @@ sampleTriangle :: (RandomGen g, Random r, Fractional r, Ord r) => Triangle 2 p r
 sampleTriangle (Triangle v1 v2 v3) = do
   a' <- getRandomR (0, 1)
   b' <- getRandomR (0, 1)
-  let (a, b) = if a' + b' > 1 then (1 - a', 1 - b') else (a', b') in
-    return $ v1^.core .+^ a*^u .+^ b*^v
+  let (a, b) = if a' + b' > 1 then (1 - a', 1 - b') else (a', b')
+  return $ v1^.core .+^ a*^u .+^ b*^v
   where
     u = v2^.core .-. v1^.core
     v = v3^.core .-. v1^.core
 
 -- | O(n log n)
 toTriangles :: (Fractional r, Ord r) => Polygon t p r -> [Triangle 2 p r]
-toTriangles = error "not implemented yet"
+toTriangles p =
+    map (polygonToTriangle . view core . (`rawFacePolygon` g) . fst) $
+    V.toList (internalFaces g)
+  where
+    g = triangulate' Proxy p
+    polygonToTriangle poly = case NonEmpty.toList $ polygonVertices poly of
+      [v1, v2, v3] -> Triangle v1 v2 v3
+      _            -> error "Invalid Triangulation"
