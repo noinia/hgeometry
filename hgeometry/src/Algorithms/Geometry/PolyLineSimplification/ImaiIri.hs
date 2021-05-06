b3dd4be4cbf6c7e47087ff1b3ef84480cb08928e
Frank Staals <frank@fstaals.net>
2021-05-06 20:25:06 +0200

    fixing some bugs + calling the proper BFS implementation

--- hgeometry/src/Algorithms/Geometry/PolyLineSimplification/ImaiIri.hs
+++ hgeometry/src/Algorithms/Geometry/PolyLineSimplification/ImaiIri.hs
@@ -6,6 +6,7 @@
 --------------------------------------------------------------------------------
 module Algorithms.Geometry.PolyLineSimplification.ImaiIri where
 
+import           Algorithms.Graph.BFS (bfs')
 import           Control.Lens
 import           Data.Ext
 import qualified Data.Foldable as F
@@ -21,7 +22,12 @@ import           Data.Tree
 import qualified Data.Vector as V
 import           Witherable
 
+import Data.RealNumber.Rational
+import Debug.Trace
 --------------------------------------------------------------------------------
+
+type R = RealNumber 5
+
 
 -- | Line simplification with the Imai-Iri alogrithm. Given a distance
 -- value eps and a polyline pl, constructs a simplification of pl
@@ -43,7 +49,7 @@ simplifyWith            :: (LineSegment d p r -> PolyLine  d p r -> Bool)
 simplifyWith isValid pl = pl&points %~ (LSeq.promise @2 . extract path)
   where
     g    = mkGraph isValid pl
-    spt  = bfs g
+    spt  = bfs' 0 g
     path = case pathsTo (pl^.points.to F.length - 1) spt of
              []      -> error "no path found?"
              (pth:_) -> pth
@@ -52,13 +58,14 @@ simplifyWith isValid pl = pl&points %~ (LSeq.promise @2 . extract path)
 type Graph = V.Vector [Int]
 
 mkGraph         :: (LineSegment d p r -> PolyLine d p r -> Bool) -> PolyLine d p r -> Graph
-mkGraph isValid = V.imap f . V.fromList . F.toList . allPrefixes
+mkGraph isValid = flip V.snoc [] . V.imap f . V.fromList . F.toList . allPrefixes
   where
     f i subPl = catMaybes
               $ zipWith isValid' [i+1..] . F.toList . allSuffixes $ subPl
 
     isValid' j subPoly = let shortcut = ClosedLineSegment (subPoly^.start) (subPoly^.end)
                          in if isValid shortcut subPoly then Just j else Nothing
+
 
 allPrefixes    :: PolyLine d p r -> Seq.Seq (PolyLine d p r)
 allPrefixes pl = mapMaybe mkPolyLine . Seq.tails . LSeq.toSeq $ pl^.points
@@ -68,6 +75,8 @@ mkPolyLine = fmap PolyLine . LSeq.eval @2 . LSeq.fromSeq
 
 allSuffixes :: PolyLine d p r -> Seq.Seq (PolyLine d p r)
 allSuffixes pl = mapMaybe mkPolyLine . Seq.drop 2 . Seq.inits . LSeq.toSeq $ pl^.points
+
+
 
 
 -- | Get all paths to the particular element in the tree.
@@ -89,24 +98,27 @@ extract    :: NonEmpty Int -> LSeq.LSeq n a -> LSeq.LSeq 0 a
 extract is = LSeq.fromList . extract' (F.toList is) 0 . F.toList
 
 
-extract'                             :: [Int] -> Int -> [a] -> [a]
-extract' []     _ _                  = []
-extract' (_:_)  _ []                 = []
-extract' (i:is) j (x:xs) | i == j    = x : extract' is (j+1) xs
-                         | otherwise = extract' is (j+1) xs
+extract'                                 :: [Int] -> Int -> [a] -> [a]
+extract' []         _ _                  = []
+extract' (_:_)      _ []                 = []
+extract' is'@(i:is) j (x:xs) | i == j    = x : extract' is (j+1) xs
+                             | otherwise = extract' is' (j+1) xs
 
 
 
-
-
-
-bfs = undefined
 
 
 tr :: Tree Int
 tr = Node 0 [Node 1 [], Node 2 [Node 3 [], Node 2 [], Node 4 [Node 5 []]]]
 
+poly :: PolyLine 2 Int R
 poly = case fromPoints [origin :+ 0, Point2 1 1 :+ 1, Point2 2 2 :+ 2, Point2 3 3 :+ 3] of
          Just p -> p
 
 test = Seq.fromList [0..5]
+
+myTree :: Tree Int
+myTree = Node {rootLabel = 0, subForest = [Node {rootLabel = 1, subForest = []}
+                                       ,Node {rootLabel = 2, subForest = []}
+                                       ,Node {rootLabel = 3, subForest = []}]
+           }
