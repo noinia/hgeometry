80d0cc1e212dc7f4a6b096f18cc226364f6b7f99
Frank Staals <frank@fstaals.net>
2021-06-07 12:06:11 +0200

    Some simple tests

--- hgeometry/src/Algorithms/Geometry/PolyLineSimplification/ImaiIri.hs
+++ hgeometry/src/Algorithms/Geometry/PolyLineSimplification/ImaiIri.hs
@@ -4,7 +4,10 @@
 -- License     :  see the LICENSE file
 -- Maintainer  :  Frank Staals
 --------------------------------------------------------------------------------
-module Algorithms.Geometry.PolyLineSimplification.ImaiIri where
+module Algorithms.Geometry.PolyLineSimplification.ImaiIri
+  ( simplify
+  , simplifyWith
+  ) where
 
 import           Algorithms.Graph.BFS (bfs')
 import           Control.Lens
@@ -23,18 +26,16 @@ import qualified Data.Vector as V
 import           Witherable
 
 import Data.RealNumber.Rational
-import Debug.Trace
---------------------------------------------------------------------------------
-
 type R = RealNumber 5
 
+--------------------------------------------------------------------------------
 
 -- | Line simplification with the Imai-Iri alogrithm. Given a distance
 -- value eps and a polyline pl, constructs a simplification of pl
 -- (i.e. with vertices from pl) s.t. all other vertices are within
 -- dist eps to the original polyline.
 --
--- Running time: \( O(n^2) \) worst case.
+-- Running time: \( O(n^2) \) time.
 simplify     :: (Ord r, Fractional r, Arity d)
              => r -> PolyLine d p r -> PolyLine d p r
 simplify eps = simplifyWith $ \shortcut subPoly -> all (closeTo shortcut) (subPoly^.points)
@@ -42,8 +43,11 @@ simplify eps = simplifyWith $ \shortcut subPoly -> all (closeTo shortcut) (subPo
     closeTo seg (p :+ _) = sqDistanceToSeg p seg  <= epsSq
     epsSq = eps*eps
 
-
--- | Given a function that tests if the shortcut is valid, compute a simplification.
+-- | Given a function that tests if the shortcut is valid, compute a
+-- simplification using the Imai-Iri algorithm.
+--
+-- Running time: \( O(Tn^2 \) time, where \(T\) is the time to
+-- evaluate the predicate.
 simplifyWith            :: (LineSegment d p r -> PolyLine  d p r -> Bool)
                         -> PolyLine d p r -> PolyLine d p r
 simplifyWith isValid pl = pl&points %~ (LSeq.promise @2 . extract path)
@@ -54,9 +58,11 @@ simplifyWith isValid pl = pl&points %~ (LSeq.promise @2 . extract path)
              []      -> error "no path found?"
              (pth:_) -> pth
 
+----------------------------------------
 
 type Graph = V.Vector [Int]
 
+-- | Constructs the shortcut graph
 mkGraph         :: (LineSegment d p r -> PolyLine d p r -> Bool) -> PolyLine d p r -> Graph
 mkGraph isValid = flip V.snoc [] . V.imap f . V.fromList . F.toList . allPrefixes
   where
@@ -66,15 +72,19 @@ mkGraph isValid = flip V.snoc [] . V.imap f . V.fromList . F.toList . allPrefixe
     isValid' j subPoly = let shortcut = ClosedLineSegment (subPoly^.start) (subPoly^.end)
                          in if isValid shortcut subPoly then Just j else Nothing
 
-
+-- | Generates all prefixes of the polyline; i.e. all contiguous
+-- polylines all starting at the original starting point.
 allPrefixes    :: PolyLine d p r -> Seq.Seq (PolyLine d p r)
 allPrefixes pl = mapMaybe mkPolyLine . Seq.tails . LSeq.toSeq $ pl^.points
 
 mkPolyLine :: Seq.Seq (Point d r :+ p) -> Maybe (PolyLine d p r)
 mkPolyLine = fmap PolyLine . LSeq.eval @2 . LSeq.fromSeq
 
+-- | Generates all suffixes of the polyline.
 allSuffixes :: PolyLine d p r -> Seq.Seq (PolyLine d p r)
 allSuffixes pl = mapMaybe mkPolyLine . Seq.drop 2 . Seq.inits . LSeq.toSeq $ pl^.points
+
+
 
 
 
@@ -94,9 +104,14 @@ findPaths p = go
                               | otherwise ->           map (x NonEmpty.<|) paths
 
 
+
+
+-- | Given a non-empty list of indices, and some LSeq, extract the elemnets
+-- on those indices.
+--
+-- running time: \(O(n)\)
 extract    :: NonEmpty Int -> LSeq.LSeq n a -> LSeq.LSeq 0 a
 extract is = LSeq.fromList . extract' (F.toList is) 0 . F.toList
-
 
 extract'                                 :: [Int] -> Int -> [a] -> [a]
 extract' []         _ _                  = []
@@ -104,8 +119,7 @@ extract' (_:_)      _ []                 = []
 extract' is'@(i:is) j (x:xs) | i == j    = x : extract' is (j+1) xs
                              | otherwise = extract' is' (j+1) xs
 
-
-
+--------------------------------------------------------------------------------
 
 
 tr :: Tree Int
