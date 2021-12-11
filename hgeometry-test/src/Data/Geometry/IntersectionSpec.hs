bf36f2c4d1bff3cc62b0f935463f67619ce8763c
Frank Staals <frank@fstaals.net>
2021-12-11 21:56:14 +0100

    test also compile again

--- hgeometry-test/src/Data/Geometry/IntersectionSpec.hs
+++ hgeometry-test/src/Data/Geometry/IntersectionSpec.hs
@@ -2,29 +2,39 @@
 module Data.Geometry.IntersectionSpec where
 
 import Data.Ext
+import Data.Geometry.Line
 import Data.Geometry.LineSegment
 import Data.Geometry.Point
 import Data.Geometry.Polygon
 import Data.Intersection
 import Data.Proxy
 import Data.RealNumber.Rational
+import Data.Vinyl
 import Ipe
 import Paths_hgeometry_test
 import Test.Hspec
 import Test.QuickCheck
 
+--------------------------------------------------------------------------------
+
+type R = RealNumber 5
 
 
 -- |
-spec = prop ("intersects agrees with intersect: Data.Geometry.Line ") propIntersectionsAgree
+spec :: Spec
+spec = do
+        it ("intersects agrees with intersect: Data.Geometry.Line ") $
+           property (propIntersectionsAgree @(Line 2 R) @(Line 2 R))
+        it ("intersects agrees with intersect: Data.Geometry.LineSegment ") $
+           property (propIntersectionsAgree @(LineSegment 2 () R) @(LineSegment 2 () R))
+        it ("intersects agrees with intersect: Data.Geometry.LineSegment x Line ") $
+           property (propIntersectionsAgree @(LineSegment 2 () R) @(Line 2 R))
 
-
-
-propIntersectionsAgree     :: ( IsIntersectableWith a b
+propIntersectionsAgree     :: forall a b. ( IsIntersectableWith a b
                               , NoIntersection ∈ IntersectionOf a b
-                              , RecApplicative (IntersectionOf g h)
+                              , RecApplicative (IntersectionOf a b)
                               )
                            => a -> b -> Expectation
 propIntersectionsAgree a b = (a `intersects` b)
                              `shouldBe`
-                             (defaultNonEmptyIntersection a b)
+                             (defaultNonEmptyIntersection (Proxy @a) (Proxy @b) $ a `intersect` b)
