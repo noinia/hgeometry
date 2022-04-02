bfa27ae3eea97e3cc88e0930f75635f1a94ef493
Frank Staals <frank@fstaals.net>
2022-04-02 23:11:23 +0200

    more fiddling

--- hgeometry-devel/src/Algorithms/Geometry/ConvexHull/Chan.hs
+++ hgeometry-devel/src/Algorithms/Geometry/ConvexHull/Chan.hs
@@ -16,9 +16,9 @@
 --------------------------------------------------------------------------------
 module Algorithms.Geometry.ConvexHull.Chan where
 
-
+import           Algorithms.BinarySearch
 import           Algorithms.DivideAndConquer
-import           Control.Lens
+import           Control.Lens((^.), view)
 import           Data.Geometry.Point (xCoord, yCoord, zCoord)
 import qualified Data.Geometry.Point as Point
 import           Data.Geometry.Polygon.Convex (lowerTangent')
@@ -29,7 +29,8 @@ import           Data.Ord (comparing, Down(..))
 -- import qualified Data.List as List
 import           Data.List.NonEmpty (NonEmpty(..))
 import qualified Data.List.NonEmpty as NonEmpty
-import           Data.Sequence (Seq(..))
+import qualified Data.Sequence as Seq
+import           Data.Sequence(Seq(..), ViewL(..), ViewR(..))
 import qualified Data.OrdSeq as OrdSeq
 import qualified Data.Set as Set
 
@@ -76,14 +77,37 @@ class Hull (hull :: Type -> Type) where
   predOf :: point -> hull point -> Maybe point
   succOf :: point -> hull point -> Maybe point
 
-  -- fromBridge :: Bridge hull point -> hull point
+  fromBridge :: Bridge hull point -> hull point
   -- default fromBridge :: (Semigroup (hull point)) => Bridge hull point -> hull point
   -- fromBridge (Bridge ll l _ _ r rr) = ll <> singleton l <> singleton r <> rr
 
   bridgeOf :: hull point -> hull point -> Bridge hull point
 
-  delete :: point -> hull point -> hull point
-  insert :: point -> hull point -> hull point
+  delete :: Point point => point -> hull point -> hull point
+  insert :: Point point => point -> hull point -> hull point
+
+instance Hull HullZ where
+  singleton p = HullZ mempty p mempty
+  focus (HullZ _ p _) = p
+
+  goLeft (HullZ ll p rr) = case Seq.viewr ll of
+                             EmptyR   -> error "cannot go left"
+                             ll' :> l -> HullZ ll' l (p :<| rr)
+  goRight (HullZ ll p rr) = case Seq.viewl rr of
+                              EmptyL    -> error "cannot go right"
+                              r :< rr' -> HullZ (ll :|> p) r rr'
+
+
+  fromBridge (Bridge (HullZ ll l _) (HullZ _ r rr)) = HullZ ll l (r :<| rr)
+
+  bridgeOf l r = undefined
+
+  delete q (HullZ ll p rr) = case q `compareX` p of
+                               LT -> HullZ (delete' q ll) p rr
+                               EQ -> undefined
+                               GT -> HullZ ll p (delete' q rr)
+
+delete' = undefined
 
   -- extractPred :: point -> hull point -> Maybe (hull point, point)
   -- extractSucc :: point -> hull point -> Maybe (point, hull point)
@@ -92,9 +116,14 @@ type Hull' = NonEmpty
 
 --------------------------------------------------------------------------------
 
+data HullZ point = HullZ (Seq point) point (Seq point)
+                 deriving (Show,Eq)
 
-fromBridge :: Bridge hull point -> hull point
-fromBridge = undefined
+
+
+
+--------------------------------------------------------------------------------
+
 
 data Bridge hull point = Bridge (hull point) (hull point)
                        deriving (Show,Eq)
@@ -254,6 +283,7 @@ apply                            :: (Point point, Hull hull)
                                  => Event point -> Bridge hull point -> Bridge hull point
 apply e b@(Bridge l r) = case applyWhere (eventPoint e) b of
     NoWhere          -> b
+
   --   LeftHull         -> Bridge (apply' e ll) l r rr
   --   RightHull        -> Bridge ll l r (apply' e rr)
   --   LeftBridgePoint  -> undefined
