3e7719e5ec5b800678ec7bfe5462254f65ea0b7e
Frank Staals <frank@fstaals.net>
2022-04-05 22:50:19 +0200

    use ToAPoint class

--- hgeometry-devel/src/Algorithms/Geometry/ConvexHull/Chan.hs
+++ hgeometry-devel/src/Algorithms/Geometry/ConvexHull/Chan.hs
@@ -51,16 +51,17 @@ lowerHull = fromSimulation . divideAndConquer1 (simulation @Hull'). NonEmpty.sor
 --------------------------------------------------------------------------------
 
 class ( Ord (NumType point), Fractional (NumType point)
+      , Point.ToAPoint point 3 (NumType point)
       ) => Point point where
   toPt3 :: point -> Point.Point 3 (NumType point)
+  toPt3 = view Point.toPoint
 
 toPt2                                :: Point point
                                      => Time point -> point -> Point.Point 2 (NumType point)
 toPt2 t (toPt3 -> Point.Point3 x y z) = Point.Point2 x (z - t*y)
 
 
-instance (Ord r, Fractional r) => Point (Point.Point 3 r) where
-  toPt3 = id
+instance (Ord r, Fractional r) => Point (Point.Point 3 r)
 
 compareX :: Point point => point -> point -> Ordering
 compareX = comparing (view xCoord . toPt3)
