cb86bd7fdc545dc5bde5b5e898f9dcc4f0907350
Frank Staals <frank@fstaals.net>
2022-04-09 21:03:27 +0200

    ask for the number of points

--- hgeometry-devel/benchmark/Prof.hs
+++ hgeometry-devel/benchmark/Prof.hs
@@ -1,18 +1,20 @@
 module Main where
 
 import Algorithms.Geometry.ConvexHull.Minimalist (lowerHull')
-import Data.Bifunctor
 import Control.Lens
+import Data.Bifunctor
 import Data.Ext
 import Data.RealNumber.Rational
 import Generate
 import PLY.Writer
+import System.Environment(getArgs)
 
 
 type R = RealNumber 5
 
 main :: IO ()
-main = do pts <- genPoints @R 2000
+main = do [n] <- fmap read <$> getArgs
+          pts <- genPoints @R n
           let lh = lowerHull' pts
           renderOutputToFile "lowerHull.ply" (over core (fmap toDouble) <$> pts)
                                              (second toDouble <$> lh)
