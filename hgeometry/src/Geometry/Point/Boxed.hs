4ab63577a1cae5aef8a1ff30a951d8a68fdb551b
Frank Staals <frank@fstaals.net>
2022-07-08 22:17:02 +0200

    the boxed point impl

---
+++ hgeometry/src/Geometry/Point/Boxed.hs
@@ -0,0 +1,32 @@
+{-# LANGUAGE ScopedTypeVariables  #-}
+{-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE AllowAmbiguousTypes #-}
+--------------------------------------------------------------------------------
+-- |
+-- Module      :  Geometry.Point.Boxed
+-- Copyright   :  (C) Frank Staals
+-- License     :  see the LICENSE file
+-- Maintainer  :  Frank Staals
+--
+-- \(d\)-dimensional points.
+--
+--------------------------------------------------------------------------------
+module Geometry.Point.Boxed
+  ( Point(..)
+  , vector
+  -- , pattern Point1
+  -- , pattern Point2
+  -- , pattern Point3
+  -- , pattern Point4
+  , PointFunctor(..)
+  ) where
+
+import  Geometry.Point.Internal
+
+--------------------------------------------------------------------------------
+-- $setup
+-- >>> :{
+-- let myVector :: Vector 3 Int
+--     myVector = Vector3 1 2 3
+--     myPoint = Point myVector
+-- :}
