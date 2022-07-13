5f7e472d33b6c35f295840d5337f6015cb325ef5
Frank Staals <frank@fstaals.net>
2022-07-13 21:32:25 +0200

    intervals and endpoints

--- hgeometry/src/Geometry/LineSegment/Class.hs
+++ hgeometry/src/Geometry/LineSegment/Class.hs
@@ -4,77 +4,137 @@ module Geometry.LineSegment.Class
   , Interval(Interval)
 
 
-  , LineSegment
+  , LineSegment_
   ) where
 
 import Control.Lens
 import Data.Kind
+import GHC.TypeNats
+import Geometry.Boundary
+import Geometry.Properties
 -- import Data.Range (EndPoint(..))
 
+--------------------------------------------------------------------------------
+
+class HasStart seg p where
+  start :: Lens' seg p
+
+class HasEnd seg p where
+  end :: Lens' seg p
+
+-- type Interval_ :: ( (Type -> Type) -> Type -> Type )
+--                -> (Type -> Type) -- ^ the endpoint
+--                -> Type    -- ^ the numeric type
+--                -> Constraint
+class ( HasStart (interval endPoint r) (endPoint r)
+      , HasEnd   (interval endPoint r) (endPoint r)
+      , EndPoint_ endPoint
+      ) => Interval_ interval endPoint r  where
 
 
--- class LineSegment_ lineSegment d point r where
---   start :: Lens' (lineSegment d point r) (point d r)
---   end   :: Lens' (lineSegment d point r) (point d r)
 
--- class HasStart s t a b where
---   start :: Lens s t a b
 
--- class HasEnd s t a b where
---   end :: Lens s t a b
-type Interval_ :: ( (Type -> Type) -> Type -> Type )
-               -> (Type -> Type) -- ^ the endpoint
-               -> Type    -- ^ the numeric type
-               -> Constraint
-class Interval_ (interval) endPoint r where
-  start :: Lens' (interval endPoint r) (endPoint r)
-  end   :: Lens' (interval endPoint r) (endPoint r)
+--------------------------------------------------------------------------------
 
+inInterval       :: forall interval endPoint r. (Ord r, Interval_ interval endPoint r)
+                 => r -> interval endPoint r -> PointLocationResult
+x `inInterval` i =
+    case x `compare` (i^.start.endPoint @endPoint) of
+      LT -> Outside
+      EQ -> OnBoundary
+      GT -> case x `compare` (i^.end.endPoint @endPoint) of
+              LT -> Inside
+              EQ -> OnBoundary
+              GT -> Outside
+
+
+
+
+--------------------------------------------------------------------------------
 
 data Interval endPoint r = Interval !(endPoint r) !(endPoint r)
 
+data EndPointType = OpenEndPoint | ClosedEndPoint deriving (Show,Eq)
 
-data EndPoint point d r = EndPoint EndPointType (point d r)
+class EndPoint_ endPoint where
+  endPoint     :: Lens (endPoint r) (endPoint r') r r'
+  endPointType :: endPoint r -> EndPointType
+
+----------------------------------------
+
+data IntervalEndPoint r = IntervalEndPoint EndPointType r
                         deriving (Show,Eq)
 
-data EndPointType = Open | Closed deriving (Show,Eq)
+instance EndPoint_ IntervalEndPoint where
+  endPoint     =
+    lens (\(IntervalEndPoint _ p) -> p) (\(IntervalEndPoint t _) p -> IntervalEndPoint t p)
+  endPointType (IntervalEndPoint t _) = t
 
 
-instance Interval_ Interval endPoint r where
+
+
+--------------------------------------------------------------------------------
+
+
+
+instance HasStart (Interval endPoint r) (endPoint r) where
   start = lens (\(Interval s _) -> s) (\(Interval _ t) s -> Interval s t)
+
+instance HasEnd (Interval endPoint r) (endPoint r) where
   end   = lens (\(Interval _ t) -> t) (\(Interval s _) t -> Interval s t)
 
+instance EndPoint_ endPoint => Interval_ Interval endPoint r where
 
 
 
 
-newtype LineSegment d point r = MkLineSegment (Interval (EndPoint point d) r)
+--------------------------------------------------------------------------------
+
+-- data SegmentEndPoint point d r = SegmentEndPoint EndPointType (point d r)
+--                         deriving (Show,Eq)
+
+
+class ( HasStart (lineSegment d point r) (point d r)
+      , HasEnd   (lineSegment d point r) (point d r)
+
+      ) => LineSegment_ lineSegment d point r where
+
+
+
+newtype LineSegmentF endPoint d point r =
+  MkLineSegment (Interval endPoint (point d r))
+
+
 
 _WrappedInterval :: Iso (LineSegment d point r)         (LineSegment d' point' r')
-                        (Interval (EndPoint point d) r) (Interval (EndPoint point' d') r')
+                        (Interval IntervalEndPoint (point d r)) (Interval IntervalEndPoint (point' d' r'))
 _WrappedInterval = iso (\(MkLineSegment i) -> i) MkLineSegment
 
 
--- instance Interval_ (LineSegment d) (point d) r where
---   start = _WrappedInterval . start
---   end   = _WrappedInterval . end
+
+instance HasStart (LineSegment d point r) (point d r) where
+  start = _WrappedInterval.start.endPoint @IntervalEndPoint
+
+instance HasEnd   (LineSegment d point r) (point d r) where
+  end   = _WrappedInterval.end.endPoint @IntervalEndPoint
+
+
+instance LineSegment_ LineSegment d point r where
+
+
+--------------------------------------------------------------------------------
+
+newtype Closed r = Closed r deriving (Show,Eq,Ord)
+
+instance EndPoint_ Closed where
+  endPoint = lens (\(Closed x) -> x) (\_ x -> Closed x)
+  endPointType = const ClosedEndPoint
+
+newtype ClosedLineSegment d point r = MkClosedLineSegment (Interval (point d) r)
+
+-- _WrappedClosedInterval :: Iso (ClosedLineSegment d point r) (ClosedLineSegment d' point' r')
 
 
 
--- newtype Interval point1 r = MkInterval (LineSegment )
-
--- class LineSegment_ lineSegment
-
-
-
--- class Interval_ lineSegment (point d) r => LineSegment_ lineSegment d point r
-
-
-
-
-
-
--- instance Interval_ (LineSegment d) point r where
---   start = lens (\(LineSegment s _) -> s) (\(LineSegment _ t) s -> LineSegment s t)
-
--- instance LineSegment_ LineSegment d point r where
+--                           (Interval IntervalEndPoint (point d r)) (Interval IntervalEndPoint (point' d' r'))
+-- _WrappedClosedInterval = iso (\(MkClosedLineSegment i) -> i) MkClosedLineSegment
