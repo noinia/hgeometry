f8b94de0375e35be5db2d3b85e13c99f1dfebbac
Frank Staals <frank@fstaals.net>
2022-11-12 23:11:19 +0100

    polygon implementation

---
+++ hgeometry/src/HGeometry/Polygon/Class.hs
@@ -0,0 +1,237 @@
+{-# LANGUAGE DefaultSignatures #-}
+--------------------------------------------------------------------------------
+-- |
+-- Module      :  HGeometry.Polygon.Class
+-- Copyright   :  (C) Frank Staals
+-- License     :  see the LICENSE file
+-- Maintainer  :  Frank Staals
+--
+-- A polygon and some basic functions to interact with them.
+--
+--------------------------------------------------------------------------------
+module HGeometry.Polygon.Class
+  ( HasOuterBoundary(..)
+  , signedArea2X
+  , minimumVertexBy, maximumVertexBy
+  -- , outerBoundaryEdgeSegments
+  , outerBoundaryWithNeighbours
+
+  , Polygon_(..)
+  ) where
+
+import Control.Lens
+import Control.Lens.Internal.Fold (NonEmptyDList(..))
+import Data.Functor.Apply (Apply)
+import Data.Functor.Contravariant (phantom)
+import Data.List.NonEmpty (NonEmpty(..))
+import Data.Maybe (fromMaybe)
+import Data.Semigroup.Foldable
+-- import HGeometry.LineSegment.Class
+import HGeometry.Point.Class
+import HGeometry.Vector.Class
+import Hiraffe.Graph
+
+--------------------------------------------------------------------------------
+
+-- ^ A class for items that have an outer boundary.
+class HasVertices polygon polygon => HasOuterBoundary polygon where
+  -- | A fold over all vertices of the outer boundary, the
+  -- vertices are traversed in CCW order.
+  --
+  -- running time :: \(O(n)\)
+  outerBoundary :: IndexedFold1 (VertexIx polygon) polygon (Vertex polygon)
+
+  -- | A particular vertex of the outer polygon
+  --
+  -- running time: \(O(1)\)
+  outerBoundaryVertexAt   :: VertexIx polygon
+                          -> IndexedGetter (VertexIx polygon) polygon (Vertex polygon)
+
+  -- | A fold over all edges in the polygon. The edges are folded over
+  -- in CCW order, and each edge is associated with the index of its start vertex
+  -- outerBoundaryEdges :: IndexedFold (VertexIx polygon) polygon (Vertex polygon, Vertex polygon)
+  --
+  --
+  -- running time :: \(O(n)\)
+  outerBoundaryEdges :: IndexedFold1 (VertexIx polygon) polygon (Vertex polygon, Vertex polygon)
+
+
+  {-# MINIMAL outerBoundary, outerBoundaryVertexAt #-}
+
+  default outerBoundaryEdges
+    :: Enum (VertexIx polygon)
+    => IndexedFold1 (VertexIx polygon) polygon (Vertex polygon, Vertex polygon)
+  outerBoundaryEdges = ifolding1 $
+    \pg -> ( \(i,u) -> (i,(u, pg ^.outerBoundaryVertexAt (succ i))) )
+         <$> itoNonEmptyOf outerBoundary pg
+    -- \pg -> fmap ( \(i,u) -> (i,(u, pg ^.outerBoundaryVertexAt (succ i))) )
+    --      . NonEmpty.fromList
+    --      $ itoListOf outerBoundary pg
+    -- this feels much more clunky than it should be somehow.
+    -- I would like an 'itoNonEmptyOf'
+
+  -- | Get the edge that has the given vertex as its starting edge
+  --
+  -- running time: \(O(1)\)
+  outerBoundaryEdgeAt   :: VertexIx polygon
+                        -> IndexedGetter (VertexIx polygon) polygon
+                                         (Vertex polygon, Vertex polygon)
+  -- default implementation of outerBoundaryEdge. It achieves the
+  -- desired running time when indexing is indeed constant.
+  default outerBoundaryEdgeAt :: Enum (VertexIx polygon)
+                              => VertexIx polygon
+                              -> IndexedGetter (VertexIx polygon) polygon
+                                               (Vertex polygon, Vertex polygon)
+  outerBoundaryEdgeAt i = ito $
+    \pg -> (i, (pg^.outerBoundaryVertexAt i, pg^.outerBoundaryVertexAt (succ i)))
+
+
+--------------------------------------------------------------------------------
+-- end of te HasOuterBoundary class
+--------------------------------------------------------------------------------
+
+-- | Version of ifolding to build an 'IndexedFold1'
+--
+-- taken and modified directly from lens
+ifolding1       :: (Foldable1 f, Indexable i p, Contravariant g, Apply g)
+                => (s -> f (i, a)) -> Over p g s t a b
+ifolding1 sfa f = phantom . traverse1_ (phantom . uncurry (indexed f)) . sfa
+{-# INLINE ifolding1 #-}
+
+-- | indexed version of 'toNonEmptyOf'
+itoNonEmptyOf   :: IndexedGetting i (NonEmptyDList (i,a)) s a -> s -> NonEmpty (i,a)
+itoNonEmptyOf l = flip getNonEmptyDList [] . ifoldMapOf l (\i a -> NonEmptyDList $ ((i,a) :|))
+
+
+--------------------------------------------------------------------------------
+-- * HasOuterBoundary Helpers
+
+-- | Yield the minimum  vertex of a polygon according to the given comparison function.
+--
+-- running time \( O(n) \)
+minimumVertexBy     :: (HasOuterBoundary polygon)
+                    => (Vertex polygon -> Vertex polygon -> Ordering)
+                    -> polygon
+                    -> Vertex polygon
+minimumVertexBy cmp = fromMaybe (error "minimumVertexBy: absurd")
+                    . minimumByOf outerBoundary cmp
+
+-- | Yield the maximum vertex of a polygon according to the given comparison function.
+--
+-- running time \( O(n) \)
+maximumVertexBy     :: (HasOuterBoundary polygon)
+                    => (Vertex polygon -> Vertex polygon -> Ordering)
+                    -> polygon
+                    -> Vertex polygon
+maximumVertexBy cmp = fromMaybe (error "maximumVertexBy: absurd")
+                    . maximumByOf outerBoundary cmp
+
+
+
+-- | Compute the signed area times 2 of a simple polygon. The the
+-- vertices are in clockwise order, the signed area will be negative,
+-- if the verices are given in counter clockwise order, the area will
+-- be positive.
+--
+-- running time: \(O(n)\)
+signedArea2X      :: (Num r, HasOuterBoundary simplePolygon
+                     , Point_ point 2 r
+                     , Vertex simplePolygon ~ point
+                     ) => simplePolygon -> r
+signedArea2X poly = sum [ p^.xCoord * q^.yCoord - q^.xCoord * p^.yCoord
+                        | (p,q) <- poly ^..outerBoundaryEdges ]
+
+{-
+
+-- | Get the line segments representing the outer boundary of the polygon.
+outerBoundaryEdgeSegments :: forall lineSegment polygon point r.
+                             ( LineSegment_ lineSegment 2 point r
+                             , HasOuterBoundary polygon
+                             , Vertex polygon ~ point 2 r
+                             )
+                          => IndexedFold1 (VertexIx polygon) polygon (lineSegment 2 point r)
+outerBoundaryEdgeSegments = outerBoundaryEdges . to toSeg
+  -- ifolding1
+  --                         $ over (mapped._2) toSeg . itoNonEmptyOf outerBoundaryEdges
+  where
+    toSeg :: (Vertex polygon, Vertex polygon) -> lineSegment 2 point r
+    toSeg = uncurry uncheckedLineSegment
+
+-}
+
+-- | A fold that associates each vertex on the boundary with its
+-- predecessor and successor (in that order) along the boundary.
+outerBoundaryWithNeighbours :: ( HasOuterBoundary polygon
+                               , Enum (VertexIx polygon)
+                               )
+                            =>  IndexedFold1 (VertexIx polygon)
+                                        polygon
+                                        (Vertex polygon, (Vertex polygon, Vertex polygon))
+outerBoundaryWithNeighbours = ifolding1 $
+    \pg -> (\(i,u) -> (i, f pg i u)) <$> itoNonEmptyOf outerBoundary pg
+  where
+    f pg i u = let v = pg^.outerBoundaryVertexAt (pred i)
+                   w = pg^.outerBoundaryVertexAt (succ i)
+               in (u, (v, w))
+
+
+--------------------------------------------------------------------------------
+
+-- class HasHoles face face' where
+--   type HoleIx face
+--   type Hole face
+--   holes :: IndexedTraversal (HoleIx face) face face' (Hole face) (Hole face')
+
+--------------------------------------------------------------------------------
+
+-- | A class representing (planar) polygons. The edges of the polygon
+-- may not intersect.
+class ( HasOuterBoundary (polygon point r)
+      , Vertex      (polygon point r) ~ point
+      , Point_ point 2 r
+      ) => Polygon_ polygon point r where
+
+  -- | The area of a polygon
+  --
+  -- running time: \(O(n)\)
+  area :: Fractional r => polygon point r -> r
+
+
+  -- | Finds the extreme points, minimum and maximum, in a given direction
+  --
+  -- running time: \(O(n)\)
+  extremes      :: (Num r, Ord r, Point_ point 2 r, VectorFor point ~ vector)
+                => vector -> polygon point r -> (point, point)
+  extremes u pg = ( minimumVertexBy (cmpExtreme u) pg
+                  , maximumVertexBy (cmpExtreme u) pg
+                  )
+
+--------------------------------------------------------------------------------
+-- end of te Polygon_ class
+--------------------------------------------------------------------------------
+
+-- | Comparison that compares which point is 'larger' in the direction given by
+-- the vector u.
+cmpExtreme       :: (Num r, Ord r, Point_ point 2 r, VectorFor point ~ vector)
+                 => vector -> point -> point -> Ordering
+cmpExtreme u p q = u `dot` (p .-. q) `compare` 0
+
+--------------------------------------------------------------------------------
+
+
+--------------------------------------------------------------------------------
+
+-- data MultiPG
+-- data SimplePG
+
+-- class DerivingStrategyPolygon strat polygon point r | polygon point r -> strat where
+--   derivedArea :: Fractional r => polygon point r -> r
+
+-- instance (SimplePolygon_ polygon point r) =>  DerivingStrategyPolygon SimplePG polygon point r where
+--   derivedArea = signedArea -- abs . signedArea
+--                 -- since the polygon is stored in CCW order, there is no need to actually
+--                 -- use the absolute value.
+
+
+{-
+-}
---
+++ hgeometry/src/HGeometry/Polygon/Simple.hs
@@ -0,0 +1,132 @@
+{-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE QuantifiedConstraints #-}
+--------------------------------------------------------------------------------
+-- |
+-- Module      :  HGeometry.Polygon.Simple
+-- Copyright   :  (C) Frank Staals
+-- License     :  see the LICENSE file
+-- Maintainer  :  Frank Staals
+--
+-- A Simple polygon and some basic functions to interact with them.
+--
+--------------------------------------------------------------------------------
+module HGeometry.Polygon.Simple
+  ( SimplePolygon_(..)
+  , SimplePolygon
+  , SimplePolygonF
+  ) where
+
+import           Control.Lens
+-- import           Data.Cyclic
+import qualified Data.Foldable as F
+import           Data.Foldable.Sort
+import qualified Data.List.NonEmpty as NonEmpty
+import           Data.Vector.NonEmpty.Internal (NonEmptyVector(..))
+-- import           Data.Vector.NonEmpty.Util ()
+import           GHC.Generics
+import           HGeometry.Point
+import           HGeometry.Polygon.Class
+import           HGeometry.Polygon.Simple.Class
+import           HGeometry.Polygon.Simple.Implementation
+import           HGeometry.Properties
+import           Hiraffe.Graph
+--------------------------------------------------------------------------------
+
+-- | Simple polygons just store their vertices in CCCW order
+newtype SimplePolygonF f point = MkSimplePolygon (f point)
+  deriving (Generic)
+
+-- | By default we store simple polygons as non-empty circular vectors.
+type SimplePolygon = SimplePolygonF (Cyclic NonEmptyVector)
+
+type instance Dimension (SimplePolygonF f point) = 2
+type instance NumType   (SimplePolygonF f point) = NumType point
+
+deriving instance Eq (f point) => Eq (SimplePolygonF f point)
+
+-- | Access the container
+_SimplePolygonF :: Iso (SimplePolygonF f point) (SimplePolygonF f' point')
+                       (f point)                (f' point' )
+_SimplePolygonF = iso (\(MkSimplePolygon vs) -> vs) MkSimplePolygon
+
+instance TraversableWithIndex Int f
+      => HasVertices (SimplePolygonF f point) (SimplePolygonF f point') where
+  vertices = _SimplePolygonF . itraversed
+
+instance TraversableWithIndex Int f =>
+         HasPoints (SimplePolygonF f point) (SimplePolygonF f point') point point' where
+  allPoints = vertices
+
+-- instance ( TraversableWithIndex Int f
+--          , HasPoints (SimplePolygonF f point r) (SimplePolygonF f point r)
+--          ) => IsTransformable (SimplePolygonF f point r)
+
+-- instance HasVertices (SimplePolygon point r) (SimplePolygon point' r') where
+--   type Vertex   (SimplePolygon point r) = point 2 r
+--   type VertexIx (SimplePolygon point r) = Int
+--   -- vertices = _SimplePolygonF . CV.itraversedRight
+
+
+
+
+instance ( TraversableWithIndex Int f
+         , Ixed (f point)
+         , IxValue (f point) ~ point
+         , Index (f point) ~ Int
+         )
+      => HasVertices' (SimplePolygonF f point) where
+  type Vertex   (SimplePolygonF f point r) = point
+  type VertexIx (SimplePolygonF f point r) = Int
+  vertexAt i = _SimplePolygonF . iix i
+
+
+instance ( TraversableWithIndex Int f
+         , Traversable1 f
+         , Ixed (f point)
+         , IxValue (f point) ~ point
+         , Index (f point) ~ Int
+         )
+      => HasOuterBoundary (SimplePolygonF f point) where
+  outerBoundary = _SimplePolygonF . traversed1
+  outerBoundaryVertexAt i = singular (vertexAt i)
+
+instance ( Point_ point 2 r
+         , TraversableWithIndex Int f
+         , Traversable1 f
+         , HasFromFoldable1 f
+         , Ixed (f point)
+         , IxValue (f point) ~ point
+         , Index (f point) ~ Int
+         ) => Polygon_ (SimplePolygonF f) point r where
+  area = areaSimplePolygon
+
+instance ( Point_ point 2 r
+         , TraversableWithIndex Int f
+         , Traversable1 f
+         , HasFromFoldable1 f
+         , Ixed (f point)
+         , IxValue (f point) ~ point
+         , Index (f point) ~ Int
+         ) => SimplePolygon_ (SimplePolygonF f) point r where
+  uncheckedFromCCWPoints = MkSimplePolygon . fromFoldable1
+                         . NonEmpty.fromList . F.toList
+
+
+instance ( Show point
+         , SimplePolygon_ (SimplePolygonF f) point r
+         ) => Show (SimplePolygonF f point) where
+  show = showSimplePolygon
+
+{-
+instance (SimplePolygon_ (SimplePolygonF f) point r, Fractional r, Ord r)
+         => HasSquaredEuclideanDistance (SimplePolygonF f point) where
+  pointClosestToWithDistance = pointClosestToWithDistanceSimplePolygon
+-}
+--------------------------------------------------------------------------------
+
+_testPoly :: SimplePolygon (Point 2 Int)
+_testPoly = uncheckedFromCCWPoints [Point2 10 20, origin, Point2 0 100]
+
+
+--------------------------------------------------------------------------------
