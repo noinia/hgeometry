b462f1afe87f706a23f6776f8db319b9add6f17c
Frank Staals <frank@fstaals.net>
2022-11-19 22:40:04 +0100

    cleaning constraints

--- hgeometry-kernel/src/HGeometry/Box/Class.hs
+++ hgeometry-kernel/src/HGeometry/Box/Class.hs
@@ -19,7 +19,6 @@ module HGeometry.Box.Class
   , width
   , height
 
-  -- , BoxFor
   , IsBoxable(..)
 
   , size
@@ -29,8 +28,7 @@ import Control.Lens
 import Data.Kind
 import Data.Type.Ord
 import GHC.TypeLits
-import HGeometry.Interval.Boxed (ClosedInterval)
-import HGeometry.Interval.Class
+import HGeometry.Interval
 import HGeometry.Point
 import HGeometry.Properties
 import HGeometry.Vector
@@ -56,18 +54,14 @@ class ( HasMinPoint box point
   -- | Get a vector with the extent of the box in each dimension. Note
   -- that the resulting vector is 0 indexed whereas one would normally
   -- count dimensions starting at zero.
-  extent :: ( OptVector_ d (IntervalFor box)
-            , ClosedInterval_ (IntervalFor box) r
+  extent :: ( OptVector_ d (ClosedInterval r)
             , r ~ NumType box
             , d ~ Dimension box
             , Num r
-            ) => box -> Vector d (IntervalFor box)
+            ) => box -> Vector d (ClosedInterval r)
 
 -- | Rectangles are two dimensional boxes.
 type Rectangle_ rectangle point = (Box_ rectangle point, Dimension rectangle ~ 2)
-
--- -- | The data type for boxes
--- type family BoxFor g
 
 -- | Types for which we can compute an axis parallel boundingbox
 class IsBoxable g where
@@ -94,10 +88,9 @@ instance HasMaxPoint (Box point) point where
 
 instance ( Affine_ point
          , Point_ point (Dimension point) (NumType point)
+         , OptCVector_ 2 (NumType point)
          ) => Box_ (Box point) point where
-  extent (Box p q) = vZipWith mkClosedInterval (p^.vector) (q^.vector)
-
--- type instance BoxFor (Box point) = Box point
+  extent (Box p q) = vZipWith ClosedInterval (p^.vector) (q^.vector)
 
 instance ( Box_ (Box point) r
          , Point_ point d r
@@ -106,9 +99,9 @@ instance ( Box_ (Box point) r
          ) => IsBoxable (Box point) where
   boundingBox (Box p q) = Box (pointFromPoint p) (pointFromPoint q)
 
-type instance IntervalFor (Box point) = ClosedInterval (NumType point)
-
 --------------------------------------------------------------------------------
+
+
 
 -- | Get the size of the box (in all dimensions). Note that the
 -- resulting vector is 0 indexed whereas one would normally count
@@ -116,36 +109,37 @@ type instance IntervalFor (Box point) = ClosedInterval (NumType point)
 size :: forall box d point r.
         ( Box_ box point, Point_ point d r
         , Num r
-        , ClosedInterval_ (IntervalFor box) r
+        , OptCVector_ 2 r
         , OptVector_ d r
-        , OptVector_ d (IntervalFor box)
-        , HasComponents   (VectorFamily' d (IntervalFor box)) (VectorFamily' d r)
+        , OptVector_ d (ClosedInterval r)
+        , HasComponents (Vector d (ClosedInterval r)) (Vector d r)
         ) => box -> Vector d r
 size = over components duration . extent
+
+
 
 -- Given a dimension, get the width of the box in that dimension.
 -- Dimensions are 0 indexed.
 widthIn :: forall i box d point r. ( Box_ box point, Point_ point d r
                                    , i < d, KnownNat i
-                                   , OptVector_ d (IntervalFor box)
+                                   , OptCVector_ 2 r
+                                   , OptVector_ d (ClosedInterval r)
                                    , OptVector_ d r
-                                   , HasComponents (VectorFamily' d (IntervalFor box))
-                                                   (VectorFamily' d r)
-                                   , ClosedInterval_ (IntervalFor box) r
+                                   , HasComponents (Vector d (ClosedInterval r))
+                                                   (Vector d r)
                                    , Num r
                                    ) => box -> r
 widthIn = view (component @i) . size
 
 
-
 -- | Get the width of a rectangle.
 width :: ( Box_ box point, Point_ point d r
          , 0 < d
+         , OptCVector_ 2 r
          , OptVector_ d r
-         , OptVector_ d (IntervalFor box)
-         , HasComponents (VectorFamily' d (IntervalFor box))
-                         (VectorFamily' d r)
-         , ClosedInterval_ (IntervalFor box) r
+         , OptVector_ d (ClosedInterval r)
+         , HasComponents (Vector d (ClosedInterval r))
+                         (Vector d r)
          , Num r
          ) => box -> r
 width  = widthIn @0
@@ -153,11 +147,11 @@ width  = widthIn @0
 -- | get the height of a rectangle
 height :: ( Box_ box point, Point_ point d r
          , 1 < d
+         , OptCVector_ 2 r
          , OptVector_ d r
-         , OptVector_ d (IntervalFor box)
-         , HasComponents (VectorFamily' d (IntervalFor box))
-                         (VectorFamily' d r)
-         , ClosedInterval_ (IntervalFor box) r
+         , OptVector_ d (ClosedInterval r)
+         , HasComponents (Vector d (ClosedInterval r))
+                         (Vector d r)
          , Num r
          ) => box -> r
 height = widthIn @1
