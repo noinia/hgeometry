5e48c02e3faf6c3b6d0f2822db28a35b8be55620
Frank Staals <frank@fstaals.net>
2022-12-06 23:13:28 +0100

    additional optimal vector types

--- hgeometry-vector/optimal_src/Optimal.hs
+++ hgeometry-vector/optimal_src/Optimal.hs
@@ -27,6 +27,7 @@ import           Optimal.Wrap
 
 -- type instance VectorFamily 2 Int = V2.V2 Int
 
+
 type instance VectorFamily 2 Int    = Int.Unpacked.Vec2
 type instance VectorFamily 3 Int    = Int.Unpacked.Vec3
 type instance VectorFamily 4 Int    = Int.Unpacked.Vec4
@@ -39,24 +40,16 @@ type instance VectorFamily 2 Float  = Float.Unpacked.Vec2
 type instance VectorFamily 3 Float  = Float.Unpacked.Vec3
 type instance VectorFamily 4 Float  = Float.Unpacked.Vec4
 
+-- hmm, so now how do I represent a larger vecotr of Ints. It is not
+-- easy to specify generically that I want Float.Unpacked.Large :(
+-- type instance VectorFamily d Int    = Int.Unpacked.VecLarge
+
+
+type instance VectorFamily d ()             = Boxed.Vector d () -- not sure about this one...
 type instance VectorFamily d Integer        = Boxed.Vector d Integer
 type instance VectorFamily d Rational       = Boxed.Vector d Rational
+
+type instance VectorFamily d [r]            = Boxed.Vector d [r]
+type instance VectorFamily d (Maybe r)      = Boxed.Vector d (Maybe r)
 type instance VectorFamily d (RealNumber n) = Boxed.Vector d (RealNumber n)
 type instance VectorFamily d (Vector d' r)  = Boxed.Vector d (Vector d' r)
-
--- type instance VectorFamily 2 (RealNumber n) = Boxed.Vector 2 (RealNumber n)
--- type instance VectorFamily 3 (RealNumber n) = Boxed.Vector 3 (RealNumber n)
--- type instance VectorFamily 4 (RealNumber n) = Boxed.Vector 4 (RealNumber n)
--- type instance VectorFamily 5 (RealNumber n) = Boxed.Vector 5 (RealNumber n)
--- type instance VectorFamily 6 (RealNumber n) = Boxed.Vector 6 (RealNumber n)
--- type instance VectorFamily 7 (RealNumber n) = Boxed.Vector 7 (RealNumber n)
--- type instance VectorFamily 8 (RealNumber n) = Boxed.Vector 8 (RealNumber n)
--- type instance VectorFamily 9 (RealNumber n) = Boxed.Vector 9 (RealNumber n)
-
-
--- type instance VectorFamily 2 (Vector d' r) = Boxed.Vector 2 (Vector d' r)
--- type instance VectorFamily 3 (Vector d' r) = Boxed.Vector 3 (Vector d' r)
--- type instance VectorFamily 4 (Vector d' r) = Boxed.Vector 4 (Vector d' r)
--- type instance VectorFamily 5 (Vector d' r) = Boxed.Vector 5 (Vector d' r)
--- type instance VectorFamily 6 (Vector d' r) = Boxed.Vector 6 (Vector d' r)
--- type instance VectorFamily 7 (Vector d' r) = Boxed.Vector 7 (Vector d' r)
