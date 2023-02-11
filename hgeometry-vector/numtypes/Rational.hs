cc95596474a1e0e1be8613c8b2197a519b4923dd
Frank Staals <frank@fstaals.net>
2023-02-11 18:36:45 +0100

    unboxed stuff

---
+++ hgeometry-vector/numtypes/Rational.hs
@@ -0,0 +1,14 @@
+--------------------------------------------------------------------------------
+-- |
+-- Module      :  Rational
+-- Copyright   :  (C) Frank Staals
+-- License     :  see the LICENSE file
+-- Maintainer  :  Frank Staals
+--
+-- Defines the Real-number type
+--
+--------------------------------------------------------------------------------
+module Rational where
+
+-- | The Real number type
+type R = Rational
