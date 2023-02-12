65fd0efed159b2748d3af568c8080d0f9872559f
Frank Staals <frank@fstaals.net>
2023-02-12 11:15:34 +0100

    rationals tests :)

--- hgeometry-vector/test-rational/RationalSpec.hs
+++ hgeometry-vector/test-rational/RationalSpec.hs
@@ -1,4 +1,4 @@
-module DoubleSpec where
+module RationalSpec where
 
 import HGeometry.Vector.Unpacked
 import HGeometry.Vector.Class
@@ -13,6 +13,6 @@ myVec = Vector2 5 11
 spec :: Spec
 spec = describe "double spec" $ do
          it "showtest" $
-           show myVec `shouldBe` "Vector2 5.0 11.0"
+           show myVec `shouldBe` "Vector2 (5 % 1) (11 % 1)"
          it "division" $
            myVec ^/ 2 `shouldBe` Vector2 2.5 5.5
