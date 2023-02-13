bf2c278b6bbd11b633b2fe20581c2ee3012a11cd
Frank Staals <frank@fstaals.net>
2023-02-14 00:17:43 +0100

    convenience methods

--- hgeometry-vector/src-shared/HGeometry/Vector/Class.hs
+++ hgeometry-vector/src-shared/HGeometry/Vector/Class.hs
@@ -18,7 +18,10 @@ module HGeometry.Vector.Class
   , vectorFromList
   , Additive_(..), negated, (*^), (^*), (^/), sumV, basis, unit
   , foldMapZip
+  , sameDirection
+  , scalarMultiple
   , Metric_(..)
+  , isScalarMultipleOf
   ) where
 
 import           Control.Lens
@@ -29,6 +32,7 @@ import           Data.Functor.Classes (readData, readUnaryWith)
 import           Data.Kind (Type)
 import qualified Data.List as List
 import           Data.Proxy
+import           Data.Semigroup
 import           Data.Type.Ord
 import           GHC.TypeLits (Nat, KnownNat, natVal)
 -- import           GHC.Generics
@@ -247,17 +251,21 @@ class VectorLike_ vector => Additive_ vector where
           -> f vector
 
 -- | "zip through the two vectors", folding over the result.
+--
+-- as an example, we can implement the dot product of two vectors u and v using:
+--
+-- >>> let myDot u v = getSum $ foldMapZip (\x x' -> Sum $ x * x') u v
+-- >>> myDot (Vector3 1 2 3) (Vector3 10 20 30)
+-- 140
 foldMapZip       :: (Semigroup m, Additive_ vector)
                  => (IxValue vector -> IxValue vector -> m) -> vector -> vector -> m
 foldMapZip f u v = getConst $ liftI2A (\x x' -> Const $ f x x') u v
-
-
-
+{-# INLINE foldMapZip #-}
 
 -- | unit vector
 unit :: forall vector. (Additive_ vector, Num (IxValue vector)) => vector
 unit = over components (const 1) (zero @vector)
-{-# INLINABLE unit #-}
+{-# INLINE unit #-}
 
 -- | negate v
 negated :: (Num (IxValue vector), VectorLike_ vector) => vector -> vector
@@ -518,3 +526,91 @@ instance ( KnownNat d
 
 instance ( KnownNat d, UV.Unbox r, VectorLike_ (Vector d r)
          ) => UV.Unbox (Vector d r) where
+
+
+--------------------------------------------------------------------------------
+
+--------------------------------------------------------------------------------
+-- * Additional Functionality
+-- not sure if I should specialize these
+
+-- | 'isScalarmultipleof u v' test if v is a scalar multiple of u.
+--
+-- >>> Vector2 1 1 `isScalarMultipleOf` Vector2 10 10
+-- True
+-- >>> Vector3 1 1 2 `isScalarMultipleOf` Vector3 10 10 20
+-- True
+-- >>> Vector2 1 1 `isScalarMultipleOf` Vector2 10 1
+-- False
+-- >>> Vector2 1 1 `isScalarMultipleOf` Vector2 (-1) (-1)
+-- True
+-- >>> Vector2 1 1 `isScalarMultipleOf` Vector2 11.1 11.1
+-- True
+-- >>> Vector2 1 1 `isScalarMultipleOf` Vector2 11.1 11.2
+-- False
+-- >>> Vector2 2 1 `isScalarMultipleOf` Vector2 11.1 11.2
+-- False
+-- >>> Vector2 2 1 `isScalarMultipleOf` Vector2 4 2
+-- True
+-- >>> Vector2 2 1 `isScalarMultipleOf` Vector2 4 0
+-- False
+-- >>> Vector3 2 1 0 `isScalarMultipleOf` Vector3 4 0 5
+-- False
+-- >>> Vector3 0 0 0 `isScalarMultipleOf` Vector3 4 0 5
+-- True
+isScalarMultipleOf       :: ( Eq (IxValue vector), Fractional (IxValue vector)
+                            , Metric_ vector
+                            )
+                         => vector -> vector  -> Bool
+u `isScalarMultipleOf` v = let d = u `dot` v
+                               num = quadrance u * quadrance v
+                           in num == 0 || num == d*d
+{-# INLINE isScalarMultipleOf #-}
+
+-- | Given two colinar vectors, u and v, test if they point in the same direction, i.e.
+-- iff scalarMultiple' u v == Just lambda, with lambda > 0
+--
+-- pre: u and v are colinear, u and v are non-zero
+sameDirection     :: ( Additive_ vector
+                     , Num (IxValue vector), Eq (IxValue vector)
+                     ) => vector -> vector -> Bool
+sameDirection u v = getAll $ foldMapZip (\ux vx -> All $ signum ux == signum vx) u v
+{-# INLINE sameDirection #-}
+
+-- | scalarMultiple u v computes the scalar labmda s.t. v = lambda * u (if it exists)
+scalarMultiple     :: (Eq (IxValue vector), Fractional (IxValue vector), Additive_ vector)
+                   => vector -> vector -> Maybe (IxValue vector)
+scalarMultiple u v
+      | allZero u || allZero v = Just 0
+      | otherwise              = scalarMultiple' u v
+  where
+    allZero = allOf components (== 0)
+{-# INLINABLE scalarMultiple #-}
+
+data ScalarMultiple r = No | Maybe | Yes r deriving (Eq,Show)
+
+instance Eq r => Semigroup (ScalarMultiple r) where
+  No      <> _       = No
+  _       <> No      = No
+  Maybe   <> x       = x
+  x       <> Maybe   = x
+  (Yes x) <> (Yes y)
+     | x == y               = Yes x
+     | otherwise            = No
+
+instance Eq r => Monoid (ScalarMultiple r) where
+  mempty = Maybe
+  mappend = (<>)
+
+-- | Actual implementation of scalarMultiple
+scalarMultiple'      :: (Eq (IxValue vector), Fractional (IxValue vector), Additive_ vector)
+                     => vector -> vector -> Maybe (IxValue vector)
+scalarMultiple' u v = g $ foldMapZip f u v
+  where
+    f 0  0  = Maybe -- we don't know lambda yet, but it may still be a scalar mult.
+    f _  0  = No      -- Not a scalar multiple
+    f ui vi = Yes $ ui / vi -- can still be a scalar multiple
+
+    g No      = Nothing
+    g Maybe   = error "scalarMultiple': found a Maybe, which means the vectors either have length zero, or one of them is all Zero!"
+    g (Yes x) = Just x
