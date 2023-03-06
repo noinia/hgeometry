07978421df722d2fbca4f4421f9aa3be5f4f3faa
Frank Staals <frank@fstaals.net>
2023-03-06 23:27:20 +0100

    direction and line

---
+++ hgeometry-kernel/src/HGeometry/Direction.hs
@@ -0,0 +1,59 @@
+--------------------------------------------------------------------------------
+-- |
+-- Module      :  HGeometry.Direction
+-- Copyright   :  (C) Frank Staals
+-- License     :  see the LICENSE file
+-- Maintainer  :  Frank Staals
+--
+-- Cardinal and Intercardinal directions.
+--
+--------------------------------------------------------------------------------
+module HGeometry.Direction
+  ( CardinalDirection(..)
+    -- , _North, _East, _South, _West
+  , oppositeDirection
+
+  , InterCardinalDirection(..)
+    -- , _NorthWest, _NorthEast, _SouthEast, _SouthWest
+
+  , interCardinalsOf
+  ) where
+
+import GHC.Generics (Generic)
+import HGeometry.Vector
+
+--------------------------------------------------------------------------------
+
+-- | The four cardinal directions.
+data CardinalDirection = North | East | South | West deriving (Show,Read,Eq,Ord,Enum,Bounded)
+-- makePrisms ''CardinalDirection
+
+--------------------------------------------------------------------------------
+-- * Functions on Cardinal Directions
+
+-- | Computes the direction opposite to the given one.
+oppositeDirection :: CardinalDirection -> CardinalDirection
+oppositeDirection = \case
+  North -> South
+  East  -> West
+  South -> North
+  West  -> East
+
+--------------------------------------------------------------------------------
+
+-- | Intercardinal directions
+data InterCardinalDirection = NorthWest | NorthEast | SouthEast | SouthWest
+  deriving (Show,Read,Eq,Ord,Enum,Generic)
+-- makePrisms ''InterCardinalDirection
+
+--------------------------------------------------------------------------------
+-- * Functions on InterCardinal Directions
+
+-- | Get the two intercardinal directions, in increasing order,
+-- corresponding to the cardinal direction.
+interCardinalsOf :: CardinalDirection -> Vector 2 InterCardinalDirection
+interCardinalsOf = \case
+  North -> Vector2 NorthWest NorthEast
+  East  -> Vector2 NorthEast SouthEast
+  South -> Vector2 SouthEast SouthWest
+  West  -> Vector2 SouthWest NorthWest
---
+++ hgeometry-kernel/src/HGeometry/Line.hs
@@ -0,0 +1,23 @@
+--------------------------------------------------------------------------------
+-- |
+-- Module      :  HGeometry.Line
+-- Copyright   :  (C) Frank Staals
+-- License     :  see the LICENSE file
+-- Maintainer  :  Frank Staals
+--
+-- Lines in d-dimensional space.
+--
+--------------------------------------------------------------------------------
+module HGeometry.Line
+  ( module HGeometry.Line.Class
+  , module HGeometry.Line.LineEQ
+  , module HGeometry.Line.PointAndVector
+  , LineLineIntersection(..)
+  ) where
+
+import HGeometry.Line.Class
+import HGeometry.Line.LineEQ
+import HGeometry.Line.PointAndVector
+import HGeometry.Line.Intersection
+
+--------------------------------------------------------------------------------
