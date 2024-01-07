8d2e2af9f7776cef684705bbc70fba7b5705b2be
Frank Staals <frank@fstaals.net>
2024-01-07 22:16:04 +0100

    fixing haddock script

--- haddock.sh
+++ haddock.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-cabal haddock all > haddock.raw.txt
+cabal haddock-project > haddock.raw.txt
 grep ") in " haddock.raw.txt | sort -hr > haddock.txt
 COVERAGE=$(awk '{good += $3; total += $5} END {printf "%.0f", good/total*100}' haddock.txt)
 echo "\
