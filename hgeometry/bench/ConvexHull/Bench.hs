fdc930b573ecd935efb5fa9473e30d7e99a75cc3
Frank Staals <frank@fstaals.net>
2024-02-21 21:54:58 +0100

    cleaned up the convex hull benchmarks

--- hgeometry/bench/ConvexHull/Bench.hs
+++ hgeometry/bench/ConvexHull/Bench.hs
@@ -4,10 +4,17 @@ module ConvexHull.Bench (
   -- , runProfile
   ) where
 
-import           System.Random
-import           HGeometry.Point
+import           Control.DeepSeq
+import           Control.Lens (over)
 import           Data.List.NonEmpty (NonEmpty (..))
+import qualified Data.List.NonEmpty as NonEmpty
+import qualified HGeometry.ConvexHull.DivideAndConquer as DivideAndConquer
+import qualified HGeometry.ConvexHull.GrahamScan as GrahamScan
+import qualified HGeometry.ConvexHull.JarvisMarch as JarvisMarch
+import qualified HGeometry.ConvexHull.QuickHull as QuickHull
 import           HGeometry.Number.Real.Rational
+import           HGeometry.Point
+import           System.Random
 import           Test.Tasty.Bench
 import           Util
 
@@ -23,84 +30,50 @@ gen = genByName "convex hull"
 
 runBenchmark :: IO ()
 runBenchmark = do
-    let intPts = randomPoints @(Point 2 Int) gen 100000
+    let intPts = randomPoints @(Point 2 Integer) gen 1000 -- generate points in a 1000x1000 square
     defaultMain [ benchmark intPts
                 ]
+
+genPts       :: forall r. (Num r, NFData r)
+             => NonEmpty (Point 2 Integer) -> Int -> NonEmpty (Point 2 r)
+genPts pts n = pts' `deepseq` (NonEmpty.fromList pts')
+  where
+    pts' :: [Point 2 r]
+    pts' = over coordinates fromInteger <$> NonEmpty.take n pts
+
 
 -- asPoints   :: [Point 2 Int] -> [point]
 -- asPoints n = NonEmpty.fromList . take n
 
 -- | Benchmark building the convexHull
-benchmark    :: NonEmpty (Point 2 Int) -> Benchmark
-benchmark intPts = bgroup "ConvexHull" $ []
-  --     [ bgroup ("1e"++show i ++ "/RealNumber") (convexHullFractional $ evalRand (genPts @R n) gen)
-  --     | i <- [3, 4::Int]
-  --     , let n = 10^i
-  --     ] ++
-  --     [ bgroup ("1e"++show i ++ "/Int") (convexHullNum $ evalRand (genPts @Int n) gen)
-  --     | i <- [4, 5::Int]
-  --     , let n = 10^i
-  --     ] ++
-  --     [ bgroup ("1e"++show i ++ "/Double") (convexHullFractional $ evalRand (genPts @Double n) gen)
-  --     | i <- [4, 5::Int]
-  --     , let n = 10^i ]
-  -- where
-  --   convexHullFractional pts =
-  --               [ bench "GrahamScan" $ nf GrahamScan.convexHull pts
-  --               , bench "DivideAndConquer" $ nf DivideAndConquer.convexHull pts
-  --               , bench "QuickHull" $ nf QuickHull.convexHull pts
-  --               , bench "JarvisMarch" $ nf JarvisMarch.convexHull pts
-  --               ]
-  --   convexHullNum pts =
-  --               [ bench "GrahamScan" $ nf GrahamScan.convexHull pts
-  --               , bench "DivideAndConquer" $ nf DivideAndConquer.convexHull pts
-  --               , bench "JarvisMarch" $ nf JarvisMarch.convexHull pts
-  --               ]
+benchmark       :: NonEmpty (Point 2 Integer) -> Benchmark
+benchmark inPts = bgroup "ConvexHull" $
+      [ bgroup ("1e"++show i ++ "/RealNumber") (convexHullFractional $ genPts @R inPts n)
+      | i <- sizes
+      , let n = 10^i
+      ] ++
+      [ bgroup ("1e"++show i ++ "/Int") (convexHullNum $ genPts @Int inPts n)
+      | i <- sizes
+      , let n = 10^i
+      ] ++
+      [ bgroup ("1e"++show i ++ "/Double") (convexHullFractional $ genPts @Double inPts n)
+      | i <- sizes
+      , let n = 10^i
+      ]
+  where
+    sizes :: [Int]
+    sizes = [2, 3, 4]
 
-
-
--- -- | Benchmark building the convexHull
--- benchmark                  :: NFData point
---                            => String -- ^ name
---                            -> [Int] -- ^ sizes i s.t. size is actually 10^i
---                            -- -> (NonEmpty point -> [Benchmark])  -- ^ function to benchmark
---                            -> NonEmpty point -- ^ sufficiently log list of points
---                            -> Benchmark
--- benchmark name sizes f pts = bgroup "ConvexHull" $
---     -- [ bgroup ("1e" <> show i <> name) (f . force $ take' n pts)
---     -- | i <- sizes
---     -- , let n = 10^i
---     -- ] <>
---     [ bgroup ("1e"++show i ++ "/RealNumber") (convexHullFractional $ genPts @R n)
---     | i <- [3, 4::Int]
---     , let n = 10^i
---     ] ++
---     [ bgroup ("1e"++show i ++ "/Int:+()") (convexHullNum $ fmap ext $ genPts @Int n)
---     | i <- [4, 5::Int]
---     , let n = 10^i
---     ] ++
---     [ bgroup ("1e"++show i ++ "/Int") (convexHullNum $ genPts @Int n)
---     | i <- [4, 5::Int]
---     , let n = 10^i
---     ] ++
---       -- [ bgroup ("1e"++show i ++ "/SafeDouble") (convexHullFractional $ genPts @SafeDouble n)
---       -- | i <- [4, 5::Int]
---       -- , let n = 10^i
---       -- ] ++
---     [ bgroup ("1e"++show i ++ "/Double") (convexHullFractional $ genPts @Double n)
---     | i <- [4, 5::Int]
---     , let n = 10^i ]
---   where
---     convexHullFractional (force -> pts) =
---                 [ bench "GrahamScan" $ nf GrahamScan.convexHull pts
---                 , bench "DivideAndConquer" $ nf DivideAndConquer.convexHull pts
---                 , bench "QuickHull" $ nf QuickHull.convexHull pts
---                 , bench "JarvisMarch" $ nf JarvisMarch.convexHull pts
---                 ]
---     convexHullNum (force -> pts) = let pts' = force (GrahamV2.fromP <$> pts) in
---                 [ bench "GrahamScan" $ nf GrahamScan.convexHull pts
---                 , bench "GrahamBaseLine" $ nf GrahamV2.convexHull pts'
---                 , bench "DivideAndConquer" $ nf DivideAndConquer.convexHull pts
---                 , bench "JarvisMarch" $ nf JarvisMarch.convexHull pts
---                 ]
---     genPts =
+    convexHullFractional pts =
+                [ bench "NonEmpty.sort" $ nf NonEmpty.sort pts
+                , bench "GrahamScan" $ nf GrahamScan.convexHull pts
+                , bench "DivideAndConquer" $ nf DivideAndConquer.convexHull pts
+                , bench "QuickHull" $ nf QuickHull.convexHull pts
+                , bench "JarvisMarch" $ nf JarvisMarch.convexHull pts
+                ]
+    convexHullNum pts =
+                [ bench "NonEmpty.sort" $ nf NonEmpty.sort pts
+                , bench "GrahamScan" $ nf GrahamScan.convexHull pts
+                , bench "DivideAndConquer" $ nf DivideAndConquer.convexHull pts
+                , bench "JarvisMarch" $ nf JarvisMarch.convexHull pts
+                ]
