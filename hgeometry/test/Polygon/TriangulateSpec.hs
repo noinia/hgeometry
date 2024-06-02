2df9d50ee3cb8edbe913fbd40e419fd414f2ece4
Frank Staals <frank@fstaals.net>
2024-06-02 15:52:23 +0200

    more testing

--- hgeometry/test/Polygon/TriangulateSpec.hs
+++ hgeometry/test/Polygon/TriangulateSpec.hs
@@ -2,6 +2,7 @@
 module Polygon.TriangulateSpec (spec) where
 
 import           Control.Lens
+import           Data.Maybe (fromJust)
 import           HGeometry
 import           HGeometry.Ext
 import           HGeometry.Number.Real.Rational
@@ -51,6 +52,23 @@ spec = do
           trigs = graphPolygons g
       in all isTriangle trigs
 
+  it "buggy polygon2 " $
+      let g = triangulate @() buggyPolygon2
+          trigs = graphPolygons g
+      in all isTriangle trigs `shouldBe` True
+
+  it "buggy polygon2 not simplified " $
+      let g = triangulate @() buggyNotSimplified
+          trigs = graphPolygons g
+      in all isTriangle trigs `shouldBe` True
+
+
+
+  it "buggy polygon2 simplified " $
+      let g = triangulate @() buggySimplified
+          trigs = graphPolygons g
+      in all isTriangle trigs `shouldBe` True
+
   -- prop "creating the graph does not create additional diagionals" $
   --   \poly ->
   --     let gr     = triangulate @() poly
@@ -69,3 +87,52 @@ graphPolygons    :: (Ord r, Num r, Point_ point 2 r)
                  => PlaneGraph s point PolygonEdgeType PolygonFaceData
                  -> [SimplePolygon (Point 2 r)]
 graphPolygons gr = map (&vertices %~ view (core.asPoint)) $ gr^..interiorFacePolygons
+
+buggyPolygon2 :: SimplePolygon (Point 2 R)
+buggyPolygon2 = read "SimplePolygon [Point2 2589.22 470.87,Point2 2588.68 470.97,Point2 2587.88 471.15,Point2 2587.5 471.28,Point2 2586.96 471.57,Point2 2586.46 471.9,Point2 2586.4 472.02,Point2 2586.87 472.14,Point2 2587.24 472.17,Point2 2588.02 472.06,Point2 2588.79 471.9,Point2 2589.55 471.71,Point2 2590.7 471.38,Point2 2591.07 471.26,Point2 2591.17 471.12,Point2 2591.09 470.96,Point2 2590.87 470.84,Point2 2590.42 470.81,Point2 2589.98 470.8]"
+
+buggySimplified :: SimplePolygon (Point 2 R)
+buggySimplified = fromJust . fromPoints $
+  [Point2 111.568 9.53226,
+   Point2 92.1954 13.1198,
+    Point2 63.4953 19.5773,
+    Point2 49.8627 24.2411,
+    Point2 30.4901 34.6449,
+    Point2 12.5525 46.4837,
+    Point2 10.4 50.7887,
+    Point2 27.2613 55.0937,
+    Point2 40.5352 56.17,
+    Point2 68.5178 52.2237,
+    Point2 96.1417 46.4837,
+    Point2 123.407 39.6674,
+    Point2 164.663 27.8286,
+    Point2 177.937 23.5236,
+    Point2 181.525 18.5011,
+    Point2 178.655 12.761,
+    Point2 170.762 8.456,
+    Point2 154.618 7.37975]
+
+
+buggyNotSimplified :: SimplePolygon (Point 2 R)
+buggyNotSimplified = fromJust . fromPoints $
+  [
+    Point2 2589.22 470.87,
+    Point2 2588.68 470.97,
+    Point2 2587.88 471.15,
+    Point2 2587.5  471.28,
+    Point2 2586.96 471.57,
+    Point2 2586.46 471.9 ,
+    Point2 2586.4  472.02,
+    Point2 2586.87 472.14,
+    Point2 2587.24 472.17,
+    Point2 2588.02 472.06,
+    Point2 2588.79 471.9 ,
+    Point2 2589.55 471.71,
+    Point2 2590.7  471.38,
+    Point2 2591.07 471.26,
+    Point2 2591.17 471.12,
+    Point2 2591.09 470.96,
+    Point2 2590.87 470.84,
+    Point2 2590.42 470.81,
+    Point2 2589.98 470.8
+    ]
