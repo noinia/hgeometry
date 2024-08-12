70513c89a3950ab163949663ea924a664b09e197
Frank Staals <frank@fstaals.net>
2024-08-12 16:23:50 +0200

    bunch of instances for the polygon type

--- hgeometry/geojson/src/HGeometry/GeoJSON.hs
+++ hgeometry/geojson/src/HGeometry/GeoJSON.hs
@@ -6,10 +6,23 @@ module HGeometry.GeoJSON
   ) where
 
 import           Control.Lens
+import           Data.Bifunctor
+import           Data.Coerce
+import           Data.Foldable1
+import           Data.Functor.Apply (Apply, (<.*>), MaybeApply(..))
 import           Data.Geospatial
 import           Data.LinearRing
+import           Data.Maybe (fromMaybe)
+import           Data.Semigroup.Traversable
+import           Data.Sequence (Seq(..))
+import qualified Data.Sequence as Seq
+import           GHC.Generics (Generic)
+import           HGeometry.Cyclic
 import           HGeometry.Ext
+import           HGeometry.Foldable.Util
 import           HGeometry.Point.Class
+import           HGeometry.Polygon.Class
+import           HGeometry.Polygon.Simple
 import           HGeometry.Properties
 import           HGeometry.Vector
 
@@ -91,14 +104,148 @@ instance Wrapped GeoPolygon
 instance Rewrapped GeoPolygon GeoPolygon
 
 --------------------------------------------------------------------------------
-type instance Index   (LinearRing a) = Int
-type instance IxValue (LinearRing a) = a
 
--- instance Ixed (LinearRing a) where
+newtype Seq1 a = Seq1 (Seq a)
+  deriving newtype (Show,Eq,Ord,Functor,Foldable)
+  deriving Generic
 
-{-
+_Seq1Seq :: Iso (Seq1 a) (Seq1 b) (Seq a) (Seq b)
+_Seq1Seq = coerced
+
+instance Traversable Seq1 where
+  traverse f (Seq1 s) = Seq1 <$> traverse f s
+
+type instance Index   (Seq1 a) = Int -- Index   (Seq a)
+type instance IxValue (Seq1 a) = a   -- IxValue (Seq a)
+
+instance Ixed (Seq1 a) where
+  ix i = _Seq1Seq . ix i
+
+instance Wrapped   (Seq1 a) -- where
+  -- type Unwrapped (Seq1 a) = Seq a
+  -- _Wrapped' = _Seq1Seq
+
+instance Rewrapped (Seq1 a) (Seq1 b)
+
+instance FunctorWithIndex Int Seq1 where
+  imap f (Seq1 s) = Seq1 $ imap f s
+
+instance FoldableWithIndex Int Seq1 where
+  ifoldMap f (Seq1 s) = ifoldMap f s
+
+instance TraversableWithIndex Int Seq1 where
+  itraverse f (Seq1 s) = Seq1 <$> itraverse f s
+
+instance Foldable1 Seq1 where
+  foldMap1 = foldMap1Default
+instance Traversable1 Seq1 where
+  traverse1 f (Seq1 s) = Seq1 <$> traverse1Seq f s
+
+instance HasFromFoldable1 Seq1 where
+  fromNonEmpty = Seq1 . fromNonEmpty
+
+-- | Traverse a non-empty sequence
+traverse1Seq   :: Apply f => (a -> f b) -> Seq a -> f (Seq b)
+traverse1Seq f = \case
+  Seq.Empty -> error "traverse1Seq: precondition violated"
+  (x :<| s) -> (:<|) <$> f x <.*> traverse1Maybe f s
+
+-- | Indexed traversal of a non-empty Sequence
+traversed1Seq :: IndexedTraversal1 Int (Seq a) (Seq b) a b
+traversed1Seq = conjoined traverse1Seq (indexing traverse1Seq)
+
+
+
+
+
+
+
+-- type instance Index   (LinearRing a) = Int
+-- type instance IxValue (LinearRing a) = a
+
+--------------------------------------------------------------------------------
+
+-- | pre: the sequence has at leat 3 elements
+_RingSeq :: (Eq b, Show b) => Iso (LinearRing a) (LinearRing b) (Seq a) (Seq b)
+_RingSeq = iso ringToSeq (fromMaybe err . seqToRing)
+  where
+    err = error "_RingSeq: failed"
+
+ringToSeq   :: LinearRing a -> Seq a
+ringToSeq r = let (s :|> _) = toSeq r in s
+
+seqToRing             :: (Eq b, Show b) => Seq b -> Maybe (LinearRing b)
+seqToRing s@(x :<| _) = either (const Nothing) Just . fromSeq $ s |> x
+
+-- | pre: the sequence has at least three elements
+_RingSeq1 :: (Eq b, Show b) => Iso (LinearRing a) (LinearRing b) (Seq1 a) (Seq1 b)
+_RingSeq1 = _RingSeq . from _Seq1Seq
+
+
 -- traverse1Ring :: Apply f => (a -> f b) -> LinearRing a -> f (LinearRing b)
 -- traverse1Ring f
+
+-- | convert a ring into a polygon
+--
+-- pre: the ring should be in CCW order.
+--
+-- O(1)
+uncheckedRingAsPolygon :: LinearRing GeoPositionWithoutCRS
+                       -> SimplePolygonF Seq GeoPositionWithoutCRS
+uncheckedRingAsPolygon = MkSimplePolygon . ringToSeq
+
+-- uncheckedRingAsPolygon' :: LinearRing GeoPositionWithoutCRS
+--                         -> SimplePolygonF Seq GeoPositionWithoutCRS'
+-- uncheckedRingAsPolygon' = uncheckedRingAsPolygon
+
+--   MkSimplePolygon . ringToSeq
+
+_RingAsSimplePolygon :: Iso' (LinearRing GeoPositionWithoutCRS)
+                             (SimplePolygonF Seq1 GeoPositionWithoutCRS)
+_RingAsSimplePolygon = _RingSeq1 . from _SimplePolygonContainer
+
+_SimplePolygonContainer :: Iso (SimplePolygonF Seq1 point) (SimplePolygonF Seq1 point')
+                               (Seq1 point) (Seq1 point')
+_SimplePolygonContainer = coerced
+
+
+-- instance HasDirectedTraversals Seq where
+
+instance HasDirectedTraversals Seq1 where
+  traverseRightFrom i = conjoined traverse' (itraverse' . indexed)
+    where
+      combine sb' sa' = Seq1 (sa' <> sb')
+
+      traverse'             :: Apply f => (a -> f b) -> Seq1 a -> f (Seq1 b)
+      traverse' f (Seq1 s)  = let (sa,sb) = Seq.splitAt i s
+                              in combine <$>  traverse1Seq   f sb
+                                         <.*> traverse1Maybe f sa
+      itraverse'            :: Apply f => (Int -> a -> f b) -> Seq1 a -> f (Seq1 b)
+      itraverse' f (Seq1 s) = let (sa,sb) = Seq.splitAt i s
+                              in combine  <$>  itraverse1Seq  (\j x -> f (i+j) x) sb
+                                          <.*> itraverseMaybe f                   sa
+
+  traverseLeftFrom  i = conjoined traverse' (itraverse' . indexed)
+    where
+      combine sb' sa' = Seq1 (Seq.reverse sa' <> Seq.reverse sb')
+
+      traverse'             :: Apply f => (a -> f b) -> Seq1 a -> f (Seq1 b)
+      traverse' f (Seq1 s)  = let (sa,sb) = Seq.splitAt i s
+                              in combine <$>  traverse1Seq   f (Seq.reverse sa)
+                                         <.*> traverse1Maybe f (Seq.reverse sb)
+      itraverse'            :: Apply f => (Int -> a -> f b) -> Seq1 a -> f (Seq1 b)
+      itraverse' f (Seq1 s) = let (sa,sb) = Seq.splitAt (i+1) s
+                              in combine <$>  itraverse1Seq  (\j x -> f (i+j) x) (Seq.reverse sa)
+                                         <.*> itraverseMaybe f                   (Seq.reverse sb)
+
+
+
+itraverse1Seq :: Apply f => (Int -> a -> f b) -> Seq a -> f (Seq b)
+itraverse1Seq = itraverseOf traversed1Seq
+
+itraverseMaybe   :: (Apply f, TraversableWithIndex i t)
+                 => (i -> a -> f b) -> t a -> MaybeApply f (t b)
+itraverseMaybe f = itraverse (\i -> MaybeApply . Left . f i)
 
 
 --------------------------------------------------------------------------------
@@ -107,15 +254,16 @@ instance HasVertices' GeoPolygon where
   type VertexIx GeoPolygon = (Int,Int) -- first one is the ringId
   type Vertex   GeoPolygon = GeoPositionWithoutCRS'
 
-  vertexAt (i,j) = _Wrapped .> iix i <.> iix j <. _GeoPositionWithoutCRS
+  vertexAt (i,j) = _Wrapped .> iix i <.> _RingAsSimplePolygon .> vertexAt j
+                 <. _GeoPositionWithoutCRS
 
-  numVertices (GeoPolygon ss) = foldl' (\a s -> a + length s - 1) 0 ss
-  -- the ring incluces a copy of the first element, so it overestimates the length by 1
+  numVertices (GeoPolygon ss) = foldl' (\a s -> a + ringLength s - 1) 0 ss
+  -- -- the ring incluces a copy of the first element, so it overestimates the length by 1
 
 instance HasVertices GeoPolygon GeoPolygon where
-  -- (Indexable i p, Apply f) => p a (f b) -> s -> f t
-  -- vertices = conjoined
-
+  vertices = _Wrapped .> traversed1Seq <.> _RingAsSimplePolygon .> vertices
+          <. singular _GeoPositionWithoutCRS
+{-
 instance HasOuterBoundary GeoPolygon where
   outerBoundaryVertexAt v@(i,_)
     | i == 0    = vertexAt v
@@ -123,6 +271,7 @@ instance HasOuterBoundary GeoPolygon where
   -- the first ring is outer boundary apparently
   ccwOuterBoundaryFrom i = undefined
   cwOuterBoundaryFrom  i = undefined
+
 
 instance Polygon_ GeoPolygon GeoPositionWithoutCRS' Double where
   area = _
