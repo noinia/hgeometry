b4f7db60e0dd72894a8b319bbab132dd55285043
Frank Staals <991345+noinia@users.noreply.github.com>
2024-08-22 21:06:50 +0200

    Discrete Frechet Distance (#242)
    * discrete frechet distance using dynamic programming
    
    * generalized the types + some cleaning up

---
+++ hgeometry/src/HGeometry/PolyLine/Frechet/Discrete.hs
@@ -0,0 +1,60 @@
+--------------------------------------------------------------------------------
+-- |
+-- Module      :  HGeometry.PolyLine.Frechet.Discrete
+-- Copyright   :  (C) Frank Staals
+-- License     :  see the LICENSE file
+-- Maintainer  :  Frank Staals
+--
+-- Computes the discrete frechet distance
+--
+--------------------------------------------------------------------------------
+module HGeometry.PolyLine.Frechet.Discrete
+  ( frechetDistanceWith
+  ) where
+
+import Control.Lens
+import Data.Array
+import Hiraffe.Graph
+
+--------------------------------------------------------------------------------
+
+-- | Computes the discrete frechet distance with respect to the given distance function.
+--
+-- O(nm*(I+T)), where I is the time to index one of the vertices and T is the time to
+-- evaluate the distance function.
+frechetDistanceWith :: ( HasVertices' polyLine
+                       , HasVertices' polyLine'
+                       , Ord r
+                       , VertexIx polyLine ~ Int, VertexIx polyLine' ~ Int
+                       )
+                    => (Vertex polyLine -> Vertex polyLine' -> r)
+                    -> polyLine -> polyLine'
+                    -> r
+frechetDistanceWith dist' polyP polyQ = fd (n,m)
+  where
+    n = numVertices polyP - 1
+    m = numVertices polyQ - 1
+    dist i j = dist' (polyP^?!vertexAt i) (polyQ^?!vertexAt j)
+
+    -- use dynamic programming
+    fd = memo ((0, 0), (n, m)) fd'
+
+    fd' (0,0) = dist 0 0
+    fd' (0,q) = dist 0 q `max` fd (0, q-1)
+    fd' (p,0) = dist p 0 `max` fd (p-1, 0)
+    fd' (p,q) = dist p q `max` (fd (p, q-1) `min` fd (p-1, q) `min` fd (p-1, q-1))
+    -- we either move on the q side, on the p side, or both
+
+--------------------------------------------------------------------------------
+-- * Dynamic programming / Memoization stuff
+
+-- | Create a table
+tabulate       :: Ix i => (i,i) -> (i -> a) -> Array i a
+tabulate rng f = listArray rng (map f $ range rng)
+
+-- | Memoize a function using an Array
+memo     :: Ix i => (i,i) -> (i -> a) -> (i -> a)
+memo rng = (!) . tabulate rng
+
+-- see: for the idea of this memoization
+-- https://byorgey.wordpress.com/2023/06/06/dynamic-programming-in-haskell-automatic-memoization/
