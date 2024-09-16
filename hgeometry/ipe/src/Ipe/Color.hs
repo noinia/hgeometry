df64c4a286e216d0323402f179e952ddd76857c9
Frank Staals <frank@fstaals.net>
2024-09-16 22:49:15 +0200

    color names

--- hgeometry/ipe/src/Ipe/Color.hs
+++ hgeometry/ipe/src/Ipe/Color.hs
@@ -77,86 +77,114 @@ basicNamedColors = [ black
 --------------------------------------------------------------------------------
 -- * Basic Named colors
 
+-- | a color named 'black'
 black :: IpeColor r
 black = named "black"
 
+-- | a color named 'white'
 white :: IpeColor r
 white = named "white"
 
+-- | a color named 'red'
 red :: IpeColor r
 red = named "red"
 
+-- | a color named 'green'
 green :: IpeColor r
 green = named "green"
 
+-- | a color named 'blue'
 blue :: IpeColor r
 blue = named "blue"
 
+-- | a color named 'yellow'
 yellow :: IpeColor r
 yellow = named "yellow"
 
+-- | a color named 'orange'
 orange :: IpeColor r
 orange = named "orange"
 
+-- | a color named 'gold'
 gold :: IpeColor r
 gold = named "gold"
 
+-- | a color named 'pruple'
 purple :: IpeColor r
 purple = named "purple"
 
+-- | a color named 'gray'
 gray :: IpeColor r
 gray = named "gray"
 
+-- | a color named 'brown'
 brown :: IpeColor r
 brown = named "brown"
 
+-- | a color named 'navy'
 navy :: IpeColor r
 navy = named "navy"
 
+-- | a color named 'pink'
 pink :: IpeColor r
 pink = named "pink"
 
+-- | a color named 'seagreen'
 seagreen :: IpeColor r
 seagreen = named "seagreen"
 
+-- | a color named 'turquoise'
 turquoise :: IpeColor r
 turquoise = named "turquoise"
 
+-- | a color named 'violet'
 violet :: IpeColor r
 violet = named "violet"
 
+-- | a color named 'darkblue'
 darkblue :: IpeColor r
 darkblue = named "darkblue"
 
+-- | a color named 'darkcyan'
 darkcyan :: IpeColor r
 darkcyan = named "darkcyan"
 
+-- | a color named 'darkgray'
 darkgray :: IpeColor r
 darkgray = named "darkgray"
 
+-- | a color named 'darkgreen'
 darkgreen :: IpeColor r
 darkgreen = named "darkgreen"
 
+-- | a color named 'darkmagenta'
 darkmagenta :: IpeColor r
 darkmagenta = named "darkmagenta"
 
+-- | a color named 'darkorange'
 darkorange :: IpeColor r
 darkorange = named "darkorange"
 
+-- | a color named 'darkred'
 darkred :: IpeColor r
 darkred = named "darkred"
 
+-- | a color named 'lighbtblue'
 lightblue :: IpeColor r
 lightblue = named "lightblue"
 
+-- | a color named 'lightcyan'
 lightcyan :: IpeColor r
 lightcyan = named "lightcyan"
 
+-- | a color named 'lightgray'
 lightgray :: IpeColor r
 lightgray = named "lightgray"
 
+-- | a color named 'lightgreen'
 lightgreen :: IpeColor r
 lightgreen = named "lightgreen"
 
+-- | a color named 'lightyellow'
 lightyellow :: IpeColor r
 lightyellow = named "lightyellow"
