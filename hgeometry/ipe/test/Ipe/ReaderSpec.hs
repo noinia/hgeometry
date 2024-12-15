0189bbf78979d1e92f559aa295f7f9a6be7e2344
Frank Staals <frank@fstaals.net>
2024-12-15 21:54:37 +0100

    trying to extract an example

--- hgeometry/ipe/test/Ipe/ReaderSpec.hs
+++ hgeometry/ipe/test/Ipe/ReaderSpec.hs
@@ -1,14 +1,23 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes #-}
 module Ipe.ReaderSpec where
 
-import Data.ByteString (ByteString)
-import Data.Proxy
-import HGeometry.Point
-import Ipe
-import Ipe.Path
-import Ipe.Reader
-import Test.Hspec
+import           Control.Lens
+import           Control.Monad ((>=>))
+import           Data.ByteString (ByteString)
+import           Data.Proxy
+import           HGeometry.Ext
+import           HGeometry.Point
+import           HGeometry.Polygon
+import           HGeometry.Polygon.Simple
+import           Ipe
+import           Ipe.Path
+import           Ipe.Reader
+import qualified Paths_hgeometry as Paths
+import           System.OsPath
+import           Test.Hspec
 
+--------------------------------------------------------------------------------
 
 -- | specializes to use Double as Numtype
 fromIpeXML' :: IpeRead (t Double) => ByteString -> Either ConversionError (t Double)
@@ -38,6 +47,16 @@ spec = do
       --   `shouldBe`
 -- "Right (Path {_pathSegments = PolyLineSegment (PolyLine {_points = Seq2 (Point2 [128.0,656.0] :+ ()) (fromList [Point2 [224.0,768.0] :+ (),Point2 [304.0,624.0] :+ ()]) (Point2 [432.0,752.0] :+ ())}) :< fromList []})"
 
+    describe "parse simplepolygon in CCW order" $ do
+      let act fName = do
+                   fp   <- getDataFileName fName
+                   (pgs :: [SimplePolygon (Point 2 Rational)]) <- fmap (^.core)
+                                                                  <$> readAllFrom fp
+                   pure $ all isCounterClockwise pgs
+      it "simple" $
+        act [osp|ipe/test/clockwisepg.ipe|] `shouldReturn` True
+      it "world" $
+        act [osp|test-with-ipe/Polygon/Triangulation/world.ipe|] `shouldReturn` True
 
   where
     useTxt = "<use name=\"mark/disk(sx)\" pos=\"320 736\" size=\"normal\" stroke=\"black\"/>"
@@ -54,3 +73,27 @@ spec = do
           , LineTo $ Point2 304 624
           , LineTo $ Point2 432 752
           ]
+
+
+
+-- clockwisePGText :: ByteString
+-- clockwisePGText = mconcat ["<path stroke=\"black\">"
+--                       , "336 592 m"
+--                       , "208 560 l"
+--                       , "160 672 l"
+--                       , "320 720 l"
+--                       , "400 640 l"
+--                       , "h"
+--                       , "</path>"
+--                       ]
+
+
+
+-- | Get the data file as an OsPath
+getDataFileName :: OsPath -> IO OsPath
+getDataFileName = decodeFS >=> Paths.getDataFileName >=> encodeFS
+
+
+isCounterClockwise :: (Num r, Eq r, SimplePolygon_ simplePolygon point r)
+                   => simplePolygon -> Bool
+isCounterClockwise = (\x -> x == abs x) . signedArea2X
