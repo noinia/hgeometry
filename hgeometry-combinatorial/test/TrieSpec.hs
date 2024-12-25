26235e139cbe4dfa1a1b656b0dda63a2818ac296
Frank Staals <frank@fstaals.net>
2024-12-25 13:31:04 +0100

    KV instances

--- hgeometry-combinatorial/test/TrieSpec.hs
+++ hgeometry-combinatorial/test/TrieSpec.hs
@@ -4,6 +4,7 @@ import           Control.Lens
 import           Data.Foldable1
 import qualified Data.List.NonEmpty as NonEmpty
 import qualified Data.Map as Map
+import           HGeometry.Sequence.KV
 import           HGeometry.Trie
 import           Test.Hspec
 
@@ -11,10 +12,16 @@ import           Test.Hspec
 
 spec :: Spec
 spec = describe "Trie tests" $ do
-         it "fold1" $
+         it "fold1 Map" $
            fold1 myTrie `shouldBe` "rootbarfoograndchild"
-         it "foldable1 to list" $
+         it "foldable1 to list Map" $
            toNonEmpty myTrie `shouldBe` NonEmpty.fromList ["root","bar","foo","grandchild"]
+
+         it "fold1 KV" $
+           fold1 myTrie `shouldBe` "rootbarfoograndchild"
+         it "foldable1 to list KV" $
+           toNonEmpty myTrie `shouldBe` NonEmpty.fromList ["root","bar","foo","grandchild"]
+
 
 
 myTrie :: TrieF Map.Map Int String
@@ -24,5 +31,13 @@ myTrie = Node "root" (Map.fromAscList [ (1,Node "foo" (Map.fromAscList
                                         )
                                       , (2,Node "bar" Map.empty)
                                       ]
+
+                     )
+
+
+myTrie2 :: TrieF (KV []) Int String
+myTrie2 = Node "root" (KV [ (1,Node "foo" (KV [(10, Node "grandchild" empty)]))
+                          , (2,Node "bar" empty)
+                          ]
 
                      )
