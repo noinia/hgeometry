8c5a441a37cb056a5ead07d54e90e575fa9be3a5
Frank Staals <frank@fstaals.net>
2025-01-02 16:38:14 +0100

    new implementation of InPolygon

--- hgeometry/test-with-ipe/test/Polygon/Simple/InPolygonSpec.hs
+++ hgeometry/test-with-ipe/test/Polygon/Simple/InPolygonSpec.hs
@@ -1,29 +1,47 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE QuasiQuotes #-}
-module Polygon.Simple.InPolygonSpec (spec) where
+module Polygon.Simple.InPolygonSpec where -- (spec) where
 
 import           Control.Lens
+import           Control.Monad
+import           Data.List.NonEmpty (NonEmpty(..))
 import qualified Data.List.NonEmpty as NonEmpty
+import           Data.Maybe (fromJust)
 import           Data.Proxy
 import           Golden
 import           HGeometry.Boundary
 import           HGeometry.Ext
+import           HGeometry.LineSegment
+import           HGeometry.Number.Real.Rational
 import           HGeometry.Point
+import           HGeometry.Polygon.Class
 import           HGeometry.Polygon.Simple
 import           Ipe
 import           System.OsPath
 import           Test.Hspec
 import           Test.QuickCheck.Instances ()
 
+import           Debug.Trace
+import           HGeometry.Intersection
+import           HGeometry.Interval
+import           HGeometry.Polygon.Simple.InPolygon
 --------------------------------------------------------------------------------
 
--- type R = RealNumber 5
+type R = RealNumber 5
 
 spec :: Spec
 spec = do
   testCases [osp|test-with-ipe/Polygon/Simple/pointInPolygon.ipe|]
   numericalSpec
   lineSegmentContainsSpec
+
+  it "darkOrangePolygonBug" $
+    ((Point2 400 288) `inPolygon` darkOrangePoly) `shouldBe` StrictlyInside
+  it "darkOrangePolygonBug2" $
+    ((Point2 400 288) `inSimplePolygonX` darkOrangePoly) `shouldBe` StrictlyInside
+  it "darkOrangePolygonBug3" $
+    ((Point2 400 240) `inSimplePolygonX` darkOrangePoly) `shouldBe` StrictlyInside
+
 
 testCases    :: OsPath -> Spec
 testCases fp = runIO (readInputFromFile =<< getDataFileName fp) >>= \case
@@ -141,14 +159,110 @@ lineSegmentContainsSpec = describe "containedIn tests" $ do
                      = NonEmpty.fromList $ readAll page
         pure (segs',pgs')
       forM_ polies $ \(poly :+ ats) -> do
-        let (inSegs,outSegs) = NonEmpty.partition (sameColor ats) segs
-        it "segments inside" $ do
-          forM_ inSegs $ \inSeg ->
-            inSeg `shouldSatisfy` (`containedIn` poly)
-        it "segments ouutside" $ do
-          forM_ outSegs $ \seg ->
-            seg `shouldSatisfy` (not . (`containedIn` poly))
+        describe ("containedIn polygon of color" <> show (fromJust $ lookupAttr SStroke ats)) $ do
+          let (inSegs,outSegs) = NonEmpty.partition (sameColor ats) segs
+            -- ClosedLineSegment (Point2 368 288) (Point2 400 288)
 
+          describe "segments inside" $ do
+            forM_ inSegs $ \(seg :+ _) -> do
+              describe (show seg) $ do
+                it "segment stats inside" $
+                  ((seg^.start) `inPolygon` poly) `shouldNotBe` StrictlyOutside
+                it "segment ends inside" $
+                  ((seg^.end) `inPolygon` poly) `shouldNotBe` StrictlyOutside
+                it "segments contained" $
+                  seg `shouldSatisfy` (`containedIn` poly)
+          it "segments ouutside" $ do
+            forM_ outSegs $ \(seg :+ _) ->
+              seg `shouldSatisfy` (not . (`containedIn` poly))
 
   where
     sameColor ats (_ :+ ats') = lookupAttr SStroke ats == lookupAttr SStroke ats'
+
+
+darkOrangePoly :: SimplePolygon (Point 2 R)
+darkOrangePoly = fromJust . fromPoints . NonEmpty.fromList $
+  [ Point2 336 272
+  , Point2 384 320
+  , Point2 480 224
+  , Point2 448 192
+  , Point2 336 240
+  , Point2 400 256
+  ]
+
+
+--------------------------------------------------------------------------------
+
+
+
+inSimplePolygonX        :: forall queryPoint simplePolygon point r.
+                          ( Num r, Ord r, Point_ point 2 r, Point_ queryPoint 2 r
+                          , SimplePolygon_ simplePolygon point r
+                          , Show r, Show queryPoint, Show point
+                          )
+                       => queryPoint -> simplePolygon
+                       -> PointLocationResultWith (VertexIx simplePolygon)
+q `inSimplePolygonX` pg = case ifoldMapOf outerBoundaryWithNeighbours countAbove pg of
+                            OnEdge s                       -> OnBoundaryEdge s
+                            NumStrictlyAbove m | odd m     -> StrictlyInside
+                                               | otherwise -> StrictlyOutside
+  where
+    -- we count the number of by the vertical upward ray from q intersects the boundary of
+    -- the polygon. If the number of times we intersect the boundary is odd we are inside,
+    -- and outside othwerise.
+
+    --
+    -- Generally, countAbove will compute the contribution of the edge uv (which is edge i).
+    --
+    --
+    -- we have to take special care of vertical edges, and when the ray goes through a
+    -- vertex u.
+    countAbove (i,_) (u,(p,v)) = case (u^.xCoord) `compare` (q^.xCoord) of
+      LT | (q^.xCoord) < (v^.xCoord) -> belowLineSeg i u v
+         -- for q to lie below the edge, v has to lie right of q and q has to actually lie
+         -- below the line segment.
+         --
+         -- Note that if q lies strictly below v we don't count it here. We handle it
+         -- when handing vertex v
+         | otherwise                 -> mempty
+
+      GT | (q^.xCoord) > (v^.xCoord) -> belowLineSeg i v u
+            -- for q to lie below the edge, v has to lie left of q and
+            -- q has to actually lie below the line through u and v.
+         | otherwise                 -> mempty
+
+
+      EQ -> case (u^.yCoord) `compare` (q^.yCoord) of
+              EQ                             -> OnEdge i
+                -- q == u, so it lies on edge i
+
+              LT | (q^.xCoord) == (v^.xCoord) &&
+                   (q^.yCoord) < (v^.yCoord) -> OnEdge i
+                 -- q lies above u. So the only case in which q does lie on the edge uv
+                 -- is if it is vertical, and q lies on it.
+                 | otherwise                 -> mempty
+                 -- q lies above u, so otherwise it does not lie on the edge starting at u.
+
+              GT -> case (q^.xCoord) `compare` (v^.xCoord) of
+                EQ | (q^.yCoord) > (v^.yCoord) -> OnEdge i
+                   | otherwise                 -> mempty
+                   -- the edge uv is vertical. We already established that u lies above
+                   -- q, so it lies on the edge only if v lies strictly below q.
+
+                LT | q^.xCoord <= p^.xCoord -> mempty
+                   -- the predecessor vertex p and v lie on the same side of the vertical line
+                   -- through u. So we don't count this vertex/edge
+                   -- TODO: not sure if this should be <= or <
+                   | otherwise              -> belowLineSeg i v u
+                GT | q^.xCoord >= p^.xCoord -> mempty -- same as before v and p on the same side.
+                   | otherwise              -> belowLineSeg i u v
+
+
+    -- | count the edge if q is below the line through l and r,
+    --
+    -- pre: l is left of r.
+    -- pre: q-x lies in the interval [lx,rx]
+    belowLineSeg i l r = case ccw l q r of
+      CW       -> mempty -- q lies strictly above the segment lr
+      CoLinear -> OnEdge i
+      CCW      -> NumStrictlyAbove 1  -- q lies strictly below the segment lr
