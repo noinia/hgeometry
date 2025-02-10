9dee9ebc960acbdcda03817b640c316b7c099d6d
Frank Staals <frank@fstaals.net>
2025-02-10 20:04:40 +0100

    moving polygon stuff into a separate subdir as well

--- hgeometry/polygon/doctests.hs
+++ hgeometry/polygon/doctests.hs
@@ -1,7 +1,5 @@
 {-# OPTIONS_GHC -F -pgmF doctest-driver-gen
                 -optF src
-                -optF polygon/src
-                -optF polyline/src
 
                 -optF -XTypeFamilies
                 -optF -XGADTs
