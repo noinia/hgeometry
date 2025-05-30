77d92190d7072d8933a672b54ed5f3eb96031176
Frank Staals <frank@fstaals.net>
2025-05-30 20:28:26 +0200

    more debugging and more tests for the unbounded x triangle intersection

--- hgeometry/test-with-ipe/test/TriangleUnboundedConvexSpec.hs
+++ hgeometry/test-with-ipe/test/TriangleUnboundedConvexSpec.hs
@@ -1,5 +1,6 @@
-{-# LANGUAGE  UndecidableInstances  #-}
+{-# LANGUAGE UndecidableInstances  #-}
 {-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE OverloadedStrings #-}
 module TriangleUnboundedConvexSpec where
 
 import           Control.Lens as Lens
@@ -24,12 +25,13 @@ import           HGeometry.Properties
 import           HGeometry.Triangle
 import           HGeometry.Vector
 import           Ipe
+import           Ipe.Color
 import           System.OsPath
 import           Test.Hspec
 import           Test.Hspec.WithTempFile
 import           Test.QuickCheck.Instances ()
 
-import Debug.Trace
+import           Debug.Trace
 --------------------------------------------------------------------------------
 
 type UnboundedConvexRegion point = UnboundedConvexRegionF (NumType point) NonEmpty point
@@ -71,11 +73,11 @@ toBoundedFrom :: (Foldable nonEmpty, Point_ point 2 r, Point_ point' 2 r, Ord r,
               -> ConvexPolygonF NonEmpty point
 toBoundedFrom tri reg@(Unbounded v pts w) = case extremalVertices reg of
     Right (Vector2 p q) -> let l@(LinePV _ u) = lineThrough p q
-                               h              = HalfSpace Positive l
+                               h              = HalfSpace Negative l
                            in compute p q h u
     Left p              -> let bisec = w ^+^ negated v -- note that v is pointing in the wrong dir.
                                u     = rot90 bisec -- perpendicular to bisec
-                               h     = HalfSpace Positive (fromPointAndVec p u)
+                               h     = HalfSpace Negative (fromPointAndVec p u)
                            in compute p p h u
   where
     -- given the halfpsace h that goes through p and q (the extremal vertices),
@@ -200,8 +202,15 @@ ipeSpec inFp outFP = do (chain,tri) <- runIO go
                         goldenWith dataPath
                                    (ipeContentGolden { name = outFP })
                                    [ case tri `intersect` chain of
-                                       Nothing -> iO' $ ipeGroup [] :+ mempty
-                                       Just x  -> iO' $ fmap (^.asPoint) <$> x
+                                       Nothing    -> iO $ ipeLabel ("no intersection" :+ origin)
+                                                        ! attr SStroke black
+                                       Just inter -> case inter of
+                                         DegenerateVertex v -> iO $ defIO (v^.asPoint)
+                                                                  ! attr SStroke red
+                                         DegenerateEdge e   -> iO $ defIO ((^.asPoint) <$> e)
+                                                                  ! attr SStroke red
+                                         ActualPolygon poly -> iO $ defIO ((^.asPoint) <$> poly)
+                                                                  ! attr SFill red
                                    , iO' chain
                                    , iO' tri
                                    ]
@@ -214,7 +223,7 @@ ipeSpec inFp outFP = do (chain,tri) <- runIO go
                  )
 
 spec :: Spec
-spec = describe "triangle x unbounde convex polygon intersection" $ do
+spec = describe "triangle x unbounded convex polygon intersection" $ do
          it "manual test" $
            (myTriangle `intersects` upperQuadrant) `shouldBe` True
          it "manual test" $
@@ -222,9 +231,13 @@ spec = describe "triangle x unbounde convex polygon intersection" $ do
          it "outside test" $
            (outsideTriangle `intersects` upperQuadrant) `shouldBe` False
          ipeSpec [osp|triangle_x_unbounded.ipe|]
-                 [osp|triangle_x_unbounded.ipe.out|]
+                 [osp|triangle_x_unbounded.out|]
          ipeSpec [osp|triangle_x_unbounded1.ipe|]
-                 [osp|triangle_x_unbounded1.ipe.out|]
+                 [osp|triangle_x_unbounded1.out|]
+         ipeSpec [osp|triangle_x_cone.ipe|]
+                 [osp|triangle_x_cone.out|]
+         ipeSpec [osp|triangle_x_cone_no.ipe|]
+                 [osp|triangle_x_cone_no.out|]
 
 
 instance ( HasDefaultIpeOut original, HasDefaultIpeOut extra
