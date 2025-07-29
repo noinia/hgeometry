bd5658a247cc1a39f2d8d7185958c43d83ba624d
Frank Staals <frank@fstaals.net>
2025-07-29 15:55:53 -0600

    more debugging / tests

--- hgeometry/test-with-ipe/test/Plane/LowerEnvelopeSpec.hs
+++ hgeometry/test-with-ipe/test/Plane/LowerEnvelopeSpec.hs
@@ -11,20 +11,28 @@ import           Data.Foldable
 import           Data.Foldable1
 import           Data.List.NonEmpty (NonEmpty(..))
 import qualified Data.List.NonEmpty as NonEmpty
+import qualified Data.Map as Map
 import qualified Data.Map.NonEmpty as NEMap
+import           Data.Semigroup
 import qualified Data.Set as Set
+import qualified Data.Set.NonEmpty as NESet
 import qualified Data.Text as Text
 import           Golden
 import           HGeometry.Box
 import           HGeometry.Cyclic
 import           HGeometry.Ext
+import           HGeometry.HyperPlane.NonVertical
+import           HGeometry.Instances ()
 import           HGeometry.Number.Real.Rational
 import           HGeometry.Plane.LowerEnvelope
+import qualified HGeometry.Plane.LowerEnvelope.Connected.BruteForce as BruteForce
 import qualified HGeometry.Plane.LowerEnvelope.Connected.Randomized as Randomized
 import           HGeometry.Point
 import           HGeometry.Polygon.Convex
 import           HGeometry.Polygon.Convex.Unbounded
+import           HGeometry.Properties
 import           HGeometry.Sequence.Alternating (separators)
+import           HGeometry.Vector
 import           HGeometry.VoronoiDiagram
 import qualified HGeometry.VoronoiDiagram as VD
 import           Hiraffe.Graph.Class
@@ -33,7 +41,9 @@ import           Ipe.Color
 import           System.OsPath
 import           System.Random
 import           Test.Hspec
+import           Test.Hspec.QuickCheck
 import           Test.Hspec.WithTempFile
+import           Test.QuickCheck
 import           Test.QuickCheck.Instances ()
 
 --------------------------------------------------------------------------------
@@ -134,24 +144,25 @@ instance ( Point_ point 2 r, Fractional r, Ord r, Ord point
 
 spec :: Spec
 spec = describe "lower envelope tests" $ do
-         testIpe [osp|trivial.ipe|]
-                 [osp|trivial_out|]
-         testIpe [osp|simplest.ipe|]
-                 [osp|simplest_out|]
-         testIpe [osp|simpler.ipe|]
-                 [osp|simpler_out|]
-         testIpe [osp|simple.ipe|]
-                 [osp|simple_out|]
-         testIpe [osp|simple1.ipe|]
-                 [osp|simple1_out|]
-         testIpe [osp|foo.ipe|]
-                 [osp|foo_out|]
-         testIpe [osp|degenerate.ipe|]
-                 [osp|degenerate_out|]
-         testIpe [osp|degenerate1.ipe|]
-                 [osp|degenerate1_out|]
-         testIpe [osp|degenerate2.ipe|]
-                 [osp|degenerate2_out|]
+         randomizedSameAsBruteForce
+         -- testIpe [osp|trivial.ipe|]
+         --         [osp|trivial_out|]
+         -- testIpe [osp|simplest.ipe|]
+         --         [osp|simplest_out|]
+         -- testIpe [osp|simpler.ipe|]
+         --         [osp|simpler_out|]
+         -- testIpe [osp|simple.ipe|]
+         --         [osp|simple_out|]
+         -- testIpe [osp|simple1.ipe|]
+         --         [osp|simple1_out|]
+         -- testIpe [osp|foo.ipe|]
+         --         [osp|foo_out|]
+         -- testIpe [osp|degenerate.ipe|]
+         --         [osp|degenerate_out|]
+         -- testIpe [osp|degenerate1.ipe|]
+         --         [osp|degenerate1_out|]
+         -- testIpe [osp|degenerate2.ipe|]
+         --         [osp|degenerate2_out|]
 
 
 -- | Build voronoi diagrams on the input points
@@ -331,3 +342,101 @@ trivialVD = VoronoiDiagram $ LowerEnvelope vInfty (Seq.fromList [bv])
   -- order of the planes is incorrect, as is the z-coord.
 
 -}
+
+
+
+
+
+
+
+
+--------------------------------------------------------------------------------
+
+newtype MyPlane = MyPlane (Plane R)
+  deriving newtype (Show,Eq,Ord)
+
+asPlane :: Iso' MyPlane (Plane R)
+asPlane = coerced
+
+type instance NumType MyPlane   = R
+type instance Dimension MyPlane = 3
+
+instance HyperPlane_ MyPlane 3 R
+instance NonVerticalHyperPlane_  MyPlane 3 R where
+  hyperPlaneCoefficients = asPlane.hyperPlaneCoefficients
+
+instance Arbitrary MyPlane where
+  arbitrary = do h <- arbitrary `suchThat` allOf (hyperPlaneCoefficients.traverse) (inRange 100)
+                 pure $ MyPlane h
+  shrink (MyPlane p) = MyPlane <$> shrink p
+
+-- instance (Arbitrary a, Ord a) => Arbitrary (NESet.NESet a) where
+--   arbitrary = do x  <- arbitrary
+--                  xs <- arbitrary
+--                  pure $ NESet.fromList (x :| xs)
+
+
+inRange m x = abs x <= m
+
+randomizedSameAsBruteForce :: Spec
+randomizedSameAsBruteForce = describe "randomized lower envelope tests" $ do
+    prop "randomized should be the same as brute force" $
+      \(hs :: NESet.NESet MyPlane) ->
+        Randomized.computeVertexForm (mkStdGen 1) hs `shouldBe` BruteForce.computeVertexForm hs
+
+    prop "definers start with up direction (brute force)" $
+      \(hs :: NESet.NESet MyPlane) ->
+        verifyStartWithUp $ BruteForce.computeVertexForm hs
+
+    -- prop "definers start with up direction (randomized)" $
+    --   \(hs :: NESet.NESet MyPlane) ->
+    --     verifyStartWithUp $ Randomized.computeVertexForm (mkStdGen 1) hs
+
+-- debug = do let hs = undefined
+--            renderToIpe [osp|/tmp/bruteforce.ipe|] BruteForce.computeVertexForm hs
+--            renderToIpe [osp|/tmp/randomized.ipe|] Randomized.computeVertexForm (mkStdGen 1) hs
+
+
+verifyStartWithUp env =  let startWithUp        :: Point 3 R -> Definers MyPlane -> All
+                             startWithUp p defs = let h = NonEmpty.head $ toNonEmpty defs
+                                                      q = projectPoint p .+^ Vector2 0 1
+                                                      z = evalAt q h
+                                                  in All $ all (\h' -> evalAt q h' >= z) defs
+                                    -- verify that h is the lowest at q
+                         in getAll $ Map.foldMapWithKey startWithUp env
+
+
+renderToIpe fp env = writeIpeFile fp . addStyleSheet opacitiesStyle $ singlePageFromContent out
+  where
+    out = zipWith render (cycle $ drop 3 basicNamedColors)
+        . toList . NEMap.assocs . HGeometry.Plane.LowerEnvelope.asMap $ env
+
+    render color (_, region) = iO' $ ipeGroup
+                               [ iO $ defIO region   ! attr SFill    color
+                                                     ! attr SOpacity (Text.pack "10%")
+                               -- , iO $ defIO (site^.asPoint) ! attr SStroke  color
+                               -- ,
+                               ]
+
+
+-- TODO: for whatever reason we get the definers in a different order.
+-- According to the spec of Definers the edges should be in CCW order, starting with the plane that is minimal at the vertical up direction.
+--
+-- bug =fromList (NonVerticalHyperPlane [-2,5,-4.33334] :| [NonVerticalHyperPlane [-0.83334,1.5,-3],NonVerticalHyperPlane [1.33333,1.5,0.33333]])
+
+
+
+--        -- expected:
+
+--      fromList [(Point3 (-1.53846~) (-0.13187~) (-1.91574~),
+
+--                 Definers (NonVerticalHyperPlane [-0.83334,1.5,-3] :| [
+--                           NonVerticalHyperPlane [1.33333,1.5,0.33333],
+--                           NonVerticalHyperPlane [-2,5,-4.33334]]))]
+
+--      but got:
+
+--      fromList [(Point3 (-1.53846~) (-0.13187~) (-1.91574~)
+--                ,Definers (NonVerticalHyperPlane [1.33333,1.5,0.33333] :| [
+--                           NonVerticalHyperPlane [-2,5,-4.33334],
+--                           NonVerticalHyperPlane [-0.83334,1.5,-3]]))]
