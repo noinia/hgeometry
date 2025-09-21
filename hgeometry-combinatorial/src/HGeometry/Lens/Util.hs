6fa23650555fc3bee00a6c13fac0ed289937c1c4
Frank Staals <991345+noinia@users.noreply.github.com>
2025-09-21 21:16:39 +0200

    some lens helpers + assocs on MonoidalMaps (#292)

--- hgeometry-combinatorial/src/HGeometry/Lens/Util.hs
+++ hgeometry-combinatorial/src/HGeometry/Lens/Util.hs
@@ -12,15 +12,23 @@ module HGeometry.Lens.Util
   ( folding1
   , ifolding1
   , itoNonEmptyOf
+
+  , toVectorOf
+  , toNonEmptyVectorOf
   ) where
 
-import Control.Lens
-import Control.Lens.Internal.Fold (NonEmptyDList(..))
-import Data.Foldable1
-import Data.Functor.Apply (Apply)
-import Data.Functor.Contravariant (phantom)
-import Data.List.NonEmpty (NonEmpty(..))
-import Data.Semigroup.Foldable
+import           Control.Lens
+import           Control.Lens.Internal.Fold (NonEmptyDList(..))
+import           Data.Foldable1
+import           Data.Functor.Apply (Apply)
+import           Data.Functor.Contravariant (phantom)
+import           Data.List.NonEmpty (NonEmpty(..))
+import           Data.Semigroup.Foldable
+import qualified VectorBuilder.Builder as Builder
+import qualified VectorBuilder.Vector as Builder
+import           Data.Vector.NonEmpty (NonEmptyVector)
+import           Data.Vector (Vector)
+import qualified Data.Vector.NonEmpty as NonEmptyV
 
 --------------------------------------------------------------------------------
 
@@ -42,3 +50,13 @@ ifolding1 sfa f = phantom . traverse1_ (phantom . uncurry (indexed f)) . sfa
 itoNonEmptyOf   :: IndexedGetting i (NonEmptyDList (i,a)) s a -> s -> NonEmpty (i,a)
 itoNonEmptyOf l = flip getNonEmptyDList [] . ifoldMapOf l (\i a -> NonEmptyDList ((i,a) :|))
 {-# INLINE itoNonEmptyOf #-}
+
+--------------------------------------------------------------------------------
+
+-- | Given a fold, construct a non-emptyVector out of it
+toNonEmptyVectorOf   :: Getting (Builder.Builder a) s a -> s -> NonEmptyVector a
+toNonEmptyVectorOf l = NonEmptyV.unsafeFromVector . toVectorOf l
+
+-- | Given a fold, construct a non-emptyVector out of it
+toVectorOf     :: Getting (Builder.Builder a) s a -> s -> Vector a
+toVectorOf l s = Builder.build $ foldMapOf l Builder.singleton s
