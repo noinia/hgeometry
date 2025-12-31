a5fd67d709077aad60e70136bc6ab902d519b820
Frank Staals <frank@fstaals.net>
2025-12-31 09:47:13 +0100

    numtypes

---
+++ hgeometry/numtypes/Double.hs
@@ -0,0 +1,14 @@
+--------------------------------------------------------------------------------
+-- |
+-- Module      :  Double
+-- Copyright   :  (C) Frank Staals
+-- License     :  see the LICENSE file
+-- Maintainer  :  Frank Staals
+--
+-- Defines the Real-number type
+--
+--------------------------------------------------------------------------------
+module Double where
+
+-- | The Real number type
+type R = Double
