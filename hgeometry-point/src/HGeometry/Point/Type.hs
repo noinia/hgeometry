cd370d9f7de973729ceb261a8820ed68d2144f19
Frank Staals <frank@fstaals.net>
2026-04-04 11:37:09 +0200

    adding a PickInteriorPoint class

--- hgeometry-point/src/HGeometry/Point/Type.hs
+++ hgeometry-point/src/HGeometry/Point/Type.hs
@@ -1,6 +1,7 @@
 module HGeometry.Point.Type
   ( Point
   , PointF(..)
+  , HasPickInteriorPoint(..)
   ) where
 
 import HGeometry.Point.PointF
@@ -8,3 +9,10 @@ import HGeometry.Vector
 
 -- | Defines our point type
 type Point d r = PointF (Vector d r)
+
+--------------------------------------------------------------------------------
+
+-- | A Class for geometry types that support returning a point inside them.
+class HasPickInteriorPoint geom d r | geom -> r, geom -> d where
+  -- | Returns a point in the interior of the object
+  pointInteriorTo :: geom -> Point d r
