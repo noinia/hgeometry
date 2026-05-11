444655a838b9d72a4f0c75a80157118819c8becb
Frank Staals <frank@fstaals.net>
2026-05-11 18:50:32 +0200

    creating a separate sublib for the api / and a separate Debug.Draw module

--- hgeometry-debugger/server/Main.hs
+++ hgeometry-debugger/server/Main.hs
@@ -19,52 +19,23 @@ import qualified Data.Map as Map
 import           Data.Map (Map)
 import           Data.Aeson
 import           Data.Text (Text)
-import           Data.Text.Encoding (decodeUtf8Lenient)
 import           GHC.Generics
-import           System.IO.Unsafe (unsafePerformIO)
 import           Data.Kind (Type)
-import           Servant.Client ( ClientM, runClientM, ClientEnv, mkClientEnv
-                                , BaseUrl(..), client, Scheme(..)
-                                )
 import           Control.Concurrent.Async
 import           Network.HTTP.Media ((//), (/:))
 import           Debugger.API
 import           HGeometry.Point
 import           HGeometry.Miso.Svg
+import           Debug.Draw
 
 --------------------------------------------------------------------------------
 
 
 -- type Svg =
 
-draw' :: Drawable a => a -> Drawing
-draw' = Drawing . toMisoString . decodeUtf8Lenient . toStrict . toHtml . flip draw []
 
 --------------------------------------------------------------------------------
 -- * Our API
-
--- | Trace and draw
-traceDrawId         :: (Show a, Drawable a) => LayerName -> a -> a
-traceDrawId layer a = traceDraw layer a a
-
--- | Trace and draw an a on the given layer, while returning a b.
---
--- this will add to the current layer
-traceDraw           :: (Show a, Drawable a) => LayerName -> a -> b -> b
-traceDraw layer a b = unsafePerformIO $ traceDrawIO layer a b
-{-# NOINLINE traceDraw #-}
-
--- | Implementation of traceDraw
-traceDrawIO           :: (Show a,Drawable a) => LayerName -> a -> b -> IO b
-traceDrawIO layer a b = b <$ debugClient (clientDrawLayer (layer,show a, draw' a))
-
--- | Clears a particular layer
-clearLayer      :: LayerName -> IO ()
-clearLayer layer = debugClient $ clientClearLayer layer
-
--- | Clears all layers
-clear :: IO ()
-clear = debugClient clientClear
 
 
 -- | Start the debug server, then do stuff
@@ -82,35 +53,6 @@ runDebugServer port act = do putStrLn $ "Starting the server on port " <> show p
 
 
 
---------------------------------------------------------------------------------
--- * The client
-
-clientDrawing
-  :<|> clientDrawLayer
-  :<|> clientClearLayer
-  :<|> clientClear
-  = client (Proxy @API)
-
--- type Client = ReaderT ClientEnv IO
-
--- | Run some client action; just print the error if it fails somehow.
-debugClient     :: ClientM a -> IO ()
-debugClient act = runClientM act debugClientEnv >>= \case
-                    Left err -> putStrLn $ "error: " <> show err
-                    Right _  -> pure ()
-
--- | The default debug clientEnv
-debugClientEnv :: ClientEnv
-debugClientEnv = unsafePerformIO debugClientEnv'
-{-# NOINLINE debugClientEnv #-}
-
--- | Creates a default ClientEnv
-debugClientEnv' :: IO ClientEnv
-debugClientEnv' = do mgr <- newManager defaultManagerSettings
-                     pure $ mkClientEnv mgr defaultBaseUrl
-
-defaultBaseUrl :: BaseUrl
-defaultBaseUrl = BaseUrl Http defaultHost defaultPort ""
 
 
 --------------------------------------------------------------------------------
